ec6a04931fb34c3bbac0b42ab43e51fa74f4a9fc
Cory McCartan <cory.mccartan@gmail.com>
2021-09-17 18:52:40 -0400

    update fix_adj

--- R/fix_adj.R
+++ R/fix_adj.R
@@ -1,33 +1,43 @@
 #' Modify geographic assignment for VTDs to avoid non-contiguities
 #'
-#' @param vtd_shp A shp file for a region, possibly generated from `create-vtd-template.R`
+#' @param data A data frame
 #' @param map_obj A redist_map object corresponding to `vtd_shp`. Used for adjacency matrix.
-#' @param col_name The column of geographic information.
+#' @param col The column of geographic information.
 #'
-#' @return A column vector with the reassigned geographic codes
+#' @return An updated data frame with corrected geographic codes
 #' @export
-fix_geo_assignment <- function(vtd_shp, col_name) {
+fix_geo_assignment <- function(data, col, adj=data$adj) {
     # get populations of geo units
-    geo_pops <- vtd_shp %>%
-        st_drop_geometry() %>%
-        count(!!as.name(col_name), wt = pop, name = "pop")
+    geo_pops <- data %>%
+        sf::st_drop_geometry() %>%
+        count({{ col }}, wt = pop, name = "pop")
 
-    # add row number to vtd_shp
-    vtd_operate <- vtd_shp %>%
-        mutate(id = row_number())
-
-    # make adjacency matrix
-    adj = redist.adjacency(vtd_shp)
+    col_val = rlang::eval_tidy(rlang::enquo(col), data)
+    if (!is.character(col_val) && !is.factor(col_val) && !is.integer(col_val))
+        cli_abort("Column must be a {.cls character}, {.cls factor}, or {.cls integer}.")
+    if (is.character(col_val)) {
+        col_type = "chr"
+        lev = unique(col_val)
+        col_val = match(col_val, lev)
+    } else if (is.factor(col_val)) {
+        col_type = "fct"
+        lev = levels(col_val)
+        col_val = as.integer(col_val)
+    } else {
+        col_type = "int"
+    }
 
     # map to internal function
-    updated_geos <- map_dbl(.x = 1:nrow(vtd_shp),
-        .f = ~ assign_geo(.x,
-            all_geos = vtd_shp[[col_name]],
-            adj = adj,
-            col_name = col_name,
-            geo_pops = geo_pops))
+    updated_geos <- purrr::map_int(.x = 1:nrow(data),
+        .f = ~ assign_geo(.x, col_val, adj, col, geo_pops))
 
-    return(updated_geos)
+    if (col_type == "chr")
+        updated_geos = lev[updated_geos]
+    if (col_type == "fct")
+        updated_geos = factor(lev[updated_geos], levels = lev,
+                              ordered = is.ordered(col_val))
+
+    mutate(data, {{ col }} := updated_geos)
 }
 
 #' Reassign geographic units to fix some common contiguity issues.
@@ -35,13 +45,13 @@ fix_geo_assignment <- function(vtd_shp, col_name) {
 #' @param i id number of the place
 #' @param all_geos vector of geographic ids
 #' @param adj adjacency matrix, from a redist_map object
-#' @param col_name The column of geographic information, must be a string
+#' @param col The column of geographic information
 #' @param geo_pops A tibble of populations in each geographic unit
 #'
 #' @return A numeric object of the new geographic code,
 #'
 #' @keywords internal
-assign_geo <- function(i, all_geos, adj, col_name, geo_pops) {
+assign_geo <- function(i, all_geos, adj, col, geo_pops) {
     # get neighbors and their geo units
     neighbors <- adj[[i]]
     neighbor_geo <- unique(all_geos[neighbors])
@@ -68,9 +78,9 @@ assign_geo <- function(i, all_geos, adj, col_name, geo_pops) {
         # if so, assign to place with highest population of neighbors
         return(
             geo_pops %>%
-                filter(!!as.name(col_name) %in% neighbor_geo) %>%
+                filter({{ col }} %in% neighbor_geo) %>%
                 filter(pop == max(pop)) %>%
-                pull(!!as.name(col_name))
+                pull({{ col }})
         )
 
     }
