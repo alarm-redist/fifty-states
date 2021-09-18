95711d5d02fdedcbd0f9ecb8f274d7e09fd329fa
Cory McCartan <cory.mccartan@gmail.com>
2021-09-17 22:27:30 -0400

    enforce style

--- analyses/IA_cd_2020/01_prep_IA_cd_2020.R
+++ analyses/IA_cd_2020/01_prep_IA_cd_2020.R
@@ -4,14 +4,14 @@
 ###############################################################################
 
 suppressMessages({
-library(dplyr)
-library(readr)
-library(sf)
-library(redist)
-library(geomander)
-library(cli)
-library(here)
-devtools::load_all(".") # load utilities
+    library(dplyr)
+    library(readr)
+    library(sf)
+    library(redist)
+    library(geomander)
+    library(cli)
+    library(here)
+    devtools::load_all(".") # load utilities
 })
 
 # Download necessary files for analysis -----
@@ -30,25 +30,25 @@ shp_path = "data-out/IA_2020/shp_vtd.rds"
 if (!file.exists(here(shp_path))) {
     cli_process_start("Preparing {.strong IA} shapefile")
     # read in redistricting data
-    ia_shp = read_csv(here(path_data), col_types = cols(GEOID20="c")) %>%
+    ia_shp = read_csv(here(path_data), col_types = cols(GEOID20 = "c")) %>%
         join_vtd_shapefile() %>%
         st_transform(EPSG$IA)
 
     d_cd = make_from_baf("IA", "CD", "VTD") %>%
         transmute(vtd = str_sub(vtd, 4),
-               cd_2010 = as.integer(cd))
-    ia_shp = left_join(ia_shp, d_cd, by="vtd") %>%
-        relocate(cd_2010, .after=county)
+            cd_2010 = as.integer(cd))
+    ia_shp = left_join(ia_shp, d_cd, by = "vtd") %>%
+        relocate(cd_2010, .after = county)
 
     ia_shp = ia_shp %>%
         group_by(state, county) %>%
-        summarize(cd_2010=cd_2010[1],
-                  across(pop:area_water, sum))
+        summarize(cd_2010 = cd_2010[1],
+            across(pop:area_water, sum))
 
     # simplifies geometry for faster processing, plotting, and smaller shapefiles.
     if (requireNamespace("rmapshaper", quietly = TRUE)) {
         ia_shp = rmapshaper::ms_simplify(ia_shp, keep = 0.15,
-                                         keep_shapes = TRUE) %>%
+            keep_shapes = TRUE) %>%
             suppressWarnings()
     }
 
@@ -61,4 +61,3 @@ if (!file.exists(here(shp_path))) {
     ia_shp = read_rds(here(shp_path))
     cli_alert_success("Loaded {.strong IA} shapefile")
 }
-
--- analyses/IA_cd_2020/02_setup_IA_cd_2020.R
+++ analyses/IA_cd_2020/02_setup_IA_cd_2020.R
@@ -5,7 +5,7 @@
 cli_process_start("Creating {.cls redist_map} object for {.pkg IA_cd_2020}")
 
 map = redist_map(ia_shp, pop_tol = 0.0001,
-                 existing_plan = cd_2010, adj = ia_shp$adj)
+    existing_plan = cd_2010, adj = ia_shp$adj)
 
 # Output the redist_map object. Do not edit this path.
 write_rds(map, "data-out/IA_2020/IA_cd_2020_map.rds", compress = "xz")
