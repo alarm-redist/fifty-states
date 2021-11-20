684907d5cdd4c188d94eee12e7ecbcfa3e1903c6
Christopher Kenny <ctk44@cornell.edu>
2021-11-19 22:16:28 -0500

    start ny

---
+++ analyses/NY_cd_2020/01_prep_NY_cd_2020.R
@@ -0,0 +1,137 @@
+###############################################################################
+# Download and prepare data for `NY_cd_2020` analysis
+# © ALARM Project, November 2021
+###############################################################################
+
+suppressMessages({
+    library(dplyr)
+    library(readr)
+    library(sf)
+    library(redist)
+    library(geomander)
+    library(cli)
+    library(here)
+    library(fs)
+    devtools::load_all() # load utilities
+})
+
+# Download necessary files for analysis -----
+cli_process_start("Downloading files for {.pkg NY_cd_2020}")
+
+path_data <- download_redistricting_file("NY", "data-raw/NY")
+
+path_dem_irc <- here("data-raw/NY/NY congress Letters Plan Draft 9.14.csv")
+path_rep_irc <- here("data-raw/NY/NY CD Block Equivalency.xlsx")
+
+cli_process_done()
+
+# Compile raw data into a final shapefile for analysis -----
+shp_path <- "data-out/NY_2020/shp_vtd.rds"
+perim_path <- "data-out/NY_2020/perim.rds"
+
+if (!file.exists(here(shp_path))) {
+    cli_process_start("Preparing {.strong NY} shapefile")
+    # read in redistricting data
+    ny_shp <- read_csv(here(path_data), col_types = cols(GEOID20 = "c")) %>%
+        join_vtd_shapefile() %>%
+        st_transform(EPSG$NY) %>%
+        rename_with(function(x) gsub("[0-9.]", "", x), starts_with("GEOID"))
+
+    # add municipalities
+    d_muni <- make_from_baf("NY", "INCPLACE_CDP", "VTD") %>%
+        mutate(GEOID = paste0(censable::match_fips("NY"), vtd)) %>%
+        select(-vtd)
+    d_cd <- make_from_baf("NY", "CD", "VTD") %>%
+        transmute(
+            GEOID = paste0(censable::match_fips("NY"), vtd),
+            cd_2010 = as.integer(cd)
+        )
+    ny_shp <- left_join(ny_shp, d_muni, by = "GEOID") %>%
+        left_join(d_cd, by = "GEOID") %>%
+        mutate(county_muni = if_else(is.na(muni), county, str_c(county, muni))) %>%
+        relocate(muni, county_muni, cd_2010, .after = county)
+
+    if (!file.exists(path_dem_irc)) {
+        temp_dem_irc <- fs::file_temp(ext = "zip")
+        download(
+            url = "https://nyirc.gov/storage/plans/20210915/congress_letters.zip",
+            path = temp_dem_irc
+        )
+        unzip(temp_dem_irc, exdir = "data-raw/NY")
+    }
+    dem_irc_baf <- read_csv(here(path_dem_irc),
+                            col_names = c("GEOID", "dem_irc"),
+                            col_types = 'cc'
+    )
+    dem_irc_baf <- dem_irc_baf %>%
+        rowwise() %>%
+        mutate(dem_irc = tolower(dem_irc),
+               dem_irc = which(dem_irc == letters)) %>%
+        ungroup()
+
+    if (!file.exists(path_rep_irc)) {
+        temp_rep_irc <- fs::file_temp(ext = "zip")
+        download(
+            url = "https://nyirc.gov/storage/plans/20210915/congress_names.zip",
+            path = temp_rep_irc
+        )
+        unzip(temp_rep_irc, exdir = "data-raw/NY")
+    }
+    rep_irc_baf <- readxl::read_xlsx(here(path_rep_irc))
+    names(rep_irc_baf) <- c("GEOID", "rep_irc")
+    vals <- unique(rep_irc_baf$rep_irc)
+    rep_irc_baf <- rep_irc_baf %>%
+        rowwise() %>%
+        mutate(rep_irc = which(rep_irc == vals)) %>%
+        ungroup()
+    rm(vals)
+
+    baf_vtd <- PL94171::pl_get_baf("NY", geographies = "VTD")$VTD %>%
+        rename(GEOID = BLOCKID, county = COUNTYFP, vtd = DISTRICT)
+    baf <- baf_vtd %>% left_join(rep_irc_baf, by = "GEOID") %>%
+        left_join(dem_irc_baf, by = "GEOID")
+    baf <- baf %>% select(-GEOID) %>%
+        mutate(GEOID = paste0(censable::match_fips("NY"), county, vtd)) %>%
+        select(-county, vtd)
+
+    baf <- baf %>%
+        group_by(GEOID) %>%
+        summarize(rep_irc = DescTools::Mode(rep_irc),
+                  dem_irc = DescTools::Mode(dem_irc)
+                  )
+
+    baf <- baf %>% select(GEOID, rep_irc, dem_irc)
+
+    ny_shp <- ny_shp %>% left_join(baf, by = "GEOID")
+
+    # Create perimeters in case shapes are simplified
+    redist.prep.polsbypopper(
+        shp = ny_shp,
+        perim_path = here(perim_path)
+    ) %>%
+        invisible()
+
+    # create adjacency graph
+    ny_shp$adj <- redist.adjacency(ny_shp)
+
+    # simplifies geometry for faster processing, plotting, and smaller shapefiles
+    if (requireNamespace("rmapshaper", quietly = TRUE)) {
+        ny_shp <- rmapshaper::ms_simplify(ny_shp,
+                                          keep = 0.05,
+                                          keep_shapes = TRUE
+        ) %>%
+            suppressWarnings()
+    }
+
+
+    # TODO any custom adjacency graph edits here
+
+    ny_shp <- ny_shp %>%
+        fix_geo_assignment(muni)
+
+    write_rds(ny_shp, here(shp_path), compress = "gz")
+    cli_process_done()
+} else {
+    ny_shp <- read_rds(here(shp_path))
+    cli_alert_success("Loaded {.strong NY} shapefile")
+}
---
+++ analyses/NY_cd_2020/doc_NY_cd_2020.md
@@ -0,0 +1,23 @@
+# 2020 New York Congressional Districts
+
+## Redistricting requirements
+In New York, districts must:
+
+1. be contiguous
+1. have equal populations
+1. be geographically compact
+1. preserve county and municipality boundaries as much as possible
+
+
+### Interpretation of requirements
+We enforce a maximum population deviation of X.X%.
+
+## Data Sources
+Data for New York comes from the ALARM Project's [2020 Redistricting Data Files](https://alarm-redist.github.io/posts/2021-08-10-census-2020/).
+
+## Pre-processing Notes
+No manual pre-processing decisions were necessary.
+
+## Simulation Notes
+We sample 5,000 districting plans for New York.
+No special techniques were needed to produce the sample.
