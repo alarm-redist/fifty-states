781f4249cf224ea7fe687c634927f5c8f2b618b1
Christopher Kenny <ctk44@cornell.edu>
2021-12-10 22:57:52 -0500

    idaho! very fun analysis: highway constraint!

---
+++ analyses/ID_cd_2020/02_setup_ID_cd_2020.R
@@ -0,0 +1,19 @@
+###############################################################################
+# Set up redistricting simulation for `ID_cd_2020`
+# © ALARM Project, December 2021
+###############################################################################
+cli_process_start("Creating {.cls redist_map} object for {.pkg ID_cd_2020}")
+
+map <- redist_map(id_shp, pop_tol = 0.005,
+    existing_plan = cd_2020, adj = id_shp$adj)
+
+# make pseudo counties with default settings
+map <- map %>%
+    mutate(pseudo_county = pick_county_muni(map, counties = county, munis = muni))
+
+# Add an analysis name attribute
+attr(map, "analysis_name") <- "ID_2020"
+
+# Output the redist_map object. Do not edit this path.
+write_rds(map, "data-out/ID_2020/ID_cd_2020_map.rds", compress = "xz")
+cli_process_done()
