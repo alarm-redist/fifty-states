62769cef7eac2687853163ab85b273909dd9e900
Christopher Kenny <ctk44@cornell.edu>
2021-12-11 14:45:22 -0500

    Update 03_sim_OK_cd_2020.R

--- analyses/OK_cd_2020/03_sim_OK_cd_2020.R
+++ analyses/OK_cd_2020/03_sim_OK_cd_2020.R
@@ -6,23 +6,10 @@
 # Run the simulation -----
 cli_process_start("Running simulations for {.pkg OK_cd_2020}")
 
-# TODO any pre-computation (VRA targets, etc.)
-
-# TODO customize as needed. Recommendations:
-#  - For many districts / tighter population tolerances, try setting
-#  `pop_temper=0.01` and nudging upward from there. Monitor the output for
-#  efficiency!
-#  - Monitor the output (i.e. leave `verbose=TRUE`) to ensure things aren't breaking
-#  - Don't change the number of simulations unless you have a good reason
-#  - If the sampler freezes, try turning off the county split constraint to see
-#  if that's the problem.
-#  - Ask for help!
 plans <- redist_smc(map, nsims = 5e3, counties = pseudo_county)
 
 cli_process_done()
 cli_process_start("Saving {.cls redist_plans} object")
-
-# TODO add any reference plans that aren't already included
 
 # Output the redist_map object. Do not edit this path.
 write_rds(plans, here("data-out/OK_2020/OK_cd_2020_plans.rds"), compress = "xz")
