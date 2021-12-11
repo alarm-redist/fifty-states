00d2305a2f7fa376ab6cac0a90db7e46ad19c84f
Christopher Kenny <ctk44@cornell.edu>
2021-12-11 01:56:35 -0500

    IN

---
+++ analyses/IN_cd_2020/01_prep_IN_cd_2020.R
@@ -0,0 +1,78 @@
+###############################################################################
+# Download and prepare data for `IN_cd_2020` analysis
+# © ALARM Project, December 2021
+###############################################################################
+
+suppressMessages({
+    library(dplyr)
+    library(readr)
+    library(sf)
+    library(redist)
+    library(geomander)
+    library(cli)
+    library(here)
+    devtools::load_all() # load utilities
+})
+
+# Download necessary files for analysis -----
+cli_process_start("Downloading files for {.pkg IN_cd_2020}")
+
+path_data <- download_redistricting_file("IN", "data-raw/IN")
+
+cli_process_done()
+
+# Compile raw data into a final shapefile for analysis -----
+shp_path <- "data-out/IN_2020/shp_vtd.rds"
+perim_path <- "data-out/IN_2020/perim.rds"
+
+if (!file.exists(here(shp_path))) {
+    cli_process_start("Preparing {.strong IN} shapefile")
+    # read in redistricting data
+    in_shp <- read_csv(here(path_data), col_types = cols(GEOID20 = "c")) %>%
+        join_vtd_shapefile() %>%
+        st_transform(EPSG$IN)  %>%
+        rename_with(function(x) gsub("[0-9.]", "", x), starts_with("GEOID"))
+
+    # add municipalities
+    d_muni <- make_from_baf("IN", "INCPLACE_CDP", "VTD")  %>%
+        mutate(GEOID = paste0(censable::match_fips("IN"), vtd)) %>%
+        select(-vtd)
+    d_cd <- make_from_baf("IN", "CD", "VTD")  %>%
+        transmute(GEOID = paste0(censable::match_fips("IN"), vtd),
+                  cd_2010 = as.integer(cd))
+    in_shp <- left_join(in_shp, d_muni, by = "GEOID") %>%
+        left_join(d_cd, by="GEOID") %>%
+        mutate(county_muni = if_else(is.na(muni), county, str_c(county, muni))) %>%
+        relocate(muni, county_muni, cd_2010, .after = county)
+
+    # add enacted ----
+    dists <- read_sf("https://redistrict2020.org/files/IN-2021-09/Proposed_Indiana_Congressional_Districts.geojson")
+
+    in_shp$cd_2020 <- geo_match(in_shp, dists, "area")
+
+    # Create perimeters in case shapes are simplified
+    redist.prep.polsbypopper(shp = in_shp,
+                             perim_path = here(perim_path)) %>%
+        invisible()
+
+    # simplifies geometry for faster processing, plotting, and smaller shapefiles
+    if (requireNamespace("rmapshaper", quietly = TRUE)) {
+        in_shp <- rmapshaper::ms_simplify(in_shp, keep = 0.05,
+                                         keep_shapes = TRUE) %>%
+            suppressWarnings()
+    }
+
+    # create adjacency graph
+    in_shp$adj <- redist.adjacency(in_shp)
+    # 4096 is split, this keeps contiguity
+    in_shp$cd_2020[4096] <- in_shp$cd_2020[4092]
+    in_shp <- in_shp %>%
+        fix_geo_assignment(muni)
+
+    write_rds(in_shp, here(shp_path), compress = "gz")
+    cli_process_done()
+} else {
+    in_shp <- read_rds(here(shp_path))
+    cli_alert_success("Loaded {.strong IN} shapefile")
+}
+
---
+++ analyses/IN_cd_2020/doc_IN_cd_2020.md
@@ -0,0 +1,20 @@
+# 2020 Indiana Congressional Districts
+
+## Redistricting requirements
+In Indiana, districts must:
+
+1. be contiguous
+1. have equal populations
+
+### Interpretation of requirements
+We enforce a maximum population deviation of 0.5%.
+
+## Data Sources
+Data for Indiana comes from the ALARM Project's [2020 Redistricting Data Files](https://alarm-redist.github.io/posts/2021-08-10-census-2020/).
+
+## Pre-processing Notes
+No manual pre-processing decisions were necessary.
+
+## Simulation Notes
+We sample 5,000 districting plans for Indiana.
+No special techniques were needed to produce the sample.
