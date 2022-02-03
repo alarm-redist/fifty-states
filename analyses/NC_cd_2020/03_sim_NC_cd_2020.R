d26a418e54cb882cda4fe676418e3674d92f636f
Melissa Wu <55368740+mzwu@users.noreply.github.com>
2022-02-02 20:53:06 -0500

    style

--- analyses/NC_cd_2020/03_sim_NC_cd_2020.R
+++ analyses/NC_cd_2020/03_sim_NC_cd_2020.R
@@ -11,16 +11,16 @@ constr <- redist_constr(map) %>%
     add_constr_grp_hinge(6, vap_black, vap, tgts_group = c(0.52))
 
 plans <- redist_smc(map, nsims = 6e3,
-                    counties = pseudo_county,
-                    constraints = constr)
+    counties = pseudo_county,
+    constraints = constr)
 
 plans <- plans %>%
     mutate(vap_minority = group_frac(map, vap - vap_white, vap)) %>%
     group_by(draw) %>%
     mutate(vap_minority = sum(vap_minority > 0.5)) %>%
     ungroup() %>%
-    filter(vap_minority >= 2 | draw == 'cd_2020') %>%
-    slice(1 : (5001 * attr(map, 'ndists')))
+    filter(vap_minority >= 2 | draw == "cd_2020") %>%
+    slice(1:(5001*attr(map, "ndists")))
 
 cli_process_done()
 cli_process_start("Saving {.cls redist_plans} object")
@@ -44,13 +44,13 @@ if (interactive()) {
     library(ggplot2)
     library(patchwork)
 
-    redist.plot.distr_qtys(plans, vap_black / total_vap,
-                           color_thresh = NULL,
-                           color = ifelse(subset_sampled(plans)$ndv > subset_sampled(plans)$nrv, '#3D77BB', '#B25D4C'),
-                           size = 0.5, alpha = 0.5) +
-        scale_y_continuous('Percent Black by VAP') +
-        labs(title = 'North Carolina Proposed Plan versus Simulations') +
-        scale_color_manual(values = c(cd_2020_prop = 'black')) +
+    redist.plot.distr_qtys(plans, vap_black/total_vap,
+        color_thresh = NULL,
+        color = ifelse(subset_sampled(plans)$ndv > subset_sampled(plans)$nrv, "#3D77BB", "#B25D4C"),
+        size = 0.5, alpha = 0.5) +
+        scale_y_continuous("Percent Black by VAP") +
+        labs(title = "North Carolina Proposed Plan versus Simulations") +
+        scale_color_manual(values = c(cd_2020_prop = "black")) +
         ggredist::theme_r21()
 
 }
