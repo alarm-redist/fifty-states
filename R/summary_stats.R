24a40fe62e00f8f784bec6e767ae3a8916a4a78c
Christopher Kenny <ctk44@cornell.edu>
2022-02-08 10:27:59 -0500

    Update summary_stats.R

--- R/summary_stats.R
+++ R/summary_stats.R
@@ -82,8 +82,8 @@ add_summary_stats <- function(plans, map, ...) {
                       e_dvs = dem,
                       pr_dem = dem > 0.5,
                       e_dem = sum(dem > 0.5, na.rm=T),
-                      pbias = -pbias[1], # flip so dem = negative
-                      egap = -egap[1]) # flip so dem = negative
+                      pbias = -pbias[1], # flip so dem = negative (only for old redist versioning)
+                      egap = egap[1])
     })
 
     elect_tb <- elect_tb %>%
