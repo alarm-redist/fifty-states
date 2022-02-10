ed91446daaf9134dc99645fac139bb27ac08e05f
Christopher Kenny <ctk44@cornell.edu>
2022-02-09 21:31:03 -0500

    handle CA islands

---
+++ analyses/CA_cd_2020/02_setup_CA_cd_2020.R
@@ -0,0 +1,25 @@
+###############################################################################
+# Set up redistricting simulation for `CA_cd_2020`
+# © ALARM Project, February 2022
+###############################################################################
+cli_process_start("Creating {.cls redist_map} object for {.pkg CA_cd_2020}")
+
+# TODO any pre-computation (usually not necessary)
+
+map <- redist_map(ca_shp, pop_tol = 0.005,
+    existing_plan = cd_2020, adj = ca_shp$adj)
+
+# TODO any filtering, cores, merging, etc.
+
+# TODO remove if not necessary. Adjust pop_muni as needed to balance county/muni splits
+# make pseudo counties with default settings
+map <- map %>%
+    mutate(pseudo_county = pick_county_muni(map, counties = county, munis = muni,
+                                            pop_muni = get_target(map)))
+
+# Add an analysis name attribute
+attr(map, "analysis_name") <- "CA_2020"
+
+# Output the redist_map object. Do not edit this path.
+write_rds(map, "data-out/CA_2020/CA_cd_2020_map.rds", compress = "xz")
+cli_process_done()
