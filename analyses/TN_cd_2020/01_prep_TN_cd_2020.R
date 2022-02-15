6d6959295c4924b958e21e5d7ee18c41a6c77ffe
Cory McCartan <cory.mccartan@gmail.com>
2022-02-15 15:31:01 -0500

    remove stray TN todo

--- analyses/TN_cd_2020/01_prep_TN_cd_2020.R
+++ analyses/TN_cd_2020/01_prep_TN_cd_2020.R
@@ -25,7 +25,7 @@ path_enacted <- "data-raw/TN/TN_enacted.zip"
 download(url, here(path_enacted))
 unzip(here(path_enacted), exdir = here(dirname(path_enacted), "TN_enacted"))
 file.remove(path_enacted)
-path_enacted <- "data-raw/TN/TN_enacted/TN_CD_Enacted_02060222.shp" # TODO use actual SHP
+path_enacted <- "data-raw/TN/TN_enacted/TN_CD_Enacted_02060222.shp"
 
 cli_process_done()
 
