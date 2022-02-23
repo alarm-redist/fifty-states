4c90c4c2c6d942afc8f4ffbb325ab1033db1f81b
Cory McCartan <cory.mccartan@gmail.com>
2022-02-23 16:58:14 -0500

    PA

---
+++ analyses/PA_cd_2020/03_sim_PA_cd_2020.R
@@ -0,0 +1,41 @@
+###############################################################################
+# Simulate plans for `PA_cd_2020`
+# © ALARM Project, February 2022
+###############################################################################
+
+# Run the simulation -----
+cli_process_start("Running simulations for {.pkg PA_cd_2020}")
+
+constr <- redist_constr(map) %>%
+    add_constr_splits(0.2, coalesce(map$county_muni, "<bg>"))
+
+plans <- redist_smc(map, nsims = 5e3, counties = pseudo_county,
+    constraints = constr)
+
+cli_process_done()
+cli_process_start("Saving {.cls redist_plans} object")
+
+# Output the redist_map object. Do not edit this path.
+write_rds(plans, here("data-out/PA_2020/PA_cd_2020_plans.rds"), compress = "xz")
+cli_process_done()
+
+# Compute summary statistics -----
+cli_process_start("Computing summary statistics for {.pkg PA_cd_2020}")
+
+plans <- add_summary_stats(plans, map)
+
+# Output the summary statistics. Do not edit this path.
+save_summary_stats(plans, "data-out/PA_2020/PA_cd_2020_stats.csv")
+
+cli_process_done()
+
+# Extra validation plots for custom constraints -----
+if (interactive()) {
+    library(ggplot2)
+    library(patchwork)
+
+    # check performance
+    mutate(plans, mvap = 1 - vap_white/total_vap) %>%
+        number_by(mvap) %>%
+        plot(e_dvs, size = 0.01, sort = F)
+}
---
+++ analyses/PA_cd_2020/doc_PA_cd_2020.md
@@ -0,0 +1,29 @@
+# 2020 Pennsylvania Congressional Districts
+
+## Redistricting requirements
+In Pennsylvania, there are few formal districting requirements, but districts must generally:
+
+1. be contiguous
+1. have equal populations
+1. be geographically compact
+1. preserve county and municipality boundaries as much as possible
+
+
+### Interpretation of requirements
+We enforce a maximum population deviation of 0.5%.
+We apply a county/municipality constraint, as described below.
+
+## Data Sources
+Data for Pennsylvania comes from the ALARM Project's [2020 Redistricting Data Files](https://alarm-redist.github.io/posts/2021-08-10-census-2020/).
+
+## Pre-processing Notes
+No manual pre-processing decisions were necessary.
+
+## Simulation Notes
+We sample 5,000 districting plans for Pennsylvania.
+To balance county and municipality splits, we create pseudocounties for use in
+the county constraint. These are counties, outside of Allegheny County,
+Montgomery County, and Philadelphia County. Within Allegheny County, Montgomery
+County, and Philadelphia County, each municipality is its own pseudocounty as
+well. These counties were chosen since they are necessarily split by
+congressional districts.
