1fd710d0f5c0773b5a85ec768741faa7e3a67806
Melissa Wu <55368740+mzwu@users.noreply.github.com>
2022-03-03 19:16:08 -0500

    initial ri sims

---
+++ analyses/RI_cd_2020/03_sim_RI_cd_2020.R
@@ -0,0 +1,50 @@
+###############################################################################
+# Simulate plans for `RI_cd_2020`
+# © ALARM Project, February 2022
+###############################################################################
+
+# Run the simulation -----
+cli_process_start("Running simulations for {.pkg RI_cd_2020}")
+
+cons <- redist_constr(map) %>%
+    add_constr_grp_hinge(
+        strength = 2,
+        group_pop = vap - vap_white,
+        total_pop = vap
+    )
+
+plans <- redist_smc(map, nsims = 5e3,
+    counties = sd_2020,
+    constraints = cons)
+
+cli_process_done()
+cli_process_start("Saving {.cls redist_plans} object")
+
+# Output the redist_map object. Do not edit this path.
+write_rds(plans, here("data-out/RI_2020/RI_cd_2020_plans.rds"), compress = "xz")
+cli_process_done()
+
+# Compute summary statistics -----
+cli_process_start("Computing summary statistics for {.pkg RI_cd_2020}")
+
+plans <- add_summary_stats(plans, map)
+
+# Output the summary statistics. Do not edit this path.
+save_summary_stats(plans, "data-out/RI_2020/RI_cd_2020_stats.csv")
+
+cli_process_done()
+
+# Extra validation plots for custom constraints -----
+if (interactive()) {
+    library(ggplot2)
+    library(patchwork)
+
+    plans %>%
+        mutate(sd_split = county_splits(map, map$sd_2020)) %>%
+        group_by(draw) %>%
+        summarize(sd_split = sd_split[1]) %>%
+        hist(sd_split) +
+        labs(title = "Senate district splits") +
+        theme_bw() +
+        theme(aspect.ratio = 3/4)
+}
