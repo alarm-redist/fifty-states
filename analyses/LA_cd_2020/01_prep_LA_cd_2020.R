c463eb890c301ed48254383bbc459ad52022d7db
Melissa Wu <55368740+mzwu@users.noreply.github.com>
2022-03-12 17:22:52 -0500

    initial LA sims

---
+++ analyses/LA_cd_2020/01_prep_LA_cd_2020.R
@@ -0,0 +1,87 @@
+###############################################################################
+# Download and prepare data for `LA_cd_2020` analysis
+# © ALARM Project, March 2022
+###############################################################################
+
+suppressMessages({
+    library(dplyr)
+    library(readr)
+    library(sf)
+    library(redist)
+    library(geomander)
+    library(cli)
+    library(here)
+    devtools::load_all() # load utilities
+})
+
+# Download necessary files for analysis -----
+cli_process_start("Downloading files for {.pkg LA_cd_2020}")
+
+path_data <- download_redistricting_file("LA", "data-raw/LA")
+
+# download the enacted plan.
+# TODO try to find a download URL at <https://redistricting.lls.edu/state/louisiana/>
+# url <- "https://redistricting.lls.edu/wp-content/uploads/`state`_2020_congress_XXXXX.zip"
+# path_enacted <- "data-raw/LA/LA_enacted.zip"
+# download(url, here(path_enacted))
+# unzip(here(path_enacted), exdir = here(dirname(path_enacted), "LA_enacted"))
+# file.remove(path_enacted)
+# path_enacted <- "data-raw/LA/LA_enacted/XXXXXXX.shp" # TODO use actual SHP
+
+cli_process_done()
+
+# Compile raw data into a final shapefile for analysis -----
+shp_path <- "data-out/LA_2020/shp_vtd.rds"
+perim_path <- "data-out/LA_2020/perim.rds"
+
+if (!file.exists(here(shp_path))) {
+    cli_process_start("Preparing {.strong LA} shapefile")
+    # read in redistricting data
+    la_shp <- read_csv(here(path_data), col_types = cols(GEOID20 = "c")) %>%
+        join_vtd_shapefile() %>%
+        st_transform(EPSG$LA)  %>%
+        rename_with(function(x) gsub("[0-9.]", "", x), starts_with("GEOID"))
+
+    # add municipalities
+    d_muni <- make_from_baf("LA", "INCPLACE_CDP", "VTD")  %>%
+        mutate(GEOID = paste0(censable::match_fips("LA"), vtd)) %>%
+        select(-vtd)
+    d_cd <- make_from_baf("LA", "CD", "VTD")  %>%
+        transmute(GEOID = paste0(censable::match_fips("LA"), vtd),
+            cd_2010 = as.integer(cd))
+    la_shp <- left_join(la_shp, d_muni, by = "GEOID") %>%
+        left_join(d_cd, by = "GEOID") %>%
+        mutate(county_muni = if_else(is.na(muni), county, str_c(county, muni))) %>%
+        relocate(muni, county_muni, cd_2010, .after = county)
+
+    # add the enacted plan
+    # cd_shp <- st_read(here(path_enacted))
+    # la_shp <- la_shp %>%
+    #     mutate(cd_2020 = as.integer(cd_shp$DISTRICT)[
+    #         geo_match(la_shp, cd_shp, method = "area")],
+    #         .after = cd_2010)
+
+    # Create perimeters in case shapes are simplified
+    redist.prep.polsbypopper(shp = la_shp,
+        perim_path = here(perim_path)) %>%
+        invisible()
+
+    # simplifies geometry for faster processing, plotting, and smaller shapefiles
+    if (requireNamespace("rmapshaper", quietly = TRUE)) {
+        la_shp <- rmapshaper::ms_simplify(la_shp, keep = 0.05,
+            keep_shapes = TRUE) %>%
+            suppressWarnings()
+    }
+
+    # create adjacency graph
+    la_shp$adj <- redist.adjacency(la_shp)
+
+    la_shp <- la_shp %>%
+        fix_geo_assignment(muni)
+
+    write_rds(la_shp, here(shp_path), compress = "gz")
+    cli_process_done()
+} else {
+    la_shp <- read_rds(here(shp_path))
+    cli_alert_success("Loaded {.strong LA} shapefile")
+}
---
+++ analyses/LA_cd_2020/doc_LA_cd_2020.md
@@ -0,0 +1,23 @@
+# 2020 Louisiana Congressional Districts
+
+## Redistricting requirements
+In Louisiana, according to [Louisiana Joint Rule No. 21](https://www.legis.la.gov/Legis/Law.aspx?d=1238755) districts must:
+
+1. be contiguous
+2. have equal populations
+3. be geographically compact
+4. preserve parish and municipality boundaries as much as possible
+5. preserve the cores of traditional district alignments
+
+
+### Interpretation of requirements
+We enforce a maximum population deviation of 0.5%. We add a VRA constraint targeting one majority-minority district.
+
+## Data Sources
+Data for Louisiana comes from the ALARM Project's [2020 Redistricting Data Files](https://alarm-redist.github.io/posts/2021-08-10-census-2020/).
+
+## Pre-processing Notes
+No manual pre-processing decisions were necessary.
+
+## Simulation Notes
+We sample 6,000 districting plans for Louisiana and subset to 5,000 which contain at least one majority-minority district. To balance county and municipality splits, we create pseudocounties for use in the county constraint, which leads to fewer municipality splits than using a county constraint.
