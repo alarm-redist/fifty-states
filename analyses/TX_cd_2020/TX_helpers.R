9dde682f3a49a8e3a33c6362884d4dee7a2dacf7
Cory McCartan <cory.mccartan@gmail.com>
2022-03-15 17:13:19 -0400

    reorg partial sims helpers

--- analyses/TX_cd_2020/TX_helpers.R
+++
@@ -1,7 +0,0 @@
-rep_cols <- function(mat, n) {
-    do.call("cbind", lapply(seq_len(ncol(mat)), \(x) rep_col(mat[, x], n)))
-}
-
-rep_col <- function(col, n) {
-    matrix(rep(col, n), ncol = n, byrow = FALSE)
-}
