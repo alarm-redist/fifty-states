6391badc9ce156b023ec43d154e3777205739a16
Cory McCartan <cory.mccartan@gmail.com>
2022-06-15 18:17:21 -0400

    redo AZ

--- analyses/AZ_cd_2020/03_sim_AZ_cd_2020.R
+++ analyses/AZ_cd_2020/03_sim_AZ_cd_2020.R
@@ -6,19 +6,65 @@
 # Run the simulation -----
 cli_process_start("Running simulations for {.pkg AZ_cd_2020}")
 
+Nsim_final <- 8e3 # sims per run in the final map
+
+## Get 1 HVAP outside Maricopa ------
+map_nomaricopa <- filter(map, county != "Maricopa County")
+attr(map_nomaricopa, "pop_bounds") <- attr(map, "pop_bounds")
+
+# ID precincts on the border of Maricopa
+border_idxs <- as_tibble(map) %>%
+    mutate(cluster_edge = ifelse(GEOID %in% map_nomaricopa$GEOID, 0, 1)) %>%
+    geomander::seam_geom(.$adj, ., admin = "cluster_edge", seam = c(0, 1)) %>%
+    suppressWarnings() %>%
+    filter(cluster_edge == 1) %>%
+    pull(GEOID) %>%
+    match(., map_nomaricopa$GEOID)
+
+
+constr <- redist_constr(map_nomaricopa) %>%
+    add_constr_compet(15, ndv, nrv) %>%
+    add_constr_grp_hinge(30, vap_hisp, vap, 0.53) %>%
+    add_constr_grp_hinge(-30, vap_hisp, vap, 0.28) %>%
+    add_constr_grp_inv_hinge(10, vap_hisp, vap, 0.58) %>%
+    add_constr_custom(100, function(plan, distr) {
+        ifelse(any(plan[border_idxs] == 0), 0, 1)
+    }) %>%
+    suppressWarnings()
+
+set.seed(2020)
+plans_nomaricopa <- redist_smc(map_nomaricopa, nsims = 1500, runs = 8L, n_steps = 3,
+    counties = county_muni, constraints = constr,
+    pop_temper = 0.02, seq_alpha = 0.99, verbose = TRUE)
+# diagnostic check on HVAP
+if (FALSE) {
+    plans_nomaricopa %>%
+        mutate(hisp = group_frac(map_nomaricopa, vap_hisp, vap),
+            min = 1 - group_frac(map_nomaricopa, vap_white, vap)) %>%
+        filter(district > 0) %>%
+        plot(hisp, geom = "boxplot")
+}
+
+# subsample 8k to init next stage
+init_m <- matrix(0, nrow = nrow(map), ncol = Nsim_final)
+idxs <- sample(ncol(as.matrix(plans_nomaricopa)), Nsim_final, replace = F)
+init_m[match(map_nomaricopa$GEOID, map$GEOID), ] <- as.matrix(plans_nomaricopa)[, idxs]
+
+
+## Finish simulations ------
+
 constr <- redist_constr(map) %>%
     add_constr_compet(15, ndv, nrv) %>%
-    add_constr_grp_hinge(10, vap_hisp, vap, 0.55) %>%
-    add_constr_grp_hinge(-5, vap_hisp, vap, 0.28) %>%
-    add_constr_grp_hinge(-10, vap_hisp, vap, 0.35) %>%
-    add_constr_grp_inv_hinge(8, vap_hisp, vap, 0.6) %>%
-    add_constr_grp_hinge(4, vap_hisp, vap, 0.15) %>%
+    add_constr_grp_hinge(20, vap_hisp, vap, 0.53) %>%
+    add_constr_grp_hinge(-22, vap_hisp, vap, 0.35) %>%
+    add_constr_grp_inv_hinge(10, vap_hisp, vap, 0.58) %>%
     suppressWarnings()
 
 set.seed(2020)
 
-plans <- redist_smc(map, nsims = 15e3, runs = 4L, counties = pseudo_county,
-    constraints = constr, pop_temper = 0.04, seq_alpha = 0.95) %>%
+plans <- redist_smc(map, nsims = 8e3, runs = 4L, counties = pseudo_county,
+    constraints = constr, init_particles = init_m, pop_temper = 0.03,
+    seq_alpha = 0.9, verbose = TRUE) %>%
     group_by(chain) %>%
     filter(as.integer(draw) < min(as.integer(draw)) + 1250) %>% # thin samples
     ungroup()
@@ -73,12 +119,12 @@ if (interactive()) {
         redist.plot.distr_qtys(ndshare, sort = "none", geom = "boxplot") +
         labs(x = "Districts, ordered by HVAP", y = "Average Democratic share")
 
-    redist.plot.distr_qtys(plans, vap_hisp / total_vap,
-                           color_thresh = NULL,
-                           color = ifelse(subset_sampled(plans)$ndv > subset_sampled(plans)$nrv, '#3D77BB', '#B25D4C'),
-                           size = 0.1) +
-        scale_y_continuous('Percent Hispanic by VAP') +
-        labs(title = 'Approximate Performance') +
-        scale_color_manual(values = c(cd_2020_prop = 'black'))
+    redist.plot.distr_qtys(plans, vap_hisp/total_vap,
+        color_thresh = NULL,
+        color = ifelse(subset_sampled(plans)$ndv > subset_sampled(plans)$nrv, "#3D77BB", "#B25D4C"),
+        size = 0.1) +
+        scale_y_continuous("Percent Hispanic by VAP") +
+        labs(title = "Approximate Performance") +
+        scale_color_manual(values = c(cd_2020_prop = "black"))
 
 }
