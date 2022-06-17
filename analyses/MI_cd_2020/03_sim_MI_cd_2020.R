a809af21e65df2f513e6c1b6c743d32ac3292548
Cory McCartan <cory.mccartan@gmail.com>
2022-06-16 20:52:06 -0400

    typo

--- analyses/MI_cd_2020/03_sim_MI_cd_2020.R
+++ analyses/MI_cd_2020/03_sim_MI_cd_2020.R
@@ -60,5 +60,5 @@ if (FALSE) {
         size = 0.1) +
         scale_y_continuous("Percent Minority by VAP") +
         labs(title = "Approximate Performance") +
-        scale_color_manual(values = c(cd_2020_prop = "black"))
+        scale_color_manual(values = c(cd_2020 = "black"))
 }
