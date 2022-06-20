f103441f2cf7a6b94164340052871151c868c62c
Cory McCartan <CoryMcCartan@users.noreply.github.com>
2022-06-20 15:21:48 -0700

    spacing

--- analyses/MO_cd_2020/03_sim_MO_cd_2020.R
+++ analyses/MO_cd_2020/03_sim_MO_cd_2020.R
@@ -10,14 +10,16 @@ constr <- redist_constr(map) %>%
     add_constr_grp_hinge(6, vap_black, vap, 0.4) %>%
     add_constr_grp_hinge(-3, vap_black, vap, 0.25) %>%
     add_constr_grp_hinge(-3, vap_black, vap, 0.08)
+
 set.seed(2020)
+
 plans <- redist_smc(map, nsims = 5e3, runs = 2L, ncores = 8, seq_alpha = 0.95,
-    counties = county, constraints = constr)
-plans <- match_numbers(plans, "cd_2020")
-plans <- plans %>%
+    counties = county, constraints = constr) %>%
     group_by(chain) %>%
     filter(as.integer(draw) < min(as.integer(draw)) + 2500) %>% # thin samples
     ungroup()
+
+plans <- match_numbers(plans, "cd_2020")
 
 cli_process_done()
 cli_process_start("Saving {.cls redist_plans} object")
