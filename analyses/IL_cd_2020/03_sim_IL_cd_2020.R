177d38e314e2efa72039241ce623149c4330f5de
Kevin Wang <kpwang@college.harvard.edu>
2022-06-25 15:52:04 -0400

    Final sims

--- analyses/IL_cd_2020/03_sim_IL_cd_2020.R
+++ analyses/IL_cd_2020/03_sim_IL_cd_2020.R
@@ -8,10 +8,18 @@ cli_process_start("Running simulations for {.pkg IL_cd_2020}")
 
 constr <- redist_constr(map) %>%
     add_constr_grp_hinge(20, vap_black, vap, tgts_group = 0.55) %>%
-    add_constr_grp_hinge(20, vap_hisp, vap, tgts_group = 0.55)
+    add_constr_grp_hinge(-20, vap_black, vap, tgts_group = 0.45) %>%
+    add_constr_grp_inv_hinge(10, vap_black, vap, tgts_group = 0.60) %>%
+    add_constr_grp_hinge(20, vap_hisp, vap, tgts_group = 0.55) %>%
+    add_constr_grp_hinge(-20, vap_hisp, vap, tgts_group = 0.44)
 
-plans <- redist_smc(map, nsims = 5e3, counties = pseudo_county,
-    constraints = constr)
+set.seed(2020)
+plans <- redist_smc(map, nsims = 3e4, runs = 2L, counties = pseudo_county,
+    pop_temper = 0.01) %>%
+    group_by(chain) %>%
+    filter(as.integer(draw) < min(as.integer(draw)) + 2500) %>% # thin samples
+    ungroup()
+plans <- match_numbers(plans, "cd_2020")
 
 cli_process_done()
 cli_process_start("Saving {.cls redist_plans} object")
@@ -24,6 +32,8 @@ cli_process_done()
 cli_process_start("Computing summary statistics for {.pkg IL_cd_2020}")
 
 plans <- add_summary_stats(plans, map)
+
+summary(plans)
 
 # Output the summary statistics. Do not edit this path.
 save_summary_stats(plans, "data-out/IL_2020/IL_cd_2020_stats.csv")
@@ -47,19 +57,19 @@ if (interactive()) {
         ggredist::theme_r21()
     ggsave("figs/performance.pdf", height = 7, width = 7)
 
-    fake_map <- map
-    fake_map$Enacted <- get_plans_matrix(plans)[,1000]
-    fake_map %>%
-        group_by(Enacted) %>%
-        summarize(geometry = st_union(geometry),
-                  hisp = sum(vap_hisp) / sum(vap)) %>%
-        ggplot() +
-        geom_sf(aes(fill = hisp), lwd = 0.25) +
-        # geom_sf(data = ctys, fill = NA, lwd = 2.5, alpha = 0.3, color = "grey") +
-        # geom_sf(data = dists, fill = NA, lwd = 1) +
-        geom_sf(data = fake_map, fill = NA, lwd = 0) +
-        theme_void()
-    # coord_sf(xlim=c(bbox$xmin, bbox$xmax),
-    #          ylim=c(bbox$ymin, bbox$ymax))
-    ggsave("figs/il_hisp.pdf", height = 10, width = 10)
+    # fake_map <- map
+    # fake_map$Enacted <- get_plans_matrix(plans)[,1000]
+    # fake_map %>%
+    #     group_by(Enacted) %>%
+    #     summarize(geometry = st_union(geometry),
+    #               hisp = sum(vap_hisp) / sum(vap)) %>%
+    #     ggplot() +
+    #     geom_sf(aes(fill = hisp), lwd = 0.25) +
+    #     # geom_sf(data = ctys, fill = NA, lwd = 2.5, alpha = 0.3, color = "grey") +
+    #     # geom_sf(data = dists, fill = NA, lwd = 1) +
+    #     geom_sf(data = fake_map, fill = NA, lwd = 0) +
+    #     theme_void()
+    # # coord_sf(xlim=c(bbox$xmin, bbox$xmax),
+    # #          ylim=c(bbox$ymin, bbox$ymax))
+    # ggsave("figs/il_hisp.pdf", height = 10, width = 10)
 }
