4d11b33b0272f0b03459d0c3f5e67b86fa17b641
Aneetej Arora <aneetej.arora1@gmail.com>
2022-08-03 17:49:01 -0400

    Update runs to reach convergence

--- analyses/WA_cd_2010/03_sim_WA_cd_2010.R
+++ analyses/WA_cd_2010/03_sim_WA_cd_2010.R
@@ -7,14 +7,17 @@
 cli_process_start("Running simulations for {.pkg WA_cd_2010}")
 
 constr <- redist_constr(map) %>%
-    add_constr_grp_hinge(10.0, vap - vap_white, vap, c(0.52, 0.35, 0.25)) %>%
+    add_constr_grp_hinge(8.0, vap - vap_white, vap, c(0.52, 0.35, 0.25)) %>%
     add_constr_grp_hinge(-8.0, vap - vap_white, vap, c(0.35, 0.25))
 
 
 
 set.seed(2010)
-plans <- redist_smc(map, nsims = 5e3, counties = pseudo_county, constraints = constr) %>% match_numbers("cd_2010")
+plans <- redist_smc(map, nsims = 7000, counties = pseudo_county, constraints = constr, runs = 2L) %>% match_numbers("cd_2010") %>% group_by(chain) %>%
+  filter(as.integer(draw) < min(as.integer(draw)) + 2500) %>% # thin samples
+  ungroup()
 
+plans <- match_numbers(plans, map$cd_2010)
 
 cli_process_done()
 cli_process_start("Saving {.cls redist_plans} object")
