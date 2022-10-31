08b5acf9ac3e16c58849915952790d2c7b6e37a1
taransamarth <tns12@icloud.com>
2022-10-31 17:14:52 -0400

    60k runs

--- analyses/NY_cd_2010/03_sim_NY_cd_2010.R
+++ analyses/NY_cd_2010/03_sim_NY_cd_2010.R
@@ -8,12 +8,14 @@ cli_process_start("Running simulations for {.pkg NY_cd_2010}")
 
 set.seed(2010)
 plans <- redist_smc(map,
-    nsims = 2e4,
+    nsims = 3e4,
     seq_alpha = .95,
     runs = 2L,
     counties = pseudo_county, verbose = TRUE,
     pop_temper = .001, ncores = 15) %>%
-    match_numbers("cd_2010") %>%
+    match_numbers("cd_2010")
+
+thinned_plans <- plans %>%
     group_by(chain) %>%
     filter(as.integer(draw) < min(as.integer(draw)) + 2500) %>%
     ungroup()
@@ -29,6 +31,7 @@ cli_process_done()
 cli_process_start("Computing summary statistics for {.pkg NY_cd_2010}")
 
 plans <- add_summary_stats(plans, map)
+thinned_plans <- add_summary_stats(thinned_plans, map)
 
 # Output the summary statistics. Do not edit this path.
 save_summary_stats(plans, "data-out/NY_2010/NY_cd_2010_stats.csv")
