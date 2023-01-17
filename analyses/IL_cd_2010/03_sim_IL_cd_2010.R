a75c02c4c1c89c9b9a23aa7735966d1912082256
Jennifer Gao <jennifergao@Jennifers-MacBook-Pro.local>
2023-01-17 14:23:54 -0500

    problem with enacted plan

---
+++ analyses/IL_cd_2010/03_sim_IL_cd_2010.R
@@ -0,0 +1,72 @@
+###############################################################################
+# Simulate plans for `IL_cd_2010`
+# © ALARM Project, January 2023
+###############################################################################
+
+# Run the simulation -----
+cli_process_start("Running simulations for {.pkg IL_cd_2010}")
+
+# TODO any pre-computation (VRA targets, etc.)
+
+# TODO customize as needed. Recommendations:
+#  - For many districts / tighter population tolerances, try setting
+#  `pop_temper=0.01` and nudging upward from there. Monitor the output for
+#  efficiency!
+#  - Monitor the output (i.e. leave `verbose=TRUE`) to ensure things aren't breaking
+#  - Don't change the number of simulations unless you have a good reason
+#  - If the sampler freezes, try turning off the county split constraint to see
+#  if that's the problem.
+#  - Ask for help!
+constr <- redist_constr(map) %>%
+    add_constr_grp_hinge(20, vap_black, vap, tgts_group = 0.55) %>%
+    add_constr_grp_hinge(-20, vap_black, vap, tgts_group = 0.45) %>%
+    add_constr_grp_inv_hinge(10, vap_black, vap, tgts_group = 0.60) %>%
+    add_constr_grp_hinge(20, vap_hisp, vap, tgts_group = 0.55) %>%
+    add_constr_grp_hinge(-20, vap_hisp, vap, tgts_group = 0.44)
+
+set.seed(2010)
+plans <- redist_smc(map, nsims = 3e4, runs = 2L, counties = pseudo_county)#,
+                    #pop_temper = 0.01) #%>%
+    #group_by(chain) %>%
+    #filter(as.integer(draw) < min(as.integer(draw)) + 2500) %>% # thin samples
+    #ungroup()
+plans <- match_numbers(plans, "cd_2010")
+
+cli_process_done()
+cli_process_start("Saving {.cls redist_plans} object")
+
+# TODO add any reference plans that aren't already included
+
+# Output the redist_map object. Do not edit this path.
+write_rds(plans, here("data-out/IL_2010/IL_cd_2010_plans.rds"), compress = "xz")
+cli_process_done()
+
+# Compute summary statistics -----
+cli_process_start("Computing summary statistics for {.pkg IL_cd_2010}")
+
+plans <- add_summary_stats(plans, map)
+
+# Output the summary statistics. Do not edit this path.
+save_summary_stats(plans, "data-out/IL_2010/IL_cd_2010_stats.csv")
+
+cli_process_done()
+
+# Extra validation plots for custom constraints -----
+# TODO remove this section if no custom constraints
+if (interactive()) {
+    library(ggplot2)
+    library(patchwork)
+
+    validate_analysis(plans, map)
+
+    redist.plot.distr_qtys(plans, vap_black/total_vap,
+                           color_thresh = NULL,
+                           color = ifelse(subset_sampled(plans)$ndv > subset_sampled(plans)$nrv, "#3D77BB", "#B25D4C"),
+                           size = 0.5, alpha = 0.5) +
+        scale_y_continuous("Percent Black by VAP") +
+        labs(title = "Approximate Performance") +
+        scale_color_manual(values = c(cd_2010_prop = "black")) +
+        ggredist::theme_r21()
+    ggsave("figs/performance.pdf", height = 7, width = 7)
+
+}
---
+++ analyses/IL_cd_2010/doc_IL_cd_2010.md
@@ -0,0 +1,21 @@
+# 2010 Illinois Congressional Districts
+
+## Redistricting requirements
+In Illinois, districts must:
+
+1. have equal populations
+2. comply with VRA section 2
+
+
+### Algorithmic Constraints
+We enforce a maximum population deviation of 0.5%.
+
+## Data Sources
+Data for Illinois comes from the ALARM Project's [2010 Redistricting Data Files](https://alarm-redist.github.io/posts/2021-08-10-census-2020/).
+
+## Pre-processing Notes
+No manual pre-processing decisions were necessary.
+
+## Simulation Notes
+We sample 5,000 districting plans for Illinois.
+No special techniques were needed to produce the sample.
