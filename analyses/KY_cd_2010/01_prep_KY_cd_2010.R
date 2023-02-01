9a43d868a00a85976432f9f601bfe3cfe2eaa83d
Kevin Wang <kpwang@college.harvard.edu>
2023-02-01 15:51:02 -0500

    download block files

--- analyses/KY_cd_2010/01_prep_KY_cd_2010.R
+++ analyses/KY_cd_2010/01_prep_KY_cd_2010.R
@@ -17,7 +17,7 @@ suppressMessages({
 # Download necessary files for analysis -----
 cli_process_start("Downloading files for {.pkg KY_cd_2010}")
 
-path_data <- download_redistricting_file("KY", "data-raw/KY", year = 2010)
+path_data <- download_redistricting_file("KY", "data-raw/KY", year = 2010, type = "block")
 
 # download the enacted plan.
 # TODO try to find a download URL at <https://redistricting.lls.edu/state/kentucky/>
@@ -26,7 +26,7 @@ path_enacted <- "data-raw/KY/KY_enacted.zip"
 download(url, here(path_enacted))
 unzip(here(path_enacted), exdir = here(dirname(path_enacted), "KY_enacted"))
 file.remove(path_enacted)
-path_enacted <- "data-raw/KY/KY_enacted/CH302C02.shp" # TODO use actual SHP
+path_enacted <- "data-raw/KY/KY_enacted/CH302C02.shp"
 
 cli_process_done()
 
@@ -37,20 +37,31 @@ perim_path <- "data-out/KY_2010/perim.rds"
 if (!file.exists(here(shp_path))) {
     cli_process_start("Preparing {.strong KY} shapefile")
     # read in redistricting data
-    ky_shp <- read_csv(here(path_data)) %>%
-        join_vtd_shapefile(year = 2010) %>%
+    ky_shp <- read_csv(here(path_data), col_types = cols(GEOID = "c")) %>%
+        left_join(y = tigris::blocks("KY", year = 2010), by = c("GEOID" = "GEOID10")) %>%
+        st_as_sf() %>%
         st_transform(EPSG$KY)  %>%
         rename_with(function(x) gsub("[0-9.]", "", x), starts_with("GEOID"))
 
     # add municipalities
-    d_muni <- make_from_baf("KY", "INCPLACE_CDP", "VTD", year = 2010)  %>%
+    d_muni <- make_from_baf("KY", "INCPLACE_CDP", "VTD", year = 2010) %>%
         mutate(GEOID = paste0(censable::match_fips("KY"), vtd)) %>%
         select(-vtd)
-    d_cd <- make_from_baf("KY", "CD", "VTD", year = 2010)  %>%
-        transmute(GEOID = paste0(censable::match_fips("KY"), vtd),
-                  cd_2000 = as.integer(cd))
+    # d_cd <- make_from_baf("KY", "CD", "VTD", year = 2010)  %>%
+    #     transmute(GEOID = paste0(censable::match_fips("KY"), vtd),
+    #               cd_2000 = as.integer(cd))
+    # ky_shp <- left_join(ky_shp, d_muni, by = "GEOID") %>%
+    #     left_join(d_cd, by="GEOID") %>%
+    #     mutate(county_muni = if_else(is.na(muni), county, str_c(county, muni))) %>%
+    #     relocate(muni, county_muni, cd_2000, .after = county)
+
+    d_muni <- PL94171::pl_get_baf("KY", "INCPLACE_CDP")[[1]] %>%
+        rename(GEOID = BLOCKID, muni = PLACEFP)
+    d_cd <- PL94171::pl_get_baf("KY", "CD")[[1]]  %>%
+        transmute(GEOID = BLOCKID,
+                  cd_2000 = as.integer(DISTRICT))
     ky_shp <- left_join(ky_shp, d_muni, by = "GEOID") %>%
-        left_join(d_cd, by="GEOID") %>%
+        left_join(d_cd, by = "GEOID") %>%
         mutate(county_muni = if_else(is.na(muni), county, str_c(county, muni))) %>%
         relocate(muni, county_muni, cd_2000, .after = county)
 
@@ -69,7 +80,6 @@ if (!file.exists(here(shp_path))) {
         invisible()
 
     # simplifies geometry for faster processing, plotting, and smaller shapefiles
-    # TODO feel free to delete if this dependency isn't available
     if (requireNamespace("rmapshaper", quietly = TRUE)) {
         ky_shp <- rmapshaper::ms_simplify(ky_shp, keep = 0.05,
                                                  keep_shapes = TRUE) %>%
@@ -78,8 +88,6 @@ if (!file.exists(here(shp_path))) {
 
     # create adjacency graph
     ky_shp$adj <- redist.adjacency(ky_shp)
-
-    # TODO any custom adjacency graph edits here
 
     ky_shp <- ky_shp %>%
         fix_geo_assignment(muni)
