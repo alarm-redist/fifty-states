b025529f91155af2c5eaad5841f7469a5e4c7da2
Christopher Kenny <ctk44@cornell.edu>
2023-02-04 20:15:27 -0500

    tidier is in the eye of rholder

--- analyses/KS_cd_2010/02_setup_KS_cd_2010.R
+++ analyses/KS_cd_2010/02_setup_KS_cd_2010.R
@@ -8,16 +8,16 @@ map <- redist_map(ks_shp, pop_tol = 0.005,
     existing_plan = cd_2010, adj = ks_shp$adj)
 
 map <- map %>%
-    mutate(core_id = redist.identify.cores(map$adj, map$cd_2000, boundary = 2),
+    mutate(
+        core_id = redist.identify.cores(map$adj, map$cd_2000, boundary = 2),
         core_id_lump = forcats::fct_lump_n(as.character(core_id), max(cd_2000) + 1), # lump all non-core precincts in to "Other"
         core_id = if_else(as.logical(is_county_split(core_id_lump, county)), # break off counties which are split by core border
             str_c(county, "_", core_id),
-            as.character(core_id))) %>%
+            as.character(core_id)),
+        state = "KS"
+    ) %>%
     select(-core_id_lump)
 map_m <- merge_by(map, core_id)
-
-map <- map %>%
-    mutate(state = "KS")
 
 # Add an analysis name attribute
 attr(map, "analysis_name") <- "KS_2010"
