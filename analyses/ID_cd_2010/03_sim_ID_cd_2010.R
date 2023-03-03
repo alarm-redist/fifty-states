13df812d6e151dcdd9532cb7c97071d7aba44ac6
Emma Ebowe <42868909+emmaebowe@users.noreply.github.com>
2023-03-03 14:11:47 -0500

    initial commit

---
+++ analyses/ID_cd_2010/03_sim_ID_cd_2010.R
@@ -0,0 +1,45 @@
+###############################################################################
+# Simulate plans for `ID_cd_2010`
+# © ALARM Project, March 2023
+###############################################################################
+
+# Run the simulation -----
+cli_process_start("Running simulations for {.pkg ID_cd_2010}")
+
+set.seed(2010)
+plans <- redist_smc(map,
+                    nsims = 15e3,
+                    runs = 2L,
+                    counties = county_muni)
+
+plans <- match_numbers(plans, "cd_2010")
+
+cli_process_done()
+cli_process_start("Saving {.cls redist_plans} object")
+
+
+# Output the redist_map object. Do not edit this path.
+write_rds(plans, here("data-out/ID_2010/ID_cd_2010_plans.rds"), compress = "xz")
+cli_process_done()
+
+# Compute summary statistics -----
+cli_process_start("Computing summary statistics for {.pkg ID_cd_2010}")
+
+plans <- add_summary_stats(plans, map)
+
+# Output the summary statistics. Do not edit this path.
+save_summary_stats(plans, "data-out/ID_2010/ID_cd_2010_stats.csv")
+
+cli_process_done()
+
+# Extra validation plots for custom constraints -----
+if (interactive()) {
+    library(ggplot2)
+    library(patchwork)
+
+    redist.plot.adj(id_shp, id_shp$adj, plan = redist.sink.plan(id_shp$county)) +
+        geom_sf(data = tigris::primary_secondary_roads("ID"), color = "red") +
+        theme_void() +
+        guides(fill = "none")
+}
+
---
+++ analyses/ID_cd_2010/doc_ID_cd_2010.md
@@ -0,0 +1,25 @@
+# 2010 Idaho Congressional Districts
+
+## Redistricting requirements
+[In Idaho, districts must:](https://legislature.idaho.gov/statutesrules/idstat/Title72/T72CH15/SECT72-1506/)
+
+1. be contiguous (72-1506(6)).
+2. have equal populations (72-1506(3)).
+3. be geographically compact (72-1506(4), 72-1506(5)).
+4. preserve county and municipality boundaries as much as possible (72-1506(2)).
+5. not be drawn to favor party or incumbents (72-1506(8)).
+6. connect counties based on highways (72-1506(9)).
+
+### Algorithmic Constraints
+We enforce a maximum population deviation of 0.5%.
+
+## Data Sources
+Data for Idaho comes from the [Loyala Law School (LLS) All About Redistricting](https://redistricting.lls.edu/state/idaho/?cycle=2010&level=Congress&startdate=2011-10-17) project
+
+## Pre-processing Notes
+No manual pre-processing decisions were necessary.
+
+## Simulation Notes
+We sample 15,000 districting plans for Idaho, across 2 independent runs of the SMC algorithm.
+We sample using the standard algorithmic municipality constraint.
+No special techniques were needed to produce the sample.
