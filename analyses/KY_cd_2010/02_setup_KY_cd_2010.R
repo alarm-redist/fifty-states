1a759bdf9788ca1a6bdc6e8bad39714facb64d43
Kevin Wang <kpwang@college.harvard.edu>
2023-05-08 17:20:54 -0400

    fixed state col to abbreviation

--- analyses/KY_cd_2010/02_setup_KY_cd_2010.R
+++ analyses/KY_cd_2010/02_setup_KY_cd_2010.R
@@ -10,7 +10,8 @@ map <- redist_map(ky_shp, pop_tol = 0.005,
 # make pseudo counties with default settings
 map <- map %>%
     mutate(pseudo_county = pick_county_muni(map, counties = county, munis = muni,
-        pop_muni = get_target(map)))
+        pop_muni = get_target(map)),
+        state = "KY")
 
 # Add an analysis name attribute
 attr(map, "analysis_name") <- "KY_2010"
