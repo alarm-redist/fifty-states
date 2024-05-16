30d2845db746c62e34c38a53090345500d9d5aac
Christopher Kenny <ctk44@cornell.edu>
2024-05-15 21:45:06 -0400

    fix cores

--- analyses/KS_cd_2020/02_setup_KS_cd_2020.R
+++ analyses/KS_cd_2020/02_setup_KS_cd_2020.R
@@ -8,11 +8,14 @@ map <- redist_map(ks_shp, pop_tol = 0.005,
     existing_plan = cd_2020, adj = ks_shp$adj)
 
 map <- map %>%
-    mutate(core_id = redist.identify.cores(map$adj, map$cd_2010, boundary = 2),
+    mutate(
+        core_id = redist.identify.cores(map$adj, map$cd_2010, boundary = 2),
         core_id_lump = forcats::fct_lump_n(as.character(core_id), max(cd_2010)), # lump all non-core precincts in to "Other"
         core_id = if_else(is_county_split(core_id_lump, county), # break off counties which are split by core border
-            str_c(county, "_", core_id),
-            as.character(core_id))) %>%
+                           str_c(county, "_", core_id),
+                           as.character(core_id)),
+        core_id = paste0(core_id, check_contiguity(map$adj, core_id)$component)
+    ) %>%
     select(-core_id_lump)
 map_m <- merge_by(map, core_id)
 
