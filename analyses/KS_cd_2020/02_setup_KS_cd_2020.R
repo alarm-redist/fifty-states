024a09e6f3cedb7c3e6329eae2682d69f9845883
Christopher Kenny <ctk44@cornell.edu>
2024-05-15 21:46:56 -0400

    Update 02_setup_KS_cd_2020.R

--- analyses/KS_cd_2020/02_setup_KS_cd_2020.R
+++ analyses/KS_cd_2020/02_setup_KS_cd_2020.R
@@ -12,8 +12,8 @@ map <- map %>%
         core_id = redist.identify.cores(map$adj, map$cd_2010, boundary = 2),
         core_id_lump = forcats::fct_lump_n(as.character(core_id), max(cd_2010)), # lump all non-core precincts in to "Other"
         core_id = if_else(is_county_split(core_id_lump, county), # break off counties which are split by core border
-                           str_c(county, "_", core_id),
-                           as.character(core_id)),
+            str_c(county, "_", core_id),
+            as.character(core_id)),
         core_id = paste0(core_id, check_contiguity(map$adj, core_id)$component)
     ) %>%
     select(-core_id_lump)
