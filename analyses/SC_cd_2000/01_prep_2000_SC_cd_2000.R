1734ab4a49b5f07d2ee8bb7de0c70080a76aa10b
Brian Zhou <87774511+brianlzhou@users.noreply.github.com>
2025-07-31 02:02:19 -0700

    init

---
+++ analyses/SC_cd_2000/01_prep_2000_SC_cd_2000.R
@@ -0,0 +1,69 @@
+###############################################################################
+# Download and prepare data for `SC_cd_2000` analysis
+# © ALARM Project, July 2025
+###############################################################################
+
+suppressMessages({
+    library(dplyr)
+    library(readr)
+    library(sf)
+    library(redist)
+    library(geomander)
+    library(baf)
+    library(cli)
+    library(here)
+    devtools::load_all() # load utilities
+})
+
+# Download necessary files for analysis -----
+cli_process_start("Downloading files for {.pkg SC_cd_2000}")
+
+path_data <- download_redistricting_file("SC", "data-raw/SC", year = 2000, overwrite = TRUE)
+
+cli_process_done()
+
+# Compile raw data into a final shapefile for analysis -----
+shp_path <- "data-out/SC_2000/shp_vtd.rds"
+perim_path <- "data-out/SC_2000/perim.rds"
+
+if (!file.exists(here(shp_path))) {
+    cli_process_start("Preparing {.strong SC} shapefile")
+    # read in redistricting data
+    sc_shp <- read_csv(here(path_data), col_types = cols(GEOID = "c")) %>%
+        join_vtd_shapefile(year = 2000) %>%
+        st_transform(EPSG$SC)
+
+    sc_shp <- sc_shp %>%
+        rename(muni = place) %>%
+        mutate(county_muni = if_else(is.na(muni), county, str_c(county, muni))) %>%
+        relocate(muni, county_muni, cd_1990, .after = county)
+
+    # TODO any additional columns or data you want to add should go here
+
+    # Create perimeters in case shapes are simplified
+    redistmetrics::prep_perims(shp = sc_shp,
+                               perim_path = here(perim_path)) %>%
+        invisible()
+
+    # simplifies geometry for faster processing, plotting, and smaller shapefiles
+    # TODO feel free to delete if this dependency isn't available
+    if (requireNamespace("rmapshaper", quietly = TRUE)) {
+        sc_shp <- rmapshaper::ms_simplify(sc_shp, keep = 0.05,
+                                                 keep_shapes = TRUE) %>%
+            suppressWarnings()
+    }
+
+    # create adjacency graph
+    sc_shp$adj <- redist.adjacency(sc_shp)
+
+    # TODO any custom adjacency graph edits here
+
+    sc_shp <- sc_shp %>%
+        fix_geo_assignment(muni)
+
+    write_rds(sc_shp, here(shp_path), compress = "gz")
+    cli_process_done()
+} else {
+    sc_shp <- read_rds(here(shp_path))
+    cli_alert_success("Loaded {.strong SC} shapefile")
+}
