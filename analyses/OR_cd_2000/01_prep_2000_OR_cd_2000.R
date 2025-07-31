253691db0d88e75909540fd806dec4cefb2f78ad
Brian Zhou <87774511+brianlzhou@users.noreply.github.com>
2025-07-31 07:42:56 -0700

    init

---
+++ analyses/OR_cd_2000/01_prep_2000_OR_cd_2000.R
@@ -0,0 +1,69 @@
+###############################################################################
+# Download and prepare data for `OR_cd_2000` analysis
+# © ALARM Project, July 2025
+###############################################################################
+
+suppressMessages({
+    library(dplyr)
+    library(readr)
+    library(sf)
+    library(redist)
+    library(geomander)
+    library(baf)
+    library(cli)
+    library(here)
+    devtools::load_all() # load utilities
+})
+
+# Download necessary files for analysis -----
+cli_process_start("Downloading files for {.pkg OR_cd_2000}")
+
+path_data <- download_redistricting_file("OR", "data-raw/OR", year = 2000, overwrite = TRUE)
+
+cli_process_done()
+
+# Compile raw data into a final shapefile for analysis -----
+shp_path <- "data-out/OR_2000/shp_vtd.rds"
+perim_path <- "data-out/OR_2000/perim.rds"
+
+if (!file.exists(here(shp_path))) {
+    cli_process_start("Preparing {.strong OR} shapefile")
+    # read in redistricting data
+    or_shp <- read_csv(here(path_data), col_types = cols(GEOID = "c")) %>%
+        join_vtd_shapefile(year = 2000) %>%
+        st_transform(EPSG$OR)
+
+    or_shp <- or_shp %>%
+        rename(muni = place) %>%
+        mutate(county_muni = if_else(is.na(muni), county, str_c(county, muni))) %>%
+        relocate(muni, county_muni, cd_1990, .after = county)
+
+    # TODO any additional columns or data you want to add should go here
+
+    # Create perimeters in case shapes are simplified
+    redistmetrics::prep_perims(shp = or_shp,
+                               perim_path = here(perim_path)) %>%
+        invisible()
+
+    # simplifies geometry for faster processing, plotting, and smaller shapefiles
+    # TODO feel free to delete if this dependency isn't available
+    if (requireNamespace("rmapshaper", quietly = TRUE)) {
+        or_shp <- rmapshaper::ms_simplify(or_shp, keep = 0.05,
+                                                 keep_shapes = TRUE) %>%
+            suppressWarnings()
+    }
+
+    # create adjacency graph
+    or_shp$adj <- redist.adjacency(or_shp)
+
+    # TODO any custom adjacency graph edits here
+
+    or_shp <- or_shp %>%
+        fix_geo_assignment(muni)
+
+    write_rds(or_shp, here(shp_path), compress = "gz")
+    cli_process_done()
+} else {
+    or_shp <- read_rds(here(shp_path))
+    cli_alert_success("Loaded {.strong OR} shapefile")
+}
---
+++ analyses/OR_cd_2000/03_sim_OR_cd_2000.R
@@ -0,0 +1,170 @@
+###############################################################################
+# Simulate plans for `OR_cd_2000`
+# © ALARM Project, July 2025
+###############################################################################
+
+# Run the simulation -----
+cli_process_start("Running simulations for {.pkg OR_cd_2000}")
+
+# TODO any pre-computation (VRA targets, etc.)
+
+# TODO customize as needed. Recommendations:
+#  - For many districts / tighter population tolerances, try setting
+#  `pop_temper=0.01` and nudging upward from there. Monitor the output for
+#  efficiency!
+#  - Monitor the output (i.e. leave `verbose=TRUE`) to ensure things aren't breaking
+#  - Don't change the number of simulations unless you have a good reason
+#  - If the sampler freezes, try turning off the county split constraint to see
+#  if that's the problem.
+#  - Ask for help!
+set.seed(2000)
+plans <- redist_smc(map, nsims = 2e3, runs = 5, counties = county)
+# IF CORES OR OTHER UNITS HAVE BEEN MERGED:
+# make sure to call `pullback()` on this plans object!
+
+plans <- plans %>%
+    group_by(chain) %>%
+    filter(as.integer(draw) < min(as.integer(draw)) + 1000) %>% # thin samples
+    ungroup()
+plans <- match_numbers(plans, "cd_2000")
+
+cli_process_done()
+cli_process_start("Saving {.cls redist_plans} object")
+
+# TODO add any reference plans that aren't already included
+
+# Output the redist_map object. Do not edit this path.
+write_rds(plans, here("data-out/OR_2000/OR_cd_2000_plans.rds"), compress = "xz")
+cli_process_done()
+
+# Compute summary statistics -----
+cli_process_start("Computing summary statistics for {.pkg OR_cd_2000}")
+
+plans <- add_summary_stats(plans, map)
+
+# Output the summary statistics. Do not edit this path.
+save_summary_stats(plans, "data-out/OR_2000/OR_cd_2000_stats.csv")
+
+cli_process_done()
+
+# Extra validation plots for custom constraints -----
+# TODO remove this section if no custom constraints
+if (interactive()) {
+    library(ggplot2)
+    library(patchwork)
+
+    validate_analysis(plans, map)
+    summary(plans)
+
+    # Black VAP Performance Plot
+    redist.plot.distr_qtys(plans, vap_black/total_vap,
+                           color_thresh = NULL,
+                           color = ifelse(subset_sampled(plans)$ndv > subset_sampled(plans)$nrv, "#3D77BB", "#B25D4C"),
+                           size = 0.5, alpha = 0.5) +
+      scale_y_continuous("Percent Black by VAP") +
+      labs(title = "Tennessee Proposed Plan versus Simulations") +
+      scale_color_manual(values = c(cd_2020_prop = "black")) +
+      theme_bw()
+
+    # Minority VAP Performance Plot
+    redist.plot.distr_qtys(plans, (total_vap - vap_white)/total_vap,
+                           color_thresh = NULL,
+                           color = ifelse(subset_sampled(plans)$ndv > subset_sampled(plans)$nrv, "#3D77BB", "#B25D4C"),
+                           size = 0.5, alpha = 0.5) +
+      scale_y_continuous("Minority Percentage by VAP") +
+      labs(title = "Tennessee Proposed Plan versus Simulations") +
+      scale_color_manual(values = c(cd_2020_prop = "black")) +
+      theme_bw()
+
+    # Dem seats by BVAP rank -- numeric
+    plans %>%
+      group_by(draw) %>%
+      mutate(bvap = vap_black/total_vap, bvap_rank = rank(bvap)) %>%
+      subset_sampled() %>%
+      select(draw, district, bvap, bvap_rank, ndv, nrv) %>%
+      mutate(dem = ndv > nrv) %>%
+      group_by(bvap_rank) %>%
+      summarize(dem = mean(dem))
+
+    # Dem seats by Minority VAP rank -- numeric
+    plans %>%
+      group_by(draw) %>%
+      mutate(tmvap = (total_vap - vap_white)/total_vap, tmvap_rank = rank(tmvap)) %>%
+      subset_sampled() %>%
+      select(draw, district, tmvap, tmvap_rank, ndv, nrv) %>%
+      mutate(dem = ndv > nrv) %>%
+      group_by(tmvap_rank) %>%
+      summarize(dem = mean(dem))
+
+    # Total Black districts that are performing
+    plans %>%
+      subset_sampled() %>%
+      group_by(draw) %>%
+      summarize(n_black_perf = sum(vap_black/total_vap > 0.3 & e_dvs > 0.5)) %>%
+      count(n_black_perf)
+
+    # Total Minority districts that are performing
+    plans %>%
+      subset_sampled() %>%
+      group_by(draw) %>%
+      summarize(n_minority_perf = sum((total_vap - vap_white)/total_vap < 0.5 & e_dvs > 0.5)) %>%
+      count(n_minority_perf)
+
+    # democratic vote
+    plans <- plans %>%
+      mutate(Compactness = distr_compactness(map),
+             `Population deviation` = plan_parity(map),
+             `Democratic vote` = group_frac(map, ndv, (ndv + nrv)))
+    plot(plans, `Democratic vote`, size = 0.5, color_thresh = 0.5) +
+      scale_color_manual(values = c("black", "tomato2", "dodgerblue")) +
+      labs(title = "Democratic vote share by district")
+
+    # majority minority districts
+    plans <- plans %>%
+      mutate(majmin = if_else(total_vap/2 > vap_white, 1, 0))
+    majminavgs <- avg_by_prec(plans, majmin, draws = NA)
+    redist.plot.map(
+      sc_shp,
+      adj = redist.adjacency(sc_shp, plan),
+      plan = NULL,
+      fill = majminavgs,
+      fill_label = "",
+      zoom_to = NULL,
+      title = ""
+    )
+
+    # majority black districts
+    plans <- plans %>%
+      mutate(bvappct = vap_black/total_vap)
+    blackavgs <- avg_by_prec(plans, bvappct, draws = NA)
+    redist.plot.map(
+      sc_shp,
+      adj = redist.adjacency(sc_shp, plan),
+      plan = NULL,
+      fill = blackavgs,
+      fill_label = "",
+      zoom_to = NULL,
+      title = ""
+    )
+
+    # this section does not work
+    plot(plans, (total_vap - vap_white)/total_vap, sort = FALSE, size = 0.5)
+
+    pal <- scales::viridis_pal()(5)[-1]
+    redist.plot.scatter(plans, pct_min, pct_dem,
+                        color = pal[subset_sampled(plans)$district]) +
+      scale_color_manual(values = "black")
+
+    avg_by_prec(plans, x, draws = NA)
+    show(plans)
+
+    redist.plot.distr_qtys(
+      plans,
+      qty = total_vap,
+      sort = "asc",
+      geom = "jitter",
+      color_thresh = NULL,
+      size = 0.1,
+      ref_label = total_vap
+    )
+}
