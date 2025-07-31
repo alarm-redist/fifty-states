cc846af6378d0e1c22a9dbc9c914343d073b6293
Brian Zhou <87774511+brianlzhou@users.noreply.github.com>
2025-07-31 08:04:14 -0700

    style enforcement

--- analyses/OR_cd_2000/01_prep_2000_OR_cd_2000.R
+++ analyses/OR_cd_2000/01_prep_2000_OR_cd_2000.R
@@ -40,13 +40,13 @@ if (!file.exists(here(shp_path))) {
 
     # Create perimeters in case shapes are simplified
     redistmetrics::prep_perims(shp = or_shp,
-                               perim_path = here(perim_path)) %>%
+        perim_path = here(perim_path)) %>%
         invisible()
 
     # simplifies geometry for faster processing, plotting, and smaller shapefiles
     if (requireNamespace("rmapshaper", quietly = TRUE)) {
         or_shp <- rmapshaper::ms_simplify(or_shp, keep = 0.05,
-                                                 keep_shapes = TRUE) %>%
+            keep_shapes = TRUE) %>%
             suppressWarnings()
     }
 
