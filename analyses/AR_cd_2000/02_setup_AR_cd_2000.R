85350eee831caee8b4f3c2e3df2be1f2d80fc8d9
KT0416838 <kt794@georgetown.edu>
2025-08-01 21:05:17 -0400

    Update 02_setup_AR_cd_2000.R

--- analyses/AR_cd_2000/02_setup_AR_cd_2000.R
+++ analyses/AR_cd_2000/02_setup_AR_cd_2000.R
@@ -4,8 +4,6 @@
 ###############################################################################
 cli_process_start("Creating {.cls redist_map} object for {.pkg AR_cd_2000}")
 
-# any pre-computation (usually not necessary)
-
 map <- redist_map(ar_shp, pop_tol = 0.005,
                   existing_plan = cd_2000, adj = ar_shp$adj)
 
