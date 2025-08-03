248f56bd4527d6dd9d5b93436786431c8208ea0c
KT0416838 <kt794@georgetown.edu>
2025-08-03 01:05:29 -0400

    Add files via upload

---
+++ analyses/IL_cd_2000/03_sim_IL_cd_2000.R
@@ -0,0 +1,34 @@
+###############################################################################
+# Simulate plans for `IL_cd_2000`
+# © ALARM Project, August 2025
+###############################################################################
+
+# Run the simulation -----
+cli_process_start("Running simulations for {.pkg IL_cd_2000}")
+
+set.seed(2000)
+plans <- redist_smc(map, nsims = 1.2e4, runs = 5, counties = pseudo_county)
+# IF CORES OR OTHER UNITS HAVE BEEN MERGED:
+# make sure to call `pullback()` on this plans object!
+
+plans <- plans %>%
+  group_by(chain) %>%
+  filter(as.integer(draw) < min(as.integer(draw)) + 1000) %>% # thin samples
+  ungroup()
+plans <- match_numbers(plans, "cd_2000")
+
+cli_process_done()
+cli_process_start("Saving {.cls redist_plans} object")
+
+# Output the redist_map object. Do not edit this path.
+write_rds(plans, here("data-out/IL_2000/IL_cd_2000_plans.rds"), compress = "xz")
+cli_process_done()
+
+# Compute summary statistics -----
+cli_process_start("Computing summary statistics for {.pkg IL_cd_2000}")
+
+plans <- add_summary_stats(plans, map)
+# Output the summary statistics. Do not edit this path.
+save_summary_stats(plans, "data-out/IL_2000/IL_cd_2000_stats.csv")
+
+cli_process_done()
