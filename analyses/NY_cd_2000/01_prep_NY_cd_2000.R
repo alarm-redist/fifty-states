c31fdc32b5c732736150f133d7d1e74dcb638451
KT0416838 <kt794@georgetown.edu>
2025-08-07 15:14:04 -0400

    Add files via upload

---
+++ analyses/NY_cd_2000/01_prep_NY_cd_2000.R
@@ -0,0 +1,66 @@
+###############################################################################
+# Download and prepare data for NY_cd_2000 analysis
+# © ALARM Project, July 2025
+###############################################################################
+
+suppressMessages({
+  library(dplyr)
+  library(readr)
+  library(sf)
+  library(redist)
+  library(geomander)
+  library(baf)
+  library(cli)
+  library(here)
+  devtools::load_all() # load utilities
+})
+
+# Download necessary files for analysis -----
+cli_process_start("Downloading files for {.pkg NY_cd_2000}")
+
+path_data <- download_redistricting_file("NY", "data-raw/NY", year = 2000)
+
+cli_process_done()
+
+# Compile raw data into a final shapefile for analysis -----
+shp_path <- "data-out/NY_2000/shp_vtd.rds"
+perim_path <- "data-out/NY_2000/perim.rds"
+
+if (!file.exists(here(shp_path))) {
+  cli_process_start("Preparing {.strong NY} shapefile")
+  # read in redistricting data
+  ny_shp <- read_csv(here(path_data), col_types = cols(GEOID = "c")) %>%
+    # If the state is not at the VTD-level, swap in a `tinytiger::tt_*` function
+    join_vtd_shapefile(year = 2000) %>%
+    st_transform(EPSG$NY)
+  
+  ny_shp <- ny_shp %>%
+    rename(muni = place) %>%
+    mutate(muni = as.character(muni), county_muni = if_else(is.na(muni), county, str_c(county, muni))) %>%
+    relocate(muni, county_muni, cd_1990, .after = county)
+  
+  # Create perimeters in case shapes are simplified
+  redistmetrics::prep_perims(shp = ny_shp,
+                             perim_path = here(perim_path)) %>%
+    invisible()
+  
+  # simplifies geometry for faster processing, plotting, and smaller shapefiles
+  # feel free to delete if this dependency isn't available
+  if (requireNamespace("rmapshaper", quietly = TRUE)) {
+    ny_shp <- rmapshaper::ms_simplify(ny_shp, keep = 0.05,
+                                      keep_shapes = TRUE) %>%
+      suppressWarnings()
+  }
+  
+  # create adjacency graph
+  ny_shp$adj <- redist.adjacency(ny_shp)
+  
+  ny_shp <- ny_shp %>%
+    fix_geo_assignment(muni)
+  
+  write_rds(ny_shp, here(shp_path), compress = "gz")
+  cli_process_done()
+} else {
+  ny_shp <- read_rds(here(shp_path))
+  cli_alert_success("Loaded {.strong NY} shapefile")
+}
