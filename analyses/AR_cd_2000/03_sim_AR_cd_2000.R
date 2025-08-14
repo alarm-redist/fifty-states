a1b8007991acc6e08306c69c58fae377c772b20e
Kento Tamaki <kt794@georgetown.edu>
2025-08-14 11:38:02 -0400

    Update 03_sim_AR_cd_2000.R

--- analyses/AR_cd_2000/03_sim_AR_cd_2000.R
+++ analyses/AR_cd_2000/03_sim_AR_cd_2000.R
@@ -9,9 +9,6 @@ cli_process_start("Running simulations for {.pkg AR_cd_2000}")
 set.seed(2000)
 plans <- redist_smc(map, nsims = 2e3, runs = 5, counties = county, constraints = constr)
 
-# IF CORES OR OTHER UNITS HAVE BEEN MERGED:
-# make sure to call `pullback()` on this plans object!
-
 plans <- plans %>%
   group_by(chain) %>%
   filter(as.integer(draw) < min(as.integer(draw)) + 1000) %>% # thin samples
