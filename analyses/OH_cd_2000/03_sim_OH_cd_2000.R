aebe15e28a383d8d133738e2b9b3b9203330dcfb
Kento Tamaki <kt794@georgetown.edu>
2025-08-14 21:25:04 -0400

    Add files via upload

---
+++ analyses/OH_cd_2000/03_sim_OH_cd_2000.R
@@ -0,0 +1,32 @@
+###############################################################################
+# Simulate plans for `OH_cd_2000`
+# © ALARM Project, August 2025
+###############################################################################
+
+# Run the simulation -----
+cli_process_start("Running simulations for {.pkg OH_cd_2000}")
+
+set.seed(2000)
+plans <- redist_smc(map, nsims = 8e3, runs = 5, counties = county, ncores = parallel::detectCores())
+
+plans <- plans %>%
+  group_by(chain) %>%
+  filter(as.integer(draw) < min(as.integer(draw)) + 1000) %>% # thin samples
+  ungroup()
+plans <- match_numbers(plans, "cd_2000")
+
+cli_process_done()
+cli_process_start("Saving {.cls redist_plans} object")
+
+# Output the redist_map object. Do not edit this path.
+write_rds(plans, here("data-out/OH_2000/OH_cd_2000_plans.rds"), compress = "xz")
+cli_process_done()
+
+# Compute summary statistics -----
+cli_process_start("Computing summary statistics for {.pkg OH_cd_2000}")
+
+plans <- add_summary_stats(plans, map)
+# Output the summary statistics. Do not edit this path.
+save_summary_stats(plans, "data-out/OH_2000/OH_cd_2000_stats.csv")
+
+cli_process_done()
