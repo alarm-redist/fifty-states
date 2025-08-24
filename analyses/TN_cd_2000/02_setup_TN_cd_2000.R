386f9838313716c41a095c19845b8195a1f6c31a
Kento Tamaki <kt794@georgetown.edu>
2025-08-24 17:36:50 -0400

    Add files via upload

--- analyses/TN_cd_2000/02_setup_TN_cd_2000.R
+++ analyses/TN_cd_2000/02_setup_TN_cd_2000.R
@@ -1,20 +1,26 @@
 ###############################################################################
 # Set up redistricting simulation for `TN_cd_2000`
-# © ALARM Project, July 2025
+# © ALARM Project, August 2025
 ###############################################################################
 cli_process_start("Creating {.cls redist_map} object for {.pkg TN_cd_2000}")
 
 map <- redist_map(tn_shp, pop_tol = 0.005,
-    existing_plan = cd_2000, adj = tn_shp$adj)
+                  existing_plan = cd_2000, adj = tn_shp$adj)
 
-# Adjust pop_muni as needed to balance county/muni splits
-# make pseudo counties with default settings
 map <- map %>%
-    mutate(pseudo_county = pick_county_muni(map, counties = county, munis = muni,
-        pop_muni = get_target(map)))
+  mutate(
+    pseudo_county = pick_county_muni(
+      map,
+      counties = county,   
+      munis    = muni,     
+      pop_muni = get_target(map)
+    )
+  )
 
 # Add an analysis name attribute
 attr(map, "analysis_name") <- "TN_2000"
+
+map$state <- "TN"
 
 # Output the redist_map object. Do not edit this path.
 write_rds(map, "data-out/TN_2000/TN_cd_2000_map.rds", compress = "xz")
