6d138e329d008b7d319706e5acd310b06b62062b
Kento Tamaki <kt794@georgetown.edu>
2025-08-29 23:25:48 +0900

    Update 03_sim_MD_cd_2010.R

--- analyses/MD_cd_2010/03_sim_MD_cd_2010.R
+++ analyses/MD_cd_2010/03_sim_MD_cd_2010.R
@@ -25,20 +25,7 @@ cli_process_done()
 
 # Compute summary statistics -----
 cli_process_start("Computing summary statistics for {.pkg MD_cd_2010}")
-
-# Ensure geometry-only perimeters exist in an appropriate location and restore map attributes needed for the summary statistics.
-dir.create(here("data-out/MD_2010"), recursive = TRUE, showWarnings = FALSE)                     
-perim_df <- redistmetrics::prep_perims(map)                                                      
-write_rds(perim_df, here("data-out/MD_2010/perim.rds"), compress = "xz")                         
-
-if (is.null(attr(map, "analysis_name"))) attr(map, "analysis_name") <- "MD_cd_2010"              
-if (is.null(attr(map, "pop_col")))       attr(map, "pop_col")       <- "pop"                     
-if (is.null(attr(map, "ndists"))) {                                                                     
-  fd <- unique(plans$draw)[1]
-  attr(map, "ndists") <- length(unique(plans$district[plans$draw == fd]))
-}                                                                                                   
-if (is.null(attr(plans, "map"))) attr(plans, "map") <- map                                      
-
+                                  
 plans <- add_summary_stats(plans, map)                                                            
 
 plans <- plans |> select(-ends_with('.y')) |> rename_with(.fn = \(x) str_sub(x, end = -3), .cols = ends_with('.x'))
