12eece6d67a21fe8a30c261783674d23becf7b0e
Kento Tamaki <kt794@georgetown.edu>
2025-08-30 18:34:44 +0900

    Update 02_setup_MD_cd_2000.R

--- analyses/MD_cd_2000/02_setup_MD_cd_2000.R
+++ analyses/MD_cd_2000/02_setup_MD_cd_2000.R
@@ -4,11 +4,17 @@
 ###############################################################################
 cli_process_start("Creating {.cls redist_map} object for {.pkg MD_cd_2000}")
 
-map <- redist_map(md_shp, pop_tol = 0.005,
-                  existing_plan = cd_2000, adj = md_shp$adj)
+map <- redist_map(md_land, pop_tol = 0.005,
+                  existing_plan = cd_2000, adj = md_land$adj)
 
 # Add an analysis name attribute
 attr(map, "analysis_name") <- "MD_2000"
+
+# Compute and attach perimeters for compactness stats; save for reproducibility
+perim_df <- redistmetrics::prep_perims(map)
+attr(map, "perim_df") <- perim_df
+dir.create(here("data-out/MD_2000"), recursive = TRUE, showWarnings = FALSE)
+saveRDS(perim_df, here("data-out/MD_2000/perim.rds"))
 
 map$state <- "MD"
 
