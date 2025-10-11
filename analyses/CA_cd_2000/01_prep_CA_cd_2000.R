f52da07dbbfda21e9a5cec33f2e2a2a7b91db713
Kento Tamaki <kt794@georgetown.edu>
2025-10-11 21:10:11 +0900

    Update 01_prep_CA_cd_2000.R

--- analyses/CA_cd_2000/01_prep_CA_cd_2000.R
+++ analyses/CA_cd_2000/01_prep_CA_cd_2000.R
@@ -1,6 +1,6 @@
 ###############################################################################
 # Download and prepare data for CA_cd_2000 analysis
-# © ALARM Project, September 2025
+# © ALARM Project, October 2025
 ###############################################################################
 
 suppressMessages({
@@ -30,59 +30,65 @@ if (!file.exists(here(shp_path))) {
   cli_process_start("Preparing {.strong CA} shapefile")
   
   # read in redistricting data
-  ca_shp <- read_csv(here(path_data), col_types = cols(GEOID = "c")) %>%
-    join_vtd_shapefile(year = 2000) %>%
+  df <- read_csv(here(path_data), col_types = cols(GEOID = "c"))
+  
+  # join the data
+  ca_shp <- read_sf(here("data-raw/CA/ca_2000_tracts.geojson")) %>%
+    mutate(GEOID = paste0(STATEFP00, COUNTYFP00, TRACTCE00),
+           county = paste0(STATEFP00, COUNTYFP00)) %>%
+    left_join(df, by = "GEOID") %>%
     st_transform(EPSG$CA)
   
+  # data cleaning
   ca_shp <- ca_shp %>%
+    mutate(
+      county = dplyr::coalesce(.data[["county.x"]],
+                               .data[["county.y"]],
+                               substr(.data[["GEOID"]], 1, 5))
+    ) %>%
+    select(-any_of(c("county.x", "county.y"))) %>%
     rename(muni = place) %>%
-    mutate(muni = as.character(muni),
-           county_muni = if_else(is.na(muni), county, str_c(county, muni))) %>%
+    mutate(
+      muni = as.character(muni),
+      county_muni = if_else(is.na(muni), county, stringr::str_c(county, muni))
+    ) %>%
     relocate(muni, county_muni, cd_1990, .after = county)
-
-# Reallocate counts from two islands to nearest mainland neighbors, then drop them to keep contiguity.
-  xwalk <- tibble::tibble(
-    donor_GEOID = c("06037599100", "06037599000"),
-    recip_GEOID = c("06037297300", "06037297500")
-  )
   
-  idx_donors <- match(xwalk$donor_GEOID, ca_shp$GEOID)
-  idx_recip  <- match(xwalk$recip_GEOID, ca_shp$GEOID)
-  stopifnot(all(!is.na(idx_donors)), all(!is.na(idx_recip)))
-  
-  counts_cols <- c(
-    "pop","vap","ndv","nrv",
-    grep("^pop_(white|black|hisp|asian|aian|nhpi|two|other)$", names(ca_shp), value = TRUE),
-    grep("^vap_(white|black|hisp|asian|aian|nhpi|two|other)$", names(ca_shp), value = TRUE)
-  ) |> intersect(names(ca_shp))
-  
-  for (k in seq_along(idx_donors)) {
-    i <- idx_donors[k]; j <- idx_recip[k]
-    di <- sf::st_drop_geometry(ca_shp[i, counts_cols, drop = FALSE]); di[is.na(di)] <- 0
-    dj <- sf::st_drop_geometry(ca_shp[j, counts_cols, drop = FALSE]); dj[is.na(dj)] <- 0
-    ca_shp[j, counts_cols] <- as.data.frame(mapply(`+`, dj, di, SIMPLIFY = FALSE))
-    ca_shp[i, counts_cols] <- 0
-  }
-  
-  ca_shp <- ca_shp %>%
-    filter(!GEOID %in% xwalk$donor_GEOID)
-  
-  dir.create(dirname(here(perim_path)), recursive = TRUE, showWarnings = FALSE)
   redistmetrics::prep_perims(ca_shp, perim_path = here(perim_path)) %>% invisible()
   
-  if (requireNamespace("rmapshaper", quietly = TRUE)) {
-    ca_shp <- rmapshaper::ms_simplify(ca_shp, keep = 0.05,
-                                      keep_shapes = TRUE) %>%
-      suppressWarnings()
+  ca_shp <- sf::st_set_precision(ca_shp, 1) %>% sf::st_buffer(0)
+  adj_full <- redist.adjacency(ca_shp)
+  
+  ca_shp_s <- if (requireNamespace("rmapshaper", quietly = TRUE)) {
+    rmapshaper::ms_simplify(ca_shp, keep = 0.05, keep_shapes = TRUE) %>% suppressWarnings()
+  } else {
+    ca_shp
   }
   
-  # create adjacency graph
-  ca_shp$adj <- redist.adjacency(ca_shp)
+  idx <- match(ca_shp_s$GEOID, ca_shp$GEOID)
+  ca_shp_s$adj <- adj_full[idx]
   
-  ca_shp <- ca_shp %>%
-    fix_geo_assignment(muni)
+  # connect island
+  pairs <- list(
+    c("06037599000", "06037599100"),
+    c("06037599100", "06037297500")
+  )
   
-  write_rds(ca_shp, here(shp_path), compress = "gz")
+  for (p in pairs) {
+    ia <- match(p[1], ca_shp_s$GEOID) 
+    ib <- match(p[2], ca_shp_s$GEOID)
+    if (!is.na(ia) && !is.na(ib)) {
+      ca_shp_s$adj <- geomander::add_edge(ca_shp_s$adj, ia, ib, zero = TRUE)
+    } else {
+      warning(sprintf("Could not find GEOID(s): %s, %s", p[1], p[2]))
+    }
+  }
+  
+  # fix_geo_assignment 
+  ca_shp_s <- ca_shp_s %>% fix_geo_assignment(muni)
+  ca_shp <- ca_shp_s
+  
+  write_rds(ca_shp_s, here(shp_path), compress = "gz")
   cli_process_done()
 } else {
   ca_shp <- read_rds(here(shp_path))
