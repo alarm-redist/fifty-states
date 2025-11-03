40c2531e6f323042e0a5518ae934d08431361369
Kento Tamaki <kt794@georgetown.edu>
2025-11-03 18:08:04 +0900

    Update 02_setup_MD_cd_2010.R

--- analyses/MD_cd_2010/02_setup_MD_cd_2010.R
+++ analyses/MD_cd_2010/02_setup_MD_cd_2010.R
@@ -1,19 +1,15 @@
 ###############################################################################
 # Set up redistricting simulation for `MD_cd_2010`
-# © ALARM Project, August 2025
+# © ALARM Project, January 2023
 ###############################################################################
 cli_process_start("Creating {.cls redist_map} object for {.pkg MD_cd_2010}")
 
-map <- redist_map(md_land, pop_tol = 0.005, 
-                  existing_plan = cd_2010, adj = md_land$adj)
+# Remove state FIPS prefix (24) from cd_2010
+md_shp <- md_shp %>%
+  mutate(cd_2010 = as.integer(stringr::str_remove(cd_2010, "^24")))
 
-# Normalize enacted labels
-nd <- attr(map, "ndists")
-u  <- sort(unique(na.omit(map$cd_2010)))   
-ref_norm <- match(map$cd_2010, u)   
-stopifnot(identical(sort(unique(na.omit(ref_norm))), seq_len(nd)))
-map$cd_2010 <- ref_norm
-attr(map, "existing_plan") <- ref_norm
+map <- redist_map(md_shp, pop_tol = 0.005,
+                  existing_plan = cd_2010, adj = md_shp$adj)
 
 map <- map %>%
   mutate(pseudo_county = pick_county_muni(map, counties = county, munis = muni,
@@ -21,14 +17,6 @@ map <- map %>%
 
 # Add an analysis name attribute
 attr(map, "analysis_name") <- "MD_2010"
-
-# Compute and attach perimeters for compactness stats; save for reproducibility
-perim_df <- redistmetrics::prep_perims(map)
-attr(map, "perim_df") <- perim_df
-dir.create(here("data-out/MD_2010"), recursive = TRUE, showWarnings = FALSE)
-saveRDS(perim_df, here("data-out/MD_2010/perim.rds"))
-
-map$state <- "MD"
 
 # Output the redist_map object. Do not edit this path.
 write_rds(map, "data-out/MD_2010/MD_cd_2010_map.rds", compress = "xz")
