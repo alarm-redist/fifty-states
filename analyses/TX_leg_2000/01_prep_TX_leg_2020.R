6a5cf78474d48079c7b5b49161ac9b7676652ea4
Taran Samarth <tns29@a1130u11n02.mghpcc.ycrc.yale.edu>
2025-11-03 12:37:05 -0500

    senate attempt and cvap fix

---
+++ analyses/TX_leg_2000/01_prep_TX_leg_2020.R
@@ -0,0 +1,128 @@
+###############################################################################
+# Download and prepare data for `TX_leg_2020` analysis
+# © ALARM Project, October 2025
+###############################################################################
+
+suppressMessages({
+    library(dplyr)
+    library(readr)
+    library(sf)
+    library(redist)
+    library(geomander)
+    library(cli)
+    library(here)
+    library(tinytiger)
+    devtools::load_all() # load utilities
+})
+
+stopifnot(utils::packageVersion("redist") >= "5.0.0.1")
+
+# Download necessary files for analysis -----
+cli_process_start("Downloading files for {.pkg TX_leg_2020}")
+
+path_data <- download_redistricting_file("TX", "data-raw/TX", year = 2020)
+
+# TODO other files here (as necessary). All paths should start with `path_`
+# If large, consider checking to see if these files exist before downloading
+
+cli_process_done()
+
+# Compile raw data into a final shapefile for analysis -----
+shp_path <- "data-out/TX_2020/shp_vtd.rds"
+perim_path <- "data-out/TX_2020/perim.rds"
+
+if (!file.exists(here(shp_path))) {
+    cli_process_start("Preparing {.strong TX} shapefile")
+    # read in redistricting data
+    tx_shp <- read_csv(here(path_data), col_types = cols(GEOID20 = "c")) |>
+        join_vtd_shapefile(year = 2020) |>
+        st_transform(EPSG$TX)  |>
+        rename_with(function(x) gsub("[0-9.]", "", x), starts_with("GEOID"))
+
+    # add municipalities
+    d_muni <- make_from_baf("TX", "INCPLACE_CDP", "VTD", year = 2020)  |>
+        mutate(GEOID = paste0(censable::match_fips("TX"), vtd)) |>
+        select(-vtd)
+    d_ssd <- make_from_baf("TX", "SLDU", "VTD", year = 2020)  |>
+        transmute(GEOID = paste0(censable::match_fips("TX"), vtd),
+                  ssd_2010 = as.integer(sldu))
+    d_shd <- make_from_baf("TX", "SLDL", "VTD", year = 2020)  |>
+        transmute(GEOID = paste0(censable::match_fips("TX"), vtd),
+                  shd_2010 = as.integer(sldl))
+
+    tx_shp <- tx_shp |>
+        left_join(d_muni, by = "GEOID") |>
+        left_join(d_ssd, by = "GEOID") |>
+        left_join(d_shd, by = "GEOID") |>
+        mutate(county_muni = if_else(is.na(muni), county, str_c(county, muni))) |>
+        relocate(muni, county_muni, ssd_2010, .after = county) |>
+        relocate(muni, county_muni, shd_2010, .after = county)
+
+    # add the enacted plan
+    tx_shp <- tx_shp |>
+        left_join(y = leg_from_baf(state = "TX"), by = "GEOID")
+
+    # CVAP columns
+    state <- "TX"
+    path_cvap <- here(paste0("data-raw/", state, "/cvap_2020.rds"))
+    
+    if (!file.exists(path_cvap)) {
+      cvap <-
+        cvap::cvap_distribute_censable(state) %>% select(GEOID, starts_with("cvap"))
+#      vest_cw <- cvap::vest_crosswalk(state)
+#      cvap <-
+#        PL94171::pl_retally(cvap, crosswalk = vest_cw)
+      vtd_baf <- read_delim("data-raw/TX/BlockAssign_ST48_TX_VTD_2020.txt", col_types = c("c"))
+      cvap <- cvap %>%
+        left_join(vtd_baf %>% rename(GEOID = BLOCKID),
+                  by = "GEOID")
+      cvap <- cvap %>%
+        mutate(GEOID = paste0(COUNTYFP, DISTRICT)) %>%
+        select(GEOID, starts_with("cvap"))
+      cvap <- cvap %>%
+        group_by(GEOID) %>%
+        summarize(across(.fns = sum))
+      saveRDS(cvap, path_cvap, compress = "xz")
+    } else {
+      cvap <- read_rds(path_cvap)
+    }
+    
+    cvap <- cvap %>% mutate(GEOID = paste0("48", GEOID))
+    
+    tx_shp <- tx_shp %>%
+      left_join(cvap, by = "GEOID") %>%
+      st_as_sf()
+    
+    # Create perimeters in case shapes are simplified
+    redistmetrics::prep_perims(shp = tx_shp,
+                             perim_path = here(perim_path)) |>
+        invisible()
+
+    # simplifies geometry for faster processing, plotting, and smaller shapefiles
+    if (requireNamespace("rmapshaper", quietly = TRUE)) {
+        tx_shp <- rmapshaper::ms_simplify(tx_shp, keep = 0.05,
+                                                 keep_shapes = TRUE) |>
+            suppressWarnings()
+    }
+
+    # create adjacency graph
+    tx_shp$adj <- adjacency(tx_shp)
+
+    # check max number of connected components
+    # 1 is one fully connected component, more is worse
+    ccm(tx_shp$adj, tx_shp$ssd_2020)
+    ccm(tx_shp$adj, tx_shp$shd_2020)
+
+    tx_shp <- tx_shp |>
+        fix_geo_assignment(muni)
+
+    write_rds(tx_shp, here(shp_path), compress = "gz")
+    cli_process_done()
+} else {
+    tx_shp <- read_rds(here(shp_path))
+    cli_alert_success("Loaded {.strong TX} shapefile")
+}
+
+# TODO visualize the enacted maps using:
+# redistio::draw(tx_shp, tx_shp$ssd_2020)
+# redistio::draw(tx_shp, tx_shp$shd_2020)
---
+++ analyses/TX_leg_2000/03_sim_TX_shd_2020.R
@@ -0,0 +1,118 @@
+###############################################################################
+# Simulate plans for `TX_shd_2020` SHD
+# © ALARM Project, October 2025
+###############################################################################
+
+# Run the simulation -----
+sink("analyses/TX_leg_2000/output_sf.txt")
+cli_process_start("Running simulations for {.pkg TX_shd_2020}")
+
+# VRA constraints
+constraints <- redist_constr(map_shd) %>%
+  add_constr_grp_hinge(
+    3,
+    cvap_hisp,
+    total_pop = cvap,
+    tgts_group = c(0.45)
+  ) %>%
+  add_constr_grp_hinge(
+    8,
+    cvap_black,
+    total_pop = cvap,
+    tgts_group = c(0.5))
+
+set.seed(2020)
+
+mh_accept_per_smc <- ceiling(n_distinct(map_shd$shd_2020)/3) * 2
+
+plans <- redist_smc(
+  map_shd,
+  nsims = 2500, runs = 5,
+  constraints = constraints,
+  counties = pseudo_county,
+  sampling_space = "spanning_forest", # linking_edge
+  ms_params = list(frequency = 1L, mh_accept_per_smc = mh_accept_per_smc),
+  split_params = list(splitting_schedule = "any_valid_sizes"),
+  verbose = TRUE, ncores = 39
+)
+
+sink()
+
+# IF CORES OR OTHER UNITS HAVE BEEN MERGED:
+# make sure to call `pullback()` on this plans object!
+
+plans <- plans |>
+    group_by(chain) |>
+    filter(as.integer(draw) < min(as.integer(draw)) + 2000) |> # thin samples
+    ungroup()
+plans <- match_numbers(plans, "shd_2020")
+
+cli_process_done()
+cli_process_start("Saving {.cls redist_plans} object")
+
+# TODO add any reference plans that aren't already included
+
+# Output the redist_map object. Do not edit this path.
+write_rds(plans, here("data-out/TX_2020/TX_shd_2020_plans.rds"), compress = "xz")
+cli_process_done()
+
+# Compute summary statistics -----
+cli_process_start("Computing summary statistics for {.pkg TX_shd_2020}")
+
+plans <- add_summary_stats(plans, map_shd) |>
+  mutate(total_cvap = tally_var(map_shd, cvap), .after = total_vap)
+
+# cvap columns
+cvap_cols <- names(map_shd)[tidyselect::eval_select(starts_with("cvap_"), map_shd)]
+for (col in rev(cvap_cols)) {
+  plans <- mutate(plans, {{ col }} := tally_var(map_shd, map_shd[[col]]), .after = vap_two)
+}
+
+# Output the summary statistics. Do not edit this path.
+save_summary_stats(plans, "data-out/TX_2020/TX_shd_2020_stats.csv")
+
+cli_process_done()
+
+if (interactive()) {
+    library(ggplot2)
+    library(patchwork)
+
+    validate_analysis(plans, map_shd)
+    summary(plans)
+    
+    # Extra validation plots for custom constraints -----
+    
+    psum <- plans %>%
+      group_by(draw) %>%
+      summarise(
+        all_hvap = sum((cvap_hisp/total_cvap) > 0.4),
+        dem_hvap = sum((cvap_hisp/total_cvap) > 0.4 &
+                         (ndv > nrv)),
+        rep_hvap = sum((cvap_hisp/total_cvap) > 0.4 &
+                         (nrv > ndv)),
+        all_bvap = sum((cvap_black/total_cvap) > 0.4),
+        dem_bvap = sum((cvap_black/total_cvap) > 0.4 &
+                        (ndv > nrv)),
+        rep_bvap = sum((cvap_black/total_cvap) > 0.4 &
+                         (nrv > ndv)),
+        mmd_coalition = sum(((
+          cvap_hisp + cvap_black + cvap_asian
+        )/total_cvap) > 0.5),
+        mmd_coalition_dem = sum(((
+          cvap_hisp + cvap_black + cvap_asian
+        )/total_cvap) > 0.5 &
+          (ndv > nrv))
+      )
+    
+    p1 <- redist.plot.hist(psum, all_hvap) + xlab("HCVAP > .4")
+    p2 <- redist.plot.hist(psum, dem_hvap) + xlab("HCVAP > .4 & Dem > Rep")
+    p3 <- redist.plot.hist(psum, all_bvap) + xlab("BCVAP > .4")
+    p4 <- redist.plot.hist(psum, dem_bvap) + xlab("BCVAP > .4 & Dem > Rep")
+    p5 <- redist.plot.hist(psum, mmd_coalition) + xlab("Hisp + Black + Asian CVAP > .5")
+    p6 <- redist.plot.hist(psum, mmd_coalition_dem) + xlab("Hisp + Black + Asian CVAP > .5 & Dem > Rep")
+    
+    p1 + p2 + p3 + p4 + p5 + p6
+
+    # Extra validation plots for custom constraints -----
+    # TODO remove this section if no custom constraints
+}
