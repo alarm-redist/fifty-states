cf334af369dd4dfb4168bee47ba9c08e610d0374
Kento Tamaki <kt794@georgetown.edu>
2025-11-14 01:05:56 +0900

    Update 03_sim_NH_cd_2000.R

--- analyses/NH_cd_2000/03_sim_NH_cd_2000.R
+++ analyses/NH_cd_2000/03_sim_NH_cd_2000.R
@@ -1,26 +1,29 @@
 ###############################################################################
 # Simulate plans for `NH_cd_2000`
-# © ALARM Project, July 2025
+# © ALARM Project, November 2025
 ###############################################################################
 
 # Run the simulation -----
 cli_process_start("Running simulations for {.pkg NH_cd_2000}")
 
-# Run simulations, replacing state FIPS with abbreviation (for ease in generating validation graphic)
-## Merge by MCDs and use county_muni.
+# Run simulations
 set.seed(2000)
 plans <- redist_smc(
   map,
   nsims    = 2e3,
   runs     = 10,
-  counties = county_muni
-  ) %>%
-  pullback() %>%
-  structure(prec_pop = map$pop) %>%
-  group_by(chain) %>%
-  filter(as.integer(draw) < min(as.integer(draw)) + 500) %>%
-  ungroup()
+  counties = county,
+  sampling_space = "linking_edge",
+  pop_temper = 0.05,
+  seq_alpha = 0.9,
+  ms_params = list(frequency = 1, mh_accept_per_smc = 50, pair_rule = "uniform")
+  )
 
+# Thin plans
+plans <- plans %>%
+  group_by(chain) %>%
+  filter(as.integer(draw) < min(as.integer(draw)) + 500) %>% 
+  ungroup()
 plans <- match_numbers(plans, "cd_2000")
 
 cli_process_done()
@@ -38,3 +41,4 @@ plans <- add_summary_stats(plans, map)
 save_summary_stats(plans, "data-out/NH_2000/NH_cd_2000_stats.csv")
 
 cli_process_done()
+
