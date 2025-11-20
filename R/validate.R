f1c0adf994dc4b615647b5af8fceab3f089daa0c
Christopher Kenny <ctk44@cornell.edu>
2025-11-20 16:08:06 -0500

    compactness + splits measures

--- R/validate.R
+++ R/validate.R
@@ -21,12 +21,38 @@ validate_analysis <- function(plans, map) {
     p_comp1 <- hist(plans, comp_edge, bins = 40) + labs(title = "Compactness: fraction kept") + theme_bw()
     p_comp2 <- plot(plans, comp_polsby, geom = "boxplot") + labs(title = "Compactness: Polsby-Popper") + theme_bw()
 
+    if ("comp_bbox_reock" %in% names(plans)) {
+        p_comp3 <- plot(plans, comp_bbox_reock, geom = "boxplot") +
+          labs(title = "Compactness: Bounding Box Reock") +
+          theme_bw()
+    } else {
+        p_comp3 <- patchwork::plot_spacer()
+    }
+
     if ("county_splits" %in% names(plans)) {
         p_split1 <- hist(plans, county_splits) + labs(title = "County splits") + theme_bw()
-    } else p_split1 <- patchwork::plot_spacer()
+    } else {
+      p_split1 <- patchwork::plot_spacer()
+    }
     if ("muni_splits" %in% names(plans)) {
         p_split2 <- hist(plans, muni_splits) + labs(title = "Municipality splits") + theme_bw()
-    } else p_split2 <- patchwork::plot_spacer()
+    } else {
+      p_split2 <- patchwork::plot_spacer()
+    }
+    if ("total_county_splits" %in% names(plans)) {
+        p_split3 <- hist(plans, total_county_splits) +
+          labs(title = "Total county splits") +
+          theme_bw()
+    } else {
+        p_split3 <- patchwork::plot_spacer()
+    }
+    if ("total_muni_splits" %in% names(plans)) {
+        p_split4 <- hist(plans, total_muni_splits) +
+          labs(title = "Total municipality splits") +
+          theme_bw()
+    } else {
+        p_split4 <- patchwork::plot_spacer()
+    }
 
     st <- map$state[1]
     enac_sum <- plans |>
@@ -79,17 +105,21 @@ validate_analysis <- function(plans, map) {
 AAABBB
 CCCDDD
 EEEEEE
+NNNNNN
 FFFGGG
+LLLMMM
 HHHHHH
 IIJJKK
 IIJJKK"
     p <- patchwork::wrap_plots(A = p_wgts, B = p_div, C = p_dev, D = p_comp1,
-                               E = p_comp2, F = p_split1, G = p_split2,
-                               H = p_vra, I = p_ex1, J = p_ex2, K = p_ex3, design = layout) +
+                               E = p_comp2, `F` = p_split1, G = p_split2,
+                               H = p_vra, I = p_ex1, J = p_ex2, K = p_ex3,
+                               L = p_split3, M = p_split4, N = p_comp3,
+                               design = layout) +
         patchwork::plot_annotation(title = str_c(map$state[1], " Validation")) +
         patchwork::plot_layout(guides = "collect")
     out_path <- here(str_glue("data-raw/{map$state[1]}/validation_{format(Sys.time(), '%Y%m%d_%H%M')}.png"))
-    ggsave(out_path, plot = p, height = 15, width = 10)
+    ggsave(out_path, plot = p, height = 18, width = 10)
     if (rstudioapi::isAvailable()) rstudioapi::viewer(out_path)
     invisible(out_path)
 }
