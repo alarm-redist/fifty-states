45527db415212513889e17ea38752a62a8f03193
Kara77779 <qinghongli98@gmail.com>
2025-11-22 23:03:49 -0500

    Add BVAP performance analysis for AL state legislative districts

--- analyses/AL_leg_2020/03_sim_AL_ssd_2020.R
+++ analyses/AL_leg_2020/03_sim_AL_ssd_2020.R
@@ -53,3 +53,46 @@ if (interactive()) {
     validate_analysis(plans, map_ssd)
     summary(plans)
 }
+# Extra validation plots for custom constraints -----
+if (interactive()) {
+  library(ggplot2)
+  library(patchwork)
+
+  # Create output directory
+  dir.create(here("data-out/AL_2020/bvap_plots"), showWarnings = FALSE, recursive = TRUE)
+
+  # Black VAP Performance Plot
+  p1 <- redist.plot.distr_qtys(plans, vap_black/total_vap,
+                               color_thresh = NULL,
+                               color = ifelse(subset_sampled(plans)$ndv > subset_sampled(plans)$nrv, "#3D77BB", "#B25D4C"),
+                               size = 0.5, alpha = 0.5) +
+    scale_y_continuous("Percent Black by VAP") +
+    labs(title = "Alabama Proposed Plan versus Simulations") +
+    scale_color_manual(values = c(ssd_2020 = "black")) +
+    theme_bw()
+
+  ggsave(here("data-out/AL_2020/bvap_plots/bvap_performance.png"), p1, width = 10, height = 6)
+
+  # Dem seats by BVAP rank -- numeric
+  tbl1 <- plans %>%
+    group_by(draw) %>%
+    mutate(bvap = vap_black/total_vap, bvap_rank = rank(bvap)) %>%
+    subset_sampled() %>%
+    select(draw, district, bvap, bvap_rank, ndv, nrv) %>%
+    mutate(dem = ndv > nrv) %>%
+    group_by(bvap_rank) %>%
+    summarize(dem = mean(dem))
+
+  print(tbl1)
+  write_csv(tbl1, here("data-out/AL_2020/bvap_plots/dem_seats_by_bvap_rank.csv"))
+
+  # Total Black districts that are performing
+  tbl2 <- plans %>%
+    subset_sampled() %>%
+    group_by(draw) %>%
+    summarize(n_black_perf = sum(vap_black/total_vap > 0.3 & ndshare > 0.5)) %>%
+    count(n_black_perf)
+
+  print(tbl2)
+  write_csv(tbl2, here("data-out/AL_2020/bvap_plots/total_black_performant.csv"))
+}
