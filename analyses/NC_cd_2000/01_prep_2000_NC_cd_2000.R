b324f0f1716d21605d7c59bda6989d49259b1c7f
Brian Zhou <87774511+brianlzhou@users.noreply.github.com>
2025-11-23 02:25:26 -0800

    Update 01_prep_2000_NC_cd_2000.R

--- analyses/NC_cd_2000/01_prep_2000_NC_cd_2000.R
+++ analyses/NC_cd_2000/01_prep_2000_NC_cd_2000.R
@@ -65,3 +65,77 @@ if (!file.exists(here(shp_path))) {
     nc_shp <- read_rds(here(shp_path))
     cli_alert_success("Loaded {.strong NC} shapefile")
 }
+
+
+###############################################################################
+# Logit-shift ndv/nrv to match 2000 MEDSL county results
+###############################################################################
+
+#' Logit Shift Baseline Data
+#'
+#' @param d_baseline baseline data containing vote columns
+#' @param ndv Unquoted Democratic vote column name
+#' @param nrv Unquoted Republican vote column name
+#' @param target target to logit shift to
+#' @param tol
+#'
+#' @returns a data frame with adjusted vote columns
+#' @export
+#'
+#' @examples
+#' # TODO
+logit_shift_baseline <- function(d_baseline, ndv, nrv,
+                                 target = 0.5,
+                                 tol = sqrt(.Machine$double.eps)) {
+  if (missing(ndv) || missing(nrv)) {
+    cli::cli_abort('Both {.arg ndv} and {.arg nrv} must be provided.')
+  }
+  ndv_q <- rlang::enquo(ndv)
+  nrv_q <- rlang::enquo(nrv)
+
+  ndv_vec <- dplyr::pull(d_baseline, !!ndv_q)
+  nrv_vec <- dplyr::pull(d_baseline, !!nrv_q)
+
+  turn <- ndv_vec + nrv_vec
+  ldvs <- dplyr::if_else(turn > 0, log(ndv_vec) - log(nrv_vec), 0)
+
+  res <- uniroot(function(shift) {
+    stats::weighted.mean(plogis(ldvs + shift), turn) - target
+  }, c(-1, 1), tol = tol)
+
+  ldvs <- ldvs + res$root
+
+  ndv_new <- turn * plogis(ldvs)
+  nrv_new <- turn - ndv_new
+
+  dplyr::mutate(
+    d_baseline,
+    !!rlang::as_name(ndv_q) := ndv_new,
+    !!rlang::as_name(nrv_q) := nrv_new
+  )
+}
+
+# 1. Load the MEDSL county CSV as `medsl_cty` ----
+medsl_cty <- read_csv(
+  here("data-raw/baseline_voteshare_medsl_00.csv"),
+  show_col_types = FALSE
+)
+
+# 2. Add county_fips column based on VTD GEOID ----
+nc_shp <- nc_shp |>
+  mutate(county_fips = stringr::str_sub(GEOID, 1, 5))
+
+names(nc_shp)
+
+# 3. For each county, logit-shift ndv/nrv to the 2000 target from MEDSL ----
+nc_shp |>
+  group_by(county_fips) |>
+  group_split() |>
+  lapply(function(x) {
+    meds <- medsl_cty |>
+      filter(county == x$county_fips[1])
+    target <- meds$dshare_00[1]
+
+    x |>
+      logit_shift_baseline(ndv = ndv, nrv = nrv, target = target)
+  })
