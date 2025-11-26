65bbb970f522b11fe60d2443bd8e09494fe62ddf
Boning Dai <jerrydai08@gmail.com>
2025-11-26 09:49:26 -0600

    1990 Idah Congressional Districts

---
+++ analyses/ID_cd_1990/01_prep_ID_cd_1990.R
@@ -0,0 +1,79 @@
+###############################################################################
+# Download and prepare data for `ID_cd_1990` analysis
+# © ALARM Project, November 2025
+###############################################################################
+
+suppressMessages({
+    library(dplyr)
+    library(readr)
+    library(sf)
+    library(redist)
+    library(geomander)
+    library(baf)
+    library(cli)
+    library(here)
+    devtools::load_all() # load utilities
+})
+
+# Download necessary files for analysis -----
+cli_process_start("Downloading files for {.pkg ID_cd_1990}")
+
+
+path_data <- download_redistricting_file("ID", "data-raw/ID", year = 1990)
+
+cli_process_done()
+
+# Compile raw data into a final shapefile for analysis -----
+shp_path <- "data-out/ID_1990/shp_vtd.rds"
+perim_path <- "data-out/ID_1990/perim.rds"
+
+if (!file.exists(here(shp_path))) {
+    cli_process_start("Preparing {.strong ID} shapefile")
+    # read in redistricting data
+    raw_data <- read_csv(here(path_data), col_types = cols(GEOID = "c"))
+    raw_data$state = as.character(raw_data$state)
+    shapefile <- st_read(tract_path, quiet = TRUE) |>
+        rename(state_shp = state)
+    id_shp <- raw_data |>
+        left_join(shapefile, by = "GEOID")
+    # manually set state to ID
+    id_shp = mutate(id_shp, state = "ID") |>
+        st_as_sf()
+
+    # id_shp = join_vtd_shapefile(raw_data, year = 1990)
+
+    id_shp = st_transform(id_shp, EPSG$ID)
+
+    id_shp <- id_shp |>
+        rename(muni = place) |>
+        mutate(county_muni = if_else(is.na(muni), county.x, str_c(county.x, muni))) |>
+        relocate(muni, county_muni, cd_1980, .after = county.x)
+
+    # TODO any additional columns or data you want to add should go here
+
+    # Create perimeters in case shapes are simplified
+    redistmetrics::prep_perims(shp = id_shp,
+                               perim_path = here(perim_path)) |>
+        invisible()
+
+    # simplifies geometry for faster processing, plotting, and smaller shapefiles
+    # TODO feel free to delete if this dependency isn't available
+    if (requireNamespace("rmapshaper", quietly = TRUE)) {
+        id_shp <- rmapshaper::ms_simplify(id_shp, keep = 0.05,
+                                                 keep_shapes = TRUE) |>
+            suppressWarnings()
+    }
+    id_shp <- id_shp |>
+      select(-county.y, -tract.y, -state_shp)
+
+    # create adjacency graph
+    id_shp$adj <- redist.adjacency(id_shp)
+
+    # TODO any custom adjacency graph edits here
+
+    write_rds(id_shp, here(shp_path), compress = "gz")
+    cli_process_done()
+} else {
+    id_shp <- read_rds(here(shp_path))
+    cli_alert_success("Loaded {.strong ID} shapefile")
+}
---
+++ analyses/ID_cd_1990/02_setup_ID_cd_1990.R
@@ -0,0 +1,29 @@
+###############################################################################
+# Set up redistricting simulation for `ID_cd_1990`
+# © ALARM Project, November 2025
+###############################################################################
+cli_process_start("Creating {.cls redist_map} object for {.pkg ID_cd_1990}")
+
+# TODO any pre-computation (usually not necessary)
+
+map <- redist_map(id_shp, pop_tol = 0.005,
+    existing_plan = cd_1990, adj = id_shp$adj)
+
+# TODO any filtering, cores, merging, etc.
+
+# TODO remove if not necessary. Adjust pop_muni as needed to balance county/muni splits
+# make pseudo counties with default settings
+map <- map |>
+    mutate(pseudo_county = pick_county_muni(map, counties = county.x, munis = muni,
+                                            pop_muni = get_target(map)))
+map <- map %>%
+  rename(county = county.x)
+# IF MERGING CORES OR OTHER UNITS:
+# make a new `map_cores` object that is merged & used for simulating. You can set `drop_geom=TRUE` for this.
+
+# Add an analysis name attribute
+attr(map, "analysis_name") <- "ID_1990"
+
+# Output the redist_map object. Do not edit this path.
+write_rds(map, "data-out/ID_1990/ID_cd_1990_map.rds", compress = "xz")
+cli_process_done()
