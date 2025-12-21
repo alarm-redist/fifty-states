924b6585670ef067f458a84d6bb585f1193b2cf3
Devanshi Shah <dishu@umich.edu>
2025-12-21 20:14:37 +0530

    CO cd draft 1990

---
+++ analyses/CO_cd_1990/01_prep_CO_cd_1990.R
@@ -0,0 +1,66 @@
+###############################################################################
+# Download and prepare data for `CO_cd_1990` analysis
+# © ALARM Project, December 2025
+###############################################################################
+
+suppressMessages({
+    library(dplyr)
+    library(readr)
+    library(sf)
+    library(redist)
+    library(geomander)
+    library(baf)
+    library(cli)
+    library(here)
+    devtools::load_all() # load utilities
+})
+
+# Download necessary files for analysis -----
+cli_process_start("Downloading files for {.pkg CO_cd_1990}")
+
+path_data <- download_redistricting_file("CO", "data-raw/CO", year = 1990)
+
+cli_process_done()
+
+# Compile raw data into a final shapefile for analysis -----
+shp_path <- "data-out/CO_1990/shp_vtd.rds"
+perim_path <- "data-out/CO_1990/perim.rds"
+
+if (!file.exists(here(shp_path))) {
+    cli_process_start("Preparing {.strong CO} shapefile")
+    # read in redistricting data
+    co_shp <- read_csv(here(path_data), col_types = cols(GEOID = "c")) |>
+        join_vtd_shapefile(year = 1990) |>
+        st_transform(EPSG$CO)
+
+    co_shp <- co_shp |>
+        rename(muni = place) |>
+        mutate(county_muni = if_else(is.na(muni), county, str_c(county, muni))) |>
+        relocate(muni, county_muni, cd_1980, .after = county)
+
+    # TODO any additional columns or data you want to add should go here
+
+    # Create perimeters in case shapes are simplified
+    redistmetrics::prep_perims(shp = co_shp,
+        perim_path = here(perim_path)) |>
+        invisible()
+
+    # simplifies geometry for faster processing, plotting, and smaller shapefiles
+    # TODO feel free to delete if this dependency isn't available
+    if (requireNamespace("rmapshaper", quietly = TRUE)) {
+        co_shp <- rmapshaper::ms_simplify(co_shp, keep = 0.05,
+            keep_shapes = TRUE) |>
+            suppressWarnings()
+    }
+
+    # create adjacency graph
+    co_shp$adj <- redist.adjacency(co_shp)
+
+    # TODO any custom adjacency graph edits here
+
+    write_rds(co_shp, here(shp_path), compress = "gz")
+    cli_process_done()
+} else {
+    co_shp <- read_rds(here(shp_path))
+    cli_alert_success("Loaded {.strong CO} shapefile")
+}
