60f2df8fbcdb5f5df39db41d96138cd00a3736ec
Brian Zhou <87774511+brianlzhou@users.noreply.github.com>
2025-12-28 16:06:52 -0800

    init

---
+++ analyses/FL_cd_1990/01_prep_FL_cd_1990.R
@@ -0,0 +1,71 @@
+###############################################################################
+# Download and prepare data for `FL_cd_1990` analysis
+# © ALARM Project, December 2025
+###############################################################################
+
+suppressMessages({
+    library(dplyr)
+    library(readr)
+    library(sf)
+    library(redist)
+    library(geomander)
+    library(baf)
+    library(cli)
+    library(here)
+    devtools::load_all() # load utilities
+})
+
+# Download necessary files for analysis -----
+cli_process_start("Downloading files for {.pkg FL_cd_1990}")
+
+path_data <- download_redistricting_file("FL", "data-raw/FL", year = 1990)
+
+cli_process_done()
+
+# Compile raw data into a final shapefile for analysis -----
+shp_path <- "data-out/FL_1990/shp_vtd.rds"
+perim_path <- "data-out/FL_1990/perim.rds"
+
+if (!file.exists(here(shp_path))) {
+    cli_process_start("Preparing {.strong FL} shapefile")
+    # read in redistricting data
+    fl_shp <- read_csv(here(path_data), col_types = cols(GEOID = "c")) |>
+      mutate(
+        state  = as.character(state),
+        county = as.character(county),
+        tract  = as.character(tract)
+      ) |>
+      join_vtd_shapefile(year = 1990) |>
+      st_transform(EPSG$FL)
+
+    fl_shp <- fl_shp |>
+        rename(muni = place) |>
+        mutate(county_muni = if_else(is.na(muni), county, str_c(county, muni))) |>
+        relocate(muni, county_muni, cd_1980, .after = county)
+
+    # TODO any additional columns or data you want to add should go here
+
+    # Create perimeters in case shapes are simplified
+    redistmetrics::prep_perims(shp = fl_shp,
+                               perim_path = here(perim_path)) |>
+        invisible()
+
+    # simplifies geometry for faster processing, plotting, and smaller shapefiles
+    # TODO feel free to delete if this dependency isn't available
+    if (requireNamespace("rmapshaper", quietly = TRUE)) {
+        fl_shp <- rmapshaper::ms_simplify(fl_shp, keep = 0.05,
+                                                 keep_shapes = TRUE) |>
+            suppressWarnings()
+    }
+
+    # create adjacency graph
+    fl_shp$adj <- redist.adjacency(fl_shp)
+
+    # TODO any custom adjacency graph edits here
+
+    write_rds(fl_shp, here(shp_path), compress = "gz")
+    cli_process_done()
+} else {
+    fl_shp <- read_rds(here(shp_path))
+    cli_alert_success("Loaded {.strong FL} shapefile")
+}
