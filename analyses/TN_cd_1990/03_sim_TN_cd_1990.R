4f199b524024813b2692923c0c8842a6a1e86360
Kento Tamaki <kt794@georgetown.edu>
2026-01-10 20:07:20 +0900

    Update 03_sim_TN_cd_1990.R

--- analyses/TN_cd_1990/03_sim_TN_cd_1990.R
+++ analyses/TN_cd_1990/03_sim_TN_cd_1990.R
@@ -6,8 +6,22 @@
 # Run the simulation -----
 cli_process_start("Running simulations for {.pkg TN_cd_1990}")
 
+sampling_space_val <- tryCatch(
+  getFromNamespace("LINKING_EDGE_SPACE", "redist"),
+  error = function(e) "linking_edge"
+)
+
 set.seed(1990)
-plans <- redist_smc(map, nsims = 2e3, runs = 5, counties = pseudo_county)
+plans <- redist_smc(
+  map,
+  nsims = 6e3,
+  runs = 5,
+  counties = pseudo_county,
+  constraints = constr,
+  sampling_space = sampling_space_val,
+  ms_params = list(frequency = 1L, mh_accept_per_smc = 60),
+  split_params = list(splitting_schedule = "any_valid_sizes")
+)
 
 plans <- plans %>%
   group_by(chain) %>%
