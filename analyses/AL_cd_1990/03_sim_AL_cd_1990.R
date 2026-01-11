001733a562d6f65fd69b8c124bbb745efab27d72
Brian Zhou <87774511+brianlzhou@users.noreply.github.com>
2026-01-11 01:37:25 -0800

    AL

---
+++ analyses/AL_cd_1990/03_sim_AL_cd_1990.R
@@ -0,0 +1,86 @@
+###############################################################################
+# Simulate plans for `AL_cd_1990`
+# © ALARM Project, January 2026
+###############################################################################
+
+# Run the simulation -----
+cli_process_start("Running simulations for {.pkg AL_cd_1990}")
+
+cli_process_start("Running simulations for {.pkg AL_cd_1990}")
+
+constr <- redist_constr(map) %>%
+  add_constr_grp_hinge(30, vap_black, vap, 0.40) %>%
+  add_constr_grp_hinge(-30, vap_black, vap, 0.33)
+
+set.seed(1990)
+plans <- redist_smc(map, nsims = 10e3, runs = 2L,
+                    counties = county, constr = constr, pop_temper = 0.05)
+plans <- match_numbers(plans, "cd_1990")
+
+# Subset plans that are not performing
+n_perf <- plans %>%
+  mutate(bvap = group_frac(map, vap_black, vap),
+         ndshare = group_frac(map, ndv, nrv + ndv)) %>%
+  group_by(draw) %>%
+  summarize(n_blk_perf = sum(bvap > 0.3 & ndshare > 0.5))
+
+plans_5k <- plans %>%
+  # subset non-performing plan
+  anti_join(filter(n_perf, n_blk_perf == 0), by = "draw") %>%
+  # thin to 5000 draws
+  group_by(chain) %>%
+  filter(as.integer(draw) < min(as.integer(draw)) + 2500) %>%
+  ungroup()
+cli_process_done()
+cli_process_start("Saving {.cls redist_plans} object")
+
+# Output the redist_map object. Do not edit this path.
+write_rds(plans, here("data-out/AL_1990/AL_cd_1990_plans.rds"), compress = "xz")
+cli_process_done()
+
+# Compute summary statistics -----
+cli_process_start("Computing summary statistics for {.pkg AL_cd_1990}")
+
+plans <- add_summary_stats(plans, map)
+
+# Output the summary statistics. Do not edit this path.
+save_summary_stats(plans, "data-out/AL_1990/AL_cd_1990_stats.csv")
+
+cli_process_done()
+
+# Extra validation plots for custom constraints -----
+if (interactive()) {
+    library(ggplot2)
+    library(patchwork)
+
+    validate_analysis(plans, map)
+    summary(plans)
+
+    redist.plot.distr_qtys(
+      plans, vap_black/total_vap,
+      color_thresh = NULL,
+      color = ifelse(
+        subset_sampled(plans)$ndv > subset_sampled(plans)$nrv,
+        "#3D77BB", "#B25D4C"),
+      size = 0.5, alpha = 0.5) +
+      scale_y_continuous("Percent Black by VAP") +
+      labs(title = "Partisanship of seats by BVAP rank") +
+      scale_color_manual(values = c(cd_2000 = "black"))
+
+    # Dem seats by BVAP rank -- numeric
+    plans %>%
+      group_by(draw) %>%
+      mutate(bvap = vap_black/total_vap, bvap_rank = rank(bvap)) %>%
+      subset_sampled() %>%
+      select(draw, district, bvap, bvap_rank, ndv, nrv) %>%
+      mutate(dem = ndv > nrv) %>%
+      group_by(bvap_rank) %>%
+      summarize(dem = mean(dem))
+
+    # Total Black districts that are performing
+    plans %>%
+      subset_sampled() %>%
+      group_by(draw) %>%
+      summarize(n_black_perf = sum(vap_black/total_vap > 0.3 & ndshare > 0.5)) %>%
+      count(n_black_perf)
+}
