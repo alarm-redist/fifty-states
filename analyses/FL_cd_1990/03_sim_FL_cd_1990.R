178fd7a502cc8e2ae5f437614925b6975f30cc1b
Brian Zhou <87774511+brianlzhou@users.noreply.github.com>
2026-01-21 17:21:13 -0800

    Update 03_sim_FL_cd_1990.R

--- analyses/FL_cd_1990/03_sim_FL_cd_1990.R
+++ analyses/FL_cd_1990/03_sim_FL_cd_1990.R
@@ -6,26 +6,68 @@
 # Run the simulation -----
 cli_process_start("Running simulations for {.pkg FL_cd_1990}")
 
-# TODO any pre-computation (VRA targets, etc.)
+HISP_STRONG <- 0.60  # 2 districts
+BVAP_STRONG <- 0.50  # 2 districts
+BVAP_OPP <- 0.40  # 1 district
 
-# TODO customize as needed. Recommendations:
-#  - For many districts / tighter population tolerances, try setting
-#  `pop_temper=0.01` and nudging upward from there. Monitor the output for
-#  efficiency!
-#  - Monitor the output (i.e. leave `verbose=TRUE`) to ensure things aren't breaking
-#  - Don't change the number of simulations unless you have a good reason
-#  - If the sampler freezes, try turning off the county split constraint to see
-#  if that's the problem.
-#  - Ask for help!
+ndists <- attr(map, "ndists")
+
+constr <- redist_constr(map) |>
+  # Hispanic CVAP: push for >= 0.60 in at least 2 districts
+  add_constr_min_group_frac(
+    strength      = 1,
+    group_pops    = list(map$vap_hisp),
+    total_pops    = list(map$vap),
+    min_fracs     = c(HISP_STRONG),
+    thresh        = -0.9,
+    only_nregions = seq.int(2L, ndists)
+  ) |>
+  # Black VAP: push for >= 0.50 in at least 2 districts
+  add_constr_min_group_frac(
+    strength      = 1,
+    group_pops    = list(map$vap_black),
+    total_pops    = list(map$vap),
+    min_fracs     = c(BVAP_STRONG),
+    thresh        = -0.9,
+    only_nregions = seq.int(2L, ndists)
+  ) |>
+  # Black VAP: push for >= 0.40 in at least 3 districts
+  add_constr_min_group_frac(
+    strength      = 1,
+    group_pops    = list(map$vap_black),
+    total_pops    = list(map$vap),
+    min_fracs     = c(BVAP_OPP),
+    thresh        = -1.9,
+    only_nregions = seq.int(3L, ndists)
+  ) |>
+  # anti-packing: discourage too many supermajority Hispanic districts
+  add_constr_grp_inv_hinge(
+    2,                 # allow up to 2 packed districts
+    vap_hisp,
+    total_pop = vap,
+    tgts_group = c(0.7)
+  )
+
 set.seed(1990)
-plans <- redist_smc(map, nsims = 2e3, runs = 5, counties = county)
-# IF CORES OR OTHER UNITS HAVE BEEN MERGED:
-# make sure to call `pullback()` on this plans object!
+plans <- redist_smc(
+  map,
+  nsims = 2e3,
+  runs = 6,
+  counties = pseudo_county,
+  constraints = constr,
+  split_params = list(splitting_schedule = "any_valid_sizes"),
+  sampling_space = "spanning_forest",
+  ms_params = list(ms_frequency = 1L, ms_moves_multiplier = 5L),
+  ncores = 112,
+  pop_temper = 0.01,
+  seq_alpha = 0.95,
+  verbose = TRUE
+)
 
 plans <- plans |>
-    group_by(chain) |>
-    filter(as.integer(draw) < min(as.integer(draw)) + 1000) |> # thin samples
-    ungroup()
+  group_by(chain) |>
+  filter(as.integer(draw) < min(as.integer(draw)) + 1000) |> # thin samples
+  ungroup()
 plans <- match_numbers(plans, "cd_1990")
 
 cli_process_done()
