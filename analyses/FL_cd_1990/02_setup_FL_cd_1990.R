db93fc6df617a47f206bc9b55ecbddde69ac2cb3
Kento Tamaki <kt794@georgetown.edu>
2026-01-24 19:45:46 +0900

    Add files via upload

--- analyses/FL_cd_1990/02_setup_FL_cd_1990.R
+++ analyses/FL_cd_1990/02_setup_FL_cd_1990.R
@@ -1,26 +1,21 @@
 ###############################################################################
 # Set up redistricting simulation for `FL_cd_1990`
-# © ALARM Project, December 2025
+# © ALARM Project, January 2026
 ###############################################################################
 cli_process_start("Creating {.cls redist_map} object for {.pkg FL_cd_1990}")
 
-# TODO any pre-computation (usually not necessary)
-
 map <- redist_map(fl_shp, pop_tol = 0.005,
-    existing_plan = cd_1990, adj = fl_shp$adj)
+                  existing_plan = cd_1990, adj = fl_shp$adj)
 
-# TODO any filtering, cores, merging, etc.
-
-# TODO remove if not necessary. Adjust pop_muni as needed to balance county/muni splits
-# make pseudo counties with default settings
-map <- map |>
-    mutate(pseudo_county = pick_county_muni(map, counties = county, munis = muni,
-                                            pop_muni = get_target(map)))
-# IF MERGING CORES OR OTHER UNITS:
-# make a new `map_cores` object that is merged & used for simulating. You can set `drop_geom=TRUE` for this.
+# pseudo-county constraint
+map <- map %>%
+  mutate(pseudo_county = pick_county_muni(map, counties = county, munis = muni,
+                                          pop_muni = get_target(map)))
 
 # Add an analysis name attribute
 attr(map, "analysis_name") <- "FL_1990"
+
+map$state <- "FL"
 
 # Output the redist_map object. Do not edit this path.
 write_rds(map, "data-out/FL_1990/FL_cd_1990_map.rds", compress = "xz")
--- analyses/FL_cd_1990/03_sim_FL_cd_1990.R
+++ analyses/FL_cd_1990/03_sim_FL_cd_1990.R
@@ -1,87 +1,56 @@
 ###############################################################################
 # Simulate plans for `FL_cd_1990`
-# © ALARM Project, December 2025
+# © ALARM Project, January 2026
 ###############################################################################
 
 # Run the simulation -----
 cli_process_start("Running simulations for {.pkg FL_cd_1990}")
 
-HISP_STRONG <- 0.60  # 2 districts
-BVAP_STRONG <- 0.50  # 2 districts
-BVAP_OPP <- 0.40  # 1 district
-BVAP_LOW <- 0.10
-
-ndists <- attr(map, "ndists")
-
-constr <- redist_constr(map) |>
-  # Hispanic CVAP: push for >= 0.60 in at least 2 districts
-  add_constr_min_group_frac(
-    strength      = 1,
-    group_pops    = list(map$vap_hisp),
-    total_pops    = list(map$vap),
-    min_fracs     = c(HISP_STRONG),
-    thresh        = -0.9,
-    only_nregions = seq.int(2L, ndists)
-  ) |>
-  # Black VAP: push for >= 0.50 in at least 2 districts
-  add_constr_min_group_frac(
-    strength      = 4,
-    group_pops    = list(map$vap_black),
-    total_pops    = list(map$vap),
-    min_fracs     = c(BVAP_STRONG),
-    thresh        = -0.9,
-    only_nregions = seq.int(2L, ndists)
-  ) |>
-  # Black VAP: push for >= 0.40 in at least 3 districts
-  add_constr_min_group_frac(
-    strength      = 4,
-    group_pops    = list(map$vap_black),
-    total_pops    = list(map$vap),
-    min_fracs     = c(BVAP_OPP),
-    thresh        = -1.9,
-    only_nregions = seq.int(3L, ndists)
-  ) |>
-  # anti-cracking: encourage more districts with less BVAP share
-  add_constr_grp_inv_hinge(
-    7,
-    vap_black,
-    total_pop = vap,
-    tgts_group = c(BVAP_LOW)
-  ) |>
-  # anti-packing: discourage too many supermajority Hispanic districts
-  add_constr_grp_inv_hinge(
-    2,
+# Hinge constraints
+constraints <- redist_constr(map) %>%
+  add_constr_grp_hinge(5,  vap_black, vap, .45) %>%
+  add_constr_grp_hinge(-7, vap_black, vap, .2)  %>%
+  add_constr_grp_hinge(5,  vap_hisp,  vap, .55) %>%
+  add_constr_grp_hinge(-7, vap_hisp,  vap, .25) %>%
+  add_constr_grp_hinge(
+    12,
     vap_hisp,
     total_pop = vap,
-    tgts_group = c(0.7)
+    tgts_group = c(0.50)
+  ) %>%
+  add_constr_grp_hinge(
+    12,
+    vap_black,
+    total_pop = vap,
+    tgts_group = c(0.50)
   )
+
+sampling_space_val <- tryCatch(getFromNamespace("LINKING_EDGE_SPACE", "redist"),
+                               error = function(e) "linking_edge")
 
 set.seed(1990)
 plans <- redist_smc(
-  map,
-  nsims = 20000,
-  runs = 10,
+  map, nsims = 1200, runs = 5,
   counties = pseudo_county,
-  constraints = constr,
+  constraints = constraints,
+  pop_temper = 0.05, seq_alpha = 0.95,
+  sampling_space = sampling_space_val,
+  ms_params = list(frequency = 1L, mh_accept_per_smc = 65),
   split_params = list(splitting_schedule = "any_valid_sizes"),
-  sampling_space = "spanning_forest",
-  ms_params = list(ms_frequency = 5L, ms_moves_multiplier = 20L),
-  ncores = 112,
-  pop_temper = 0.01,
-  seq_alpha = 0.95,
-  verbose = TRUE
+  verbose = T
 )
 
-plans <- plans |>
-  group_by(chain) |>
-  filter(as.integer(draw) < min(as.integer(draw)) + 1000) |> # thin samples
+plans <- plans %>% filter(draw != "cd_1990") %>%
+  group_by(chain) %>%
+  filter(as.integer(draw) < min(as.integer(draw)) + 1000) %>% # thin samples
   ungroup()
+
+plans <- plans %>% add_reference(ref_plan = map$cd_1990)
+
 plans <- match_numbers(plans, "cd_1990")
 
 cli_process_done()
 cli_process_start("Saving {.cls redist_plans} object")
-
-# TODO add any reference plans that aren't already included
 
 # Output the redist_map object. Do not edit this path.
 write_rds(plans, here("data-out/FL_1990/FL_cd_1990_plans.rds"), compress = "xz")
@@ -97,81 +66,108 @@ save_summary_stats(plans, "data-out/FL_1990/FL_cd_1990_stats.csv")
 
 cli_process_done()
 
-# read in from fasrc (MAKE SURE TO RERUN add_summary_stats)
-plans <- read_rds(
-  here("data-out/FL_1990/FL_cd_1990_plans.rds")
-)
-
-map <- read_rds(
-  here("data-out/FL_1990/FL_cd_1990_map.rds")
-)
-
-stats <- read_csv(
-  here("data-out/FL_1990/FL_cd_1990_stats.csv")
-)
-
 # Extra validation plots for custom constraints -----
-# TODO remove this section if no custom constraints
 if (interactive()) {
-    library(ggplot2)
-    library(patchwork)
-
-    validate_analysis(plans, map)
-    summary(plans)
-
+  library(ggplot2)
+  library(patchwork)
+  
+  ## VAP charts
+  d1 <- redist.plot.distr_qtys(
+    plans,
+    vap_black/total_vap,
+    color_thresh = NULL,
+    color = ifelse(
+      subset_sampled(plans)$ndv > subset_sampled(plans)$nrv,
+      "#3D77BB",
+      "#B25D4C"
+    ),
+    size = 0.5,
+    alpha = 0.5
+  ) +
+    scale_y_continuous("Percent Black by VAP") +
+    labs(title = "FL Proposed Plan versus Simulations") +
+    scale_color_manual(values = c(cd_1990 = "black"))
+  
+  d2 <- redist.plot.distr_qtys(
+    plans,
+    vap_hisp/total_vap,
+    color_thresh = NULL,
+    color = ifelse(
+      subset_sampled(plans)$ndv > subset_sampled(plans)$nrv,
+      "#3D77BB",
+      "#B25D4C"
+    ),
+    size = 0.5,
+    alpha = 0.5
+  ) +
+    scale_y_continuous("Percent Hispanic by VAP") +
+    labs(title = "FL Proposed Plan versus Simulations") +
+    scale_color_manual(values = c(cd_1990 = "black"))
+  
+  d3 <-
     redist.plot.distr_qtys(
-      plans, vap_black/total_vap,
+      plans,
+      (vap_hisp + vap_black)/total_vap,
       color_thresh = NULL,
       color = ifelse(
         subset_sampled(plans)$ndv > subset_sampled(plans)$nrv,
-        "#3D77BB", "#B25D4C"),
-      size = 0.5, alpha = 0.5) +
-      scale_y_continuous("Percent Black by VAP") +
-      labs(title = "Partisanship of seats by BVAP rank") +
-      scale_color_manual(values = c(cd_1990 = "black"))
-
-    redist.plot.distr_qtys(
-      plans, vap_hisp/total_vap,
-      color_thresh = NULL,
-      color = ifelse(
-        subset_sampled(plans)$ndv > subset_sampled(plans)$nrv,
-        "#3D77BB", "#B25D4C"),
-      size = 0.5, alpha = 0.5) +
-      scale_y_continuous("Percent Hispanic by VAP") +
-      labs(title = "Partisanship of seats by HVAP rank") +
-      scale_color_manual(values = c(cd_1990 = "black"))
-
-    # Dem seats by BVAP rank -- numeric
-    plans %>%
-      group_by(draw) %>%
-      mutate(bvap = vap_black/total_vap, bvap_rank = rank(bvap)) %>%
-      subset_sampled() %>%
-      select(draw, district, bvap, bvap_rank, ndv, nrv) %>%
-      mutate(dem = ndv > nrv) %>%
-      group_by(bvap_rank) %>%
-      summarize(dem = mean(dem))
-
-    # Dem seats by HVAP rank -- numeric
-    plans %>%
-      group_by(draw) %>%
-      mutate(hvap = vap_hisp/total_vap, hvap_rank = rank(hvap)) %>%
-      subset_sampled() %>%
-      select(draw, district, hvap, hvap_rank, ndv, nrv) %>%
-      mutate(dem = ndv > nrv) %>%
-      group_by(hvap_rank) %>%
-      summarize(dem = mean(dem))
-
-    # Total Black districts that are performing
-    plans %>%
-      subset_sampled() %>%
-      group_by(draw) %>%
-      summarize(n_black_perf = sum(vap_black/total_vap > 0.3 & ndshare > 0.5)) %>%
-      count(n_black_perf)
-
-    # Total Hispanic districts that are performing
-    plans %>%
-      subset_sampled() %>%
-      group_by(draw) %>%
-      summarize(n_hisp_perf = sum(vap_hisp/total_vap > 0.3 & ndshare > 0.5)) %>%
-      count(n_hisp_perf)
+        "#3D77BB",
+        "#B25D4C"
+      ),
+      size = 0.5,
+      alpha = 0.5
+    ) +
+    scale_y_continuous("HVAP + BVAP / VAP") +
+    labs(title = "FL Proposed Plan versus Simulations") +
+    scale_color_manual(values = c(cd_1990 = "black"))
+  
+  ggsave(
+    plot = d1/d2,
+    filename = "data-raw/FL/vap_plots.png",
+    height = 9,
+    width = 9
+  )
+  ggsave(
+    plot = d3,
+    filename = "data-raw/FL/vap_sum_plots.png",
+    height = 9,
+    width = 9
+  )
+  
+  # Minority opportunity district histograms
+  psum <- plans %>%
+    group_by(draw) %>%
+    mutate(vap_nonwhite = total_vap - vap_white) %>%
+    summarise(
+      all_hvap = sum((vap_hisp/total_vap) > 0.4),
+      dem_hvap = sum((vap_hisp/total_vap) > 0.4 &
+                       (ndv > nrv)),
+      rep_hvap = sum((vap_hisp/total_vap) > 0.4 &
+                       (nrv > ndv)),
+      all_bvap_40 = sum((vap_black/total_vap) > 0.4),
+      all_bvap_25 = sum((vap_black/total_vap) > 0.25),
+      dem_bvap_25 = sum((vap_black/total_vap) > .25 & (ndv > nrv)),
+      mmd_all = sum(vap_nonwhite/total_vap > 0.5),
+      mmd_coalition = sum(((
+        vap_hisp + vap_black
+      )/total_vap) > 0.5)
+    )
+  
+  p1 <-
+    redist.plot.hist(psum, mmd_coalition) + labs(x = "HVAP + BVAP > 0.5", y = NULL)
+  p2 <-
+    redist.plot.hist(psum, all_hvap) + labs(x = "HVAP > 0.4", y = NULL)
+  p3 <-
+    redist.plot.hist(psum, dem_hvap) + labs(x = "HVAP > 0.4 & Dem > Rep", y = NULL)
+  p4 <-
+    redist.plot.hist(psum, rep_hvap) + labs(x = "HVAP > 0.4 & Dem < Rep", y = NULL)
+  p5 <-
+    redist.plot.hist(psum, all_bvap_40) + labs(x = "BVAP > 0.4", y = NULL)
+  p6 <-
+    redist.plot.hist(psum, all_bvap_25) + labs(x = "BVAP > 0.25", y = NULL)
+  p7 <-
+    redist.plot.hist(psum, dem_bvap_25) + labs(x = "BVAP > 0.25 & Dem > Rep", y = NULL)
+  
+  ggsave("data-raw/FL/vap_histograms.png", p1/p2/p3/p4/p5/p6/p7, height = 10)
+  
 }
