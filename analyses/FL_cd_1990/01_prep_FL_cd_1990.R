ce7dd468a71b70fb21cbd7bb145218f27b99b282
Brian Zhou <87774511+brianlzhou@users.noreply.github.com>
2026-01-24 21:12:03 -0800

    ADD leip

--- analyses/FL_cd_1990/01_prep_FL_cd_1990.R
+++ analyses/FL_cd_1990/01_prep_FL_cd_1990.R
@@ -43,15 +43,12 @@ if (!file.exists(here(shp_path))) {
         mutate(county_muni = if_else(is.na(muni), county, str_c(county, muni))) |>
         relocate(muni, county_muni, cd_1980, .after = county)
 
-    # TODO any additional columns or data you want to add should go here
-
     # Create perimeters in case shapes are simplified
     redistmetrics::prep_perims(shp = fl_shp,
                                perim_path = here(perim_path)) |>
         invisible()
 
     # simplifies geometry for faster processing, plotting, and smaller shapefiles
-    # TODO feel free to delete if this dependency isn't available
     if (requireNamespace("rmapshaper", quietly = TRUE)) {
         fl_shp <- rmapshaper::ms_simplify(fl_shp, keep = 0.05,
                                                  keep_shapes = TRUE) |>
@@ -61,7 +58,36 @@ if (!file.exists(here(shp_path))) {
     # create adjacency graph
     fl_shp$adj <- redist.adjacency(fl_shp)
 
-    # TODO any custom adjacency graph edits here
+    ###############################################################################
+    # Logit-shift ndv/nrv to match 1990 LEIP county results
+    ###############################################################################
+
+    # 1. Load the LEIP county CSV as `leip_cty` ----
+    leip_cty <- read_csv(
+      here("data-raw/baseline_voteshare_leip_92.csv"),
+      show_col_types = FALSE
+    )
+
+    # 2. Add county_fips column based on VTD GEOID ----
+    fl_shp <- fl_shp |>
+      mutate(county_fips = stringr::str_sub(GEOID, 1, 5))
+
+    names(fl_shp)
+
+    # 3. For each county, logit-shift ndv/nrv to the 2000 target from MEDSL ----
+    fl_shp <- fl_shp |>
+      group_by(county_fips) |>
+      group_split() |>
+      lapply(function(x) {
+        meds <- leip_cty |>
+          filter(county == x$county_fips[1])
+        target <- meds$dshare_00[1]
+
+        if (is.na(target)) return(x)
+
+        logit_shift_baseline(x, ndv = ndv, nrv = nrv, target = target)
+      }) |>
+      bind_rows()
 
     write_rds(fl_shp, here(shp_path), compress = "gz")
     cli_process_done()
