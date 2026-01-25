a29252fb490d36e1c81219c48114c776cacd5012
Brian Zhou <87774511+brianlzhou@users.noreply.github.com>
2026-01-25 01:38:22 -0800

    Implement logit-shift for 1990 LEIP county results
    Added logit-shift functionality to adjust ndv/nrv based on 1990 LEIP county results, including loading CSV data and processing by county.

--- analyses/MO_cd_1990/01_prep_MO_cd_1990.R
+++ analyses/MO_cd_1990/01_prep_MO_cd_1990.R
@@ -54,6 +54,37 @@ if (!file.exists(here(shp_path))) {
     # create adjacency graph
     mo_shp$adj <- redist.adjacency(mo_shp)
 
+    ###############################################################################
+    # Logit-shift ndv/nrv to match 1990 LEIP county results
+    ###############################################################################
+
+    # 1. Load the LEIP county CSV as `leip_cty` ----
+    leip_cty <- read_csv(
+      here("data-raw/baseline_voteshare_leip_92.csv"),
+      show_col_types = FALSE
+    )
+
+    # 2. Add county_fips column based on VTD GEOID ----
+    mo_shp <- mo_shp |>
+      mutate(county_fips = stringr::str_sub(GEOID, 1, 5))
+
+    names(al_shp)
+
+    # 3. For each county, logit-shift ndv/nrv to the 2000 target from MEDSL ----
+    mo_shp <- mo_shp |>
+      group_by(county_fips) |>
+      group_split() |>
+      lapply(function(x) {
+        meds <- leip_cty |>
+          filter(county == x$county_fips[1])
+        target <- meds$dshare_92[1]
+
+        if (is.na(target)) return(x)
+
+        logit_shift_baseline(x, ndv = ndv, nrv = nrv, target = target)
+      }) |>
+      bind_rows()
+
     write_rds(mo_shp, here(shp_path), compress = "gz")
     cli_process_done()
 } else {
