951e83f9c4dba85c7498049a9b06c18019c76b9c
Brian Zhou <87774511+brianlzhou@users.noreply.github.com>
2026-02-01 18:21:01 -0500

    Update 03_sim_AL_cd_1990.R

--- analyses/AL_cd_1990/03_sim_AL_cd_1990.R
+++ analyses/AL_cd_1990/03_sim_AL_cd_1990.R
@@ -26,6 +26,7 @@ plans <- redist_smc(map, nsims = 3e3, runs = 6,
                     sampling_space = "spanning_forest",
                     ms_params = list(frequency = 1, mh_accept_per_smc = 50),
                     pop_temper = 0.01,
+                    ncores = 112,
                     verbose = TRUE)
 
 plans <- plans %>%
