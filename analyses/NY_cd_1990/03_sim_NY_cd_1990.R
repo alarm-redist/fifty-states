b01e2fb003ec3e9d26b681df67f3b73df8fcc5a2
Christopher T. Kenny <ctk44@cornell.edu>
2026-02-05 10:56:01 -0500

    Update 03_sim_NY_cd_1990.R

--- analyses/NY_cd_1990/03_sim_NY_cd_1990.R
+++ analyses/NY_cd_1990/03_sim_NY_cd_1990.R
@@ -47,7 +47,6 @@ save_summary_stats(plans, "data-out/NY_1990/NY_cd_1990_stats.csv")
 cli_process_done()
 
 # Extra validation plots for custom constraints -----
-# TODO remove this section if no custom constraints
 if (interactive()) {
     library(ggplot2)
     library(patchwork)
