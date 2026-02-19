8994ae36e5faeb10ad65f8fc07dcde5e01ef4395
Brian Zhou <87774511+brianlzhou@users.noreply.github.com>
2026-02-19 00:07:39 -0800

    Update 03_sim_MI_cd_1990.R

--- analyses/MI_cd_1990/03_sim_MI_cd_1990.R
+++ analyses/MI_cd_1990/03_sim_MI_cd_1990.R
@@ -47,6 +47,18 @@ cli_process_start("Saving {.cls redist_plans} object")
 write_rds(plans, here("data-out/MI_1990/MI_cd_1990_plans.rds"), compress = "xz")
 cli_process_done()
 
+# read in from FASRC
+map <- readRDS(
+  here("data-out/MI_1990/MI_cd_1990_map.rds")
+)
+plans <- readRDS(
+  here("data-out/MI_1990/MI_cd_1990_plans.rds")
+)
+stats <- read_csv(
+  here("data-out/MI_1990/MI_cd_1990_stats.csv"),
+  show_col_types = FALSE
+)
+
 # Compute summary statistics -----
 cli_process_start("Computing summary statistics for {.pkg MI_cd_1990}")
 
@@ -64,4 +76,32 @@ if (interactive()) {
 
     validate_analysis(plans, map)
     summary(plans)
+
+    redist.plot.distr_qtys(
+      plans, vap_black/total_vap,
+      color_thresh = NULL,
+      color = ifelse(
+        subset_sampled(plans)$ndv > subset_sampled(plans)$nrv,
+        "#3D77BB", "#B25D4C"),
+      size = 0.5, alpha = 0.5) +
+      scale_y_continuous("Percent Black by VAP") +
+      labs(title = "Partisanship of seats by BVAP rank") +
+      scale_color_manual(values = c(cd_2000 = "black"))
+
+    # Dem seats by BVAP rank -- numeric
+    plans %>%
+      group_by(draw) %>%
+      mutate(bvap = vap_black/total_vap, bvap_rank = rank(bvap)) %>%
+      subset_sampled() %>%
+      select(draw, district, bvap, bvap_rank, ndv, nrv) %>%
+      mutate(dem = ndv > nrv) %>%
+      group_by(bvap_rank) %>%
+      summarize(dem = mean(dem))
+
+    # Total Black districts that are performing
+    plans %>%
+      subset_sampled() %>%
+      group_by(draw) %>%
+      summarize(n_black_perf = sum(vap_black/total_vap > 0.3 & ndshare > 0.5)) %>%
+      count(n_black_perf)
 }
