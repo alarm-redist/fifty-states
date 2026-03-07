d01cdd29388d7b33040cfad30e07b617f157b579
Kento Tamaki <kt794@georgetown.edu>
2026-03-08 00:56:25 +0900

    Update 02_setup_LA_cd_1990.R

--- analyses/LA_cd_1990/02_setup_LA_cd_1990.R
+++ analyses/LA_cd_1990/02_setup_LA_cd_1990.R
@@ -1,11 +1,11 @@
 ###############################################################################
 # Set up redistricting simulation for `LA_cd_1990`
-# © ALARM Project, November 2025
+# © ALARM Project, February 2026
 ###############################################################################
 cli_process_start("Creating {.cls redist_map} object for {.pkg LA_cd_1990}")
 
 map <- redist_map(la_shp, pop_tol = 0.005,
-                  existing_plan = cd_1990, adj = la_shp$adj)
+    existing_plan = cd_1990, adj = la_shp$adj)
 
 # Add an analysis name attribute
 attr(map, "analysis_name") <- "LA_1990"
