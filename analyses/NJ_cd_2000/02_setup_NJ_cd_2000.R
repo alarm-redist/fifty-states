4c259b4362ac42ff4963d384cacc66d649c13a93
Kento Tamaki <kt794@georgetown.edu>
2026-03-14 16:04:27 +0900

    Update 02_setup_NJ_cd_2000.R

--- analyses/NJ_cd_2000/02_setup_NJ_cd_2000.R
+++ analyses/NJ_cd_2000/02_setup_NJ_cd_2000.R
@@ -1,24 +1,33 @@
 ###############################################################################
 # Set up redistricting simulation for `NJ_cd_2000`
-# © ALARM Project, February 2026
+# © ALARM Project, March 2026
 ###############################################################################
 cli_process_start("Creating {.cls redist_map} object for {.pkg NJ_cd_2000}")
 
 map <- redist_map(nj_shp, pop_tol = 0.005,
-                  existing_plan = cd_2000, adj = nj_shp$adj)
+    existing_plan = cd_2000, adj = nj_shp$adj)
 
 map <- map %>% merge_by(merge_group, drop_geom = FALSE)
 
+# Extract merged geometry as sf
+merged_sf <- sf::st_as_sf(map)
+
+redistmetrics::prep_perims(
+    shp = merged_sf,
+    perim_path = here(perim_path)
+) %>%
+    invisible()
+
 # Pseudo-county
 map <- map %>%
-  mutate(
-    pseudo_county = pick_county_muni(
-      map,
-      counties = county,   
-      munis    = muni,     
-      pop_muni = get_target(map)
+    mutate(
+        pseudo_county = pick_county_muni(
+            map,
+            counties = county,
+            munis    = muni,
+            pop_muni = get_target(map)
+        )
     )
-  )
 
 # Add an analysis name attribute
 attr(map, "analysis_name") <- "NJ_2000"
