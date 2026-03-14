1a74ea053059663427fdc08f46a3c1454055e421
Kento Tamaki <kt794@georgetown.edu>
2026-03-14 16:04:53 +0900

    Update 03_sim_NJ_cd_2000.R

--- analyses/NJ_cd_2000/03_sim_NJ_cd_2000.R
+++ analyses/NJ_cd_2000/03_sim_NJ_cd_2000.R
@@ -1,35 +1,41 @@
-###############################################################################
+################################################################################
 # Simulate plans for `NJ_cd_2000`
-# © ALARM Project, February 2026
+# © ALARM Project, March 2026
 ###############################################################################
 
 # Run the simulation -----
 cli_process_start("Running simulations for {.pkg NJ_cd_2000}")
 
+BVAP_THRESH <- 0.30
+DEM_THRESH  <- 0.50
+ndists <- attr(map, "ndists")
+
+constr <- redist_constr(map) |>
+    add_constr_min_group_frac(
+        strength      = -1,
+        group_pops    = list(map$vap_black, map$ndv),
+        total_pops    = list(map$vap, map$nrv + map$ndv),
+        min_fracs     = c(BVAP_THRESH, DEM_THRESH),
+        thresh        = -0.9,
+        only_nregions = ndists
+    )
+
 set.seed(2000)
-plans <- redist_smc(map, nsims = 6e3, runs = 5, counties = pseudo_county)
+plans <- redist_smc(map,
+    nsims = 1e3,
+    runs = 5,
+    counties = pseudo_county,
+    constraints = constr,
+    sampling_space = "spanning_forest",
+    ms_params = list(frequency = 1L, mh_accept_per_smc = 20),
+    split_params = list(splitting_schedule = "any_valid_sizes"),
+    verbose = T, pop_temper = 0.01)
 
-# contiguity validation
-pmat  <- redist::get_plans_matrix(plans)
-ok    <- check_plans_polygon_contiguity(map, pmat)
-
-# Build a index in the same order as pmat columns
-draw_index <- plans %>%
-  dplyr::distinct(chain, draw) %>%
-  dplyr::arrange(chain, as.integer(draw))
-
-# Keep only the pairs that passed contiguity
-keep_pairs <- draw_index[ok, ]
-
-# Filter original plans object
 plans <- plans %>%
-  dplyr::inner_join(keep_pairs, by = c("chain", "draw"))
-
-# Thin samples
-plans <- plans |>
-  group_by(chain) |>
-  filter(as.integer(draw) < min(as.integer(draw)) + 1000) |> # thin samples
-  ungroup()
+    group_by(chain) %>%
+    filter(as.integer(draw) < min(as.integer(draw)) + 1000) %>% # thin samples
+    ungroup()
+plans <- match_numbers(plans, "cd_2000")
 
 cli_process_done()
 cli_process_start("Saving {.cls redist_plans} object")
@@ -47,25 +53,3 @@ plans <- add_summary_stats(plans, map)
 save_summary_stats(plans, "data-out/NJ_2000/NJ_cd_2000_stats.csv")
 
 cli_process_done()
-
-# Validation plots
-if (interactive()) {
-  library(ggplot2)
-  library(patchwork)
-  
-  # Black VAP Performance Plot  
-  redist.plot.distr_qtys(plans, vap_black / total_vap,
-                         color_thresh = NULL,
-                         color = ifelse(subset_sampled(plans)$ndv > subset_sampled(plans)$nrv, "#3D77BB", "#B25D4C"),
-                         size = 0.5, alpha = 0.5) +
-    scale_y_continuous("Percent Black by VAP") +
-    labs(title = "New Jersey Proposed Plan versus Simulations") +
-    scale_color_manual(values = c(cd_2000 = "black")) +
-    theme_bw()
-  
-  # Total Black districts that are performing
-  plans %>%
-    subset_sampled() %>%
-    group_by(draw) %>%
-    summarize(n_black_perf = sum(vap_black/total_vap > 0.3 & ndshare > 0.5)) %>%
-    count(n_black_perf)
