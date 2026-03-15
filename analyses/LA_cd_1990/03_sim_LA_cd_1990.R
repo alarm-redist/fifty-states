859a540430ad7a9082bd45e14cb14ca1298534c5
Kento Tamaki <kt794@georgetown.edu>
2026-03-15 12:45:23 +0900

    Update 03_sim_LA_cd_1990.R

--- analyses/LA_cd_1990/03_sim_LA_cd_1990.R
+++ analyses/LA_cd_1990/03_sim_LA_cd_1990.R
@@ -1,6 +1,6 @@
 ###############################################################################
 # Simulate plans for `LA_cd_1990`
-# © ALARM Project, February 2026
+# © ALARM Project, March 2026
 ###############################################################################
 
 # Run the simulation -----
@@ -8,64 +8,53 @@ cli_process_start("Running simulations for {.pkg LA_cd_1990}")
 
 BVAP_THRESH <- 0.30
 DEM_THRESH  <- 0.45
-ndists <- attr(map, "ndists")
 
 constr <- redist_constr(map) |>
     add_constr_grp_hinge(
         0.01,
         vap_black,
         vap,
-        BVAP_THRESH
-    ) |>
-    add_constr_min_group_frac(
-        strength      = -1,
-        group_pops    = list(map$vap_black, map$ndv),
-        total_pops    = list(map$vap, map$nrv + map$ndv),
-        min_fracs     = c(BVAP_THRESH, DEM_THRESH),
-        thresh        = -0.9,
-        only_nregions = seq.int(2, ndists)
-    ) |>
-    add_constr_min_group_frac(
-        strength      = -1,
-        group_pops    = list(map$vap_black, map$ndv),
-        total_pops    = list(map$vap, map$nrv + map$ndv),
-        min_fracs     = c(BVAP_THRESH, DEM_THRESH),
-        thresh        = -1.9,
-        only_nregions = ndists
-    )
+        BVAP_THRESH)
 
 set.seed(1990)
 plans <- redist_smc(
     map,
-    nsims = 1e3,
+    nsims = 2e4,
     runs = 5,
     counties = county,
-    constraints = constr,
-    split_params = list(splitting_schedule = "any_valid_sizes"),
-    sampling_space = "spanning_forest",
-    ms_params = list(frequency = 1L, mh_accept_per_smc = 80)
+    constraints = constr
 )
-
-plans <- plans |>
-    group_by(chain) |>
-    filter(as.integer(draw) < min(as.integer(draw)) + 1000) |> # thin samples
-    ungroup()
 plans <- match_numbers(plans, "cd_1990")
+
+# Subset plans that are not performing
+n_perf <- plans %>%
+  mutate(
+    bvap = group_frac(map, vap_black, vap),
+    ndshare = group_frac(map, ndv, nrv + ndv)
+  ) %>%
+  group_by(draw) %>%
+  summarize(n_black_perf = sum(bvap > BVAP_THRESH & ndshare > DEM_THRESH))
+
+plans_5k <- plans %>%
+  anti_join(filter(n_perf, n_black_perf == 0), by = "draw") %>%
+  group_by(chain) %>%
+  filter(as.integer(draw) < min(as.integer(draw)) + 1000) %>%
+  ungroup()
 
 cli_process_done()
 cli_process_start("Saving {.cls redist_plans} object")
 
 # Output the redist_map object. Do not edit this path.
-write_rds(plans, here("data-out/LA_1990/LA_cd_1990_plans.rds"), compress = "xz")
+write_rds(plans_5k, here("data-out/LA_1990/LA_cd_1990_plans.rds"), compress = "xz")
 cli_process_done()
 
 # Compute summary statistics -----
 cli_process_start("Computing summary statistics for {.pkg LA_cd_1990}")
 
-plans <- add_summary_stats(plans, map)
+plans_5k <- add_summary_stats(plans_5k, map)
 
 # Output the summary statistics. Do not edit this path.
-save_summary_stats(plans, "data-out/LA_1990/LA_cd_1990_stats.csv")
+save_summary_stats(plans_5k, "data-out/LA_1990/LA_cd_1990_stats.csv")
 
 cli_process_done()
 
