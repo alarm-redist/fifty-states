5793c2094f4ec5a7fe1acc034e40f76bc89cb049
Kento Tamaki <kt794@georgetown.edu>
2026-03-15 12:17:43 +0900

    Update 02_setup_MD_cd_2000.R

--- analyses/MD_cd_2000/02_setup_MD_cd_2000.R
+++ analyses/MD_cd_2000/02_setup_MD_cd_2000.R
@@ -1,27 +1,21 @@
 ###############################################################################
 # Set up redistricting simulation for `MD_cd_2000`
-# © ALARM Project, February 2026
+# © ALARM Project, March 2026
 ###############################################################################
 cli_process_start("Creating {.cls redist_map} object for {.pkg MD_cd_2000}")
 
 map <- redist_map(md_shp, pop_tol = 0.005,
     existing_plan = cd_2000, adj = md_shp$adj)
 
-map <- map %>% merge_by(merge_group, drop_geom = FALSE)
-
-# Extract merged geometry as sf
-merged_sf <- sf::st_as_sf(map)
-
-redistmetrics::prep_perims(
-    shp = merged_sf,
-    perim_path = here(perim_path)
-) %>%
-    invisible()
+# Merged map: use this only for simulation
+map_merged <- map %>%
+  merge_by(merge_group, drop_geom = FALSE)
 
 # Add an analysis name attribute
 attr(map, "analysis_name") <- "MD_2000"
 
 map$state <- "MD"
+map_merged$state <- "MD"
 
 # Output the redist_map object. Do not edit this path.
 write_rds(map, "data-out/MD_2000/MD_cd_2000_map.rds", compress = "xz")
