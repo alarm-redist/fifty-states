a33dbe04ee29ecdb4a6c68d7a1a15ff040d38e1f
Kento Tamaki <kt794@georgetown.edu>
2026-03-17 02:16:18 +0900

    Add files via upload

--- R/logit_shift.R
+++ R/logit_shift.R
@@ -4,6 +4,7 @@
 #' @param ndv Unquoted Democratic vote column name
 #' @param nrv Unquoted Republican vote column name
 #' @param target target to logit shift to
+#' @param interval interval passed to `uniroot()`
 #' @param tol
 #'
 #' @returns a data frame with adjusted vote columns
@@ -13,33 +14,34 @@
 #' # TODO
 logit_shift_baseline <- function(d_baseline, ndv, nrv,
                                  target = 0.5,
+                                 interval = c(-1, 1),
                                  tol = sqrt(.Machine$double.eps)) {
   if (missing(ndv) || missing(nrv)) {
     cli::cli_abort('Both {.arg ndv} and {.arg nrv} must be provided.')
   }
   ndv_q <- rlang::enquo(ndv)
   nrv_q <- rlang::enquo(nrv)
-
+  
   ndv_vec <- dplyr::pull(d_baseline, !!ndv_q)
   nrv_vec <- dplyr::pull(d_baseline, !!nrv_q)
-
+  
   turn <- ndv_vec + nrv_vec
-
+  
   if (sum(turn) == 0) {
     return(d_baseline)
   }
-
+  
   ldvs <- dplyr::if_else(turn > 0, log(ndv_vec) - log(nrv_vec), 0)
-
+  
   res <- uniroot(function(shift) {
     stats::weighted.mean(plogis(ldvs + shift), turn) - target
-  }, c(-1, 1), tol = tol)
-
+  }, interval = interval, tol = tol)
+  
   ldvs <- ldvs + res$root
-
+  
   ndv_new <- turn * plogis(ldvs)
   nrv_new <- turn - ndv_new
-
+  
   dplyr::mutate(
     d_baseline,
     !!rlang::as_name(ndv_q) := ndv_new,
