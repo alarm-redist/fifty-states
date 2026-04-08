cc57d584d38f2a93aa0e8e48b1770e2c0d66caf1
Christopher T. Kenny <ctk44@cornell.edu>
2026-04-08 11:30:21 -0400

    Cleanup tryCatch statements; fix misnamed files; run `enforce_style()` (#319)
    * remove try catch
    
    * rename files; merge duplicate for TN 2000
    
    * run the styler on 2000

--- analyses/GA_cd_2000/03_sim_GA_cd_2000.R
+++ analyses/GA_cd_2000/03_sim_GA_cd_2000.R
@@ -10,29 +10,29 @@ BVAP_THRESH  <- 0.30
 DEM_THRESH   <- 0.50
 ndists <- attr(map, "ndists")
 constr <- redist_constr(map) |>
-  add_constr_min_group_frac(
-    strength=-1,
-    group_pops=list(map$vap_black, map$ndv),
-    total_pops=list(map$vap, map$nrv + map$ndv),
-    min_fracs=c(BVAP_THRESH, DEM_THRESH),
-    thresh = -.9,
-    only_nregions = seq.int(2, ndists)
-  ) |> add_constr_min_group_frac(
-    strength=-1,
-    group_pops=list(map$vap_black, map$ndv),
-    total_pops=list(map$vap, map$nrv + map$ndv),
-    min_fracs=c(BVAP_THRESH, DEM_THRESH),
-    thresh = -1.9,
-    only_nregions = seq.int(5, ndists)
-  )
+    add_constr_min_group_frac(
+        strength = -1,
+        group_pops = list(map$vap_black, map$ndv),
+        total_pops = list(map$vap, map$nrv + map$ndv),
+        min_fracs = c(BVAP_THRESH, DEM_THRESH),
+        thresh = -.9,
+        only_nregions = seq.int(2, ndists)
+    ) |> add_constr_min_group_frac(
+        strength = -1,
+        group_pops = list(map$vap_black, map$ndv),
+        total_pops = list(map$vap, map$nrv + map$ndv),
+        min_fracs = c(BVAP_THRESH, DEM_THRESH),
+        thresh = -1.9,
+        only_nregions = seq.int(5, ndists)
+    )
 
 set.seed(2000)
 plans <- redist_smc(map, nsims = 2e3, runs = 6,
-                    counties = county, constraints=constr,
-                    split_params = list(splitting_schedule = "any_valid_sizes"),
-                    sampling_space = "spanning_forest",
-                    ms_params = list(frequency = 1, mh_accept_per_smc = 30),
-                    ncores = 0)
+    counties = county, constraints = constr,
+    split_params = list(splitting_schedule = "any_valid_sizes"),
+    sampling_space = "spanning_forest",
+    ms_params = list(frequency = 1, mh_accept_per_smc = 30),
+    ncores = 0)
 
 plans <- plans %>%
     group_by(chain) %>%
@@ -48,11 +48,11 @@ cli_process_start("Computing summary statistics for {.pkg GA_cd_2000}")
 plans <- add_summary_stats(plans, map)
 
 plans <- plans %>%
-  mutate(
-    denom = ndv + nrv,
-    ndshare = ifelse(denom == 0, 0.5, ndv / denom)
-  ) %>%
-  select(-denom)
+    mutate(
+        denom = ndv + nrv,
+        ndshare = ifelse(denom == 0, 0.5, ndv/denom)
+    ) %>%
+    select(-denom)
 
 cli_process_done()
 
--- analyses/NY_cd_2000/02_setup_NY_cd_2000.R
+++ analyses/NY_cd_2000/02_setup_NY_cd_2000.R
@@ -5,17 +5,17 @@
 cli_process_start("Creating {.cls redist_map} object for {.pkg NY_cd_2000}")
 
 map <- redist_map(ny_shp, pop_tol = 0.005,
-                  existing_plan = cd_2000, adj = ny_shp$adj)
+    existing_plan = cd_2000, adj = ny_shp$adj)
 # Pseudo-county
 map <- map %>%
-  mutate(
-    pseudo_county = pick_county_muni(
-      map,
-      counties = county,   
-      munis    = muni,     
-      pop_muni = get_target(map)
+    mutate(
+        pseudo_county = pick_county_muni(
+            map,
+            counties = county,
+            munis    = muni,
+            pop_muni = get_target(map)
+        )
     )
-  )
 
 # Add an analysis name attribute
 attr(map, "analysis_name") <- "NY_2000"
