6b7d03da4b26ca740c7f3d50f722348c94bd2c15
Brian Zhou <87774511+brianlzhou@users.noreply.github.com>
2026-04-13 21:15:41 -0700

    Update 03_sim_FL_cd_2000.R

--- analyses/FL_cd_2000/03_sim_FL_cd_2000.R
+++ analyses/FL_cd_2000/03_sim_FL_cd_2000.R
@@ -37,7 +37,8 @@ plans <- redist_smc(
   sampling_space = sampling_space_val,
   ms_params = list(frequency = 1L, mh_accept_per_smc = 65),
   split_params = list(splitting_schedule = "any_valid_sizes"),
-  verbose = T
+  verbose = T,
+  ncores = 112
 )
 
 plans <- plans %>% filter(draw != "cd_2000") %>%
