489ee27462a91206f2b8e2f188d04f8141f5b6c9
Christopher Kenny <ctk44@cornell.edu>
2026-05-12 10:22:56 -0400

    Update baf.R

--- R/baf.R
+++ R/baf.R
@@ -8,54 +8,70 @@
 #'
 #' @return a data from of `to` units, with `from` columns added, ready to be joined
 #' @export
-make_from_baf <- function(state, from = "INCPLACE_CDP", to = "VTD", year = 2020) {
-    if (year == 2020) {
-        baf <- PL94171::pl_get_baf(state, cache_to = here(str_glue("data-raw/{state}/{state}_baf.rds")))
-    } else {
-        baf <- get_baf_10(state, cache_to = here(str_glue("data-raw/{state}/{state}_baf_10.rds")))
-        if ('VTD' %in% names(baf)) {
-            baf[['VTD']] <- baf[['VTD']] |>
-                mutate(DISTRICT = str_pad_l0(DISTRICT, 6))
-        }
+make_from_baf <- function(
+  state,
+  from = "INCPLACE_CDP",
+  to = "VTD",
+  year = 2020
+) {
+  if (year == 2020) {
+    baf <- PL94171::pl_get_baf(
+      state,
+      cache_to = here(str_glue("data-raw/{state}/{state}_baf.rds"))
+    )
+  } else {
+    baf <- get_baf_10(
+      state,
+      cache_to = here(str_glue("data-raw/{state}/{state}_baf_10.rds"))
+    )
+    if ('VTD' %in% names(baf)) {
+      baf[['VTD']] <- baf[['VTD']] |>
+        mutate(DISTRICT = str_pad_l0(DISTRICT, 6))
     }
+  }
 
-    if (is.character(from)) {
-        d_from <- baf[[from]]
-    } else {
-        d_from <- from
-        from = names(from)[2]
-    }
-    d_to <- baf[[to]]
-    if (is.null(from)) {
-      cli::cli_abort("{.arg from} not found in {state} BAF.")
-    }
-    if (is.null(to)) {
-      cli::cli_abort("{.arg to} not found in {state} BAF.")
-    }
+  if (is.character(from)) {
+    d_from <- baf[[from]]
+  } else {
+    d_from <- from
+    from = names(from)[2]
+  }
+  d_to <- baf[[to]]
+  if (is.null(from)) {
+    cli::cli_abort("{.arg from} not found in {state} BAF.")
+  }
+  if (is.null(to)) {
+    cli::cli_abort("{.arg to} not found in {state} BAF.")
+  }
 
-    state_fp <- str_sub(d_to$BLOCKID[1], 1, 2)
-    fmt_baf <- function(x, nm) {
-        tidyr::unite(x, {{ nm }}, -BLOCKID, sep = "") %>%
-            mutate({{ nm }} := na_if(.[[nm]], "NA"))
-    }
+  state_fp <- str_sub(d_to$BLOCKID[1], 1, 2)
+  fmt_baf <- function(x, nm) {
+    tidyr::unite(x, {{ nm }}, -BLOCKID, sep = "") %>%
+      mutate({{ nm }} := na_if(.[[nm]], "NA"))
+  }
 
-    d_to <- fmt_baf(d_to, "to")
-    d_from <- fmt_baf(d_from, "from")
-    d <- left_join(d_to, d_from, by = "BLOCKID")
-    d <- d |>
-        group_by(to) |>
-        summarize(from = names(which.max(table(from, useNA = "always"))))
-    if (from == "INCPLACE_CDP") {
-      from <- "muni"
-    }
-    to <- stringr::str_to_lower(to)
-    from <- stringr::str_to_lower(from)
-    d |>
-      dplyr::rename({{ to }} := to, {{ from }} := from)
+  d_to <- fmt_baf(d_to, "to")
+  d_from <- fmt_baf(d_from, "from")
+  d <- left_join(d_to, d_from, by = "BLOCKID")
+  d <- d |>
+    group_by(to) |>
+    summarize(from = names(which.max(table(from, useNA = "always"))))
+  if (from == "INCPLACE_CDP") {
+    from <- "muni"
+  }
+  to <- stringr::str_to_lower(to)
+  from <- stringr::str_to_lower(from)
+  d |>
+    dplyr::rename({{ to }} := to, {{ from }} := from)
 }
 
 
-get_baf_10 <- function(state, geographies = NULL, cache_to = NULL, refresh = FALSE) {
+get_baf_10 <- function(
+  state,
+  geographies = NULL,
+  cache_to = NULL,
+  refresh = FALSE
+) {
   if (!is.null(cache_to) && file.exists(cache_to) && !refresh) {
     return(readRDS(cache_to))
   }
@@ -66,7 +82,9 @@ get_baf_10 <- function(state, geographies = NULL, cache_to = NULL, refresh = FAL
   zip_path <- tempfile(fileext = '.zip')
   zip_dir <- dirname(zip_path)
   base_name <- str_glue('BlockAssign_ST{fips}_{abb}')
-  zip_url <- str_glue('https://www2.census.gov/geo/docs/maps-data/data/baf/{base_name}.zip')
+  zip_url <- str_glue(
+    'https://www2.census.gov/geo/docs/maps-data/data/baf/{base_name}.zip'
+  )
   download(url = zip_url, path = zip_path)
 
   files <- utils::unzip(zip_path, list = TRUE)$Name
@@ -75,11 +93,15 @@ get_baf_10 <- function(state, geographies = NULL, cache_to = NULL, refresh = FAL
 
   for (fname in files) {
     geogr <- str_match(fname, paste0(base_name, '_([A-Z_]+)\\.txt'))[, 2]
-    if (!is.null(geographies) && !(geogr %in% geographies)) next
-    table <- readr::read_delim(file.path(zip_dir, fname),
+    if (!is.null(geographies) && !(geogr %in% geographies)) {
+      next
+    }
+    table <- readr::read_delim(
+      file.path(zip_dir, fname),
       delim = ',',
       col_types = readr::cols(.default = 'c'),
-      progress = interactive(), lazy = FALSE
+      progress = interactive(),
+      lazy = FALSE
     )
     # check final column is not all NA
     if (!all(is.na(table[[ncol(table)]]))) {
@@ -100,12 +122,12 @@ get_baf_10 <- function(state, geographies = NULL, cache_to = NULL, refresh = FAL
 #' @return path to new zip directory, invisibly
 #' @export
 download_baf_cd113 <- function() {
-    zip_dir <- fs::dir_create('data-raw/ZZ_baf_cd113')
-    zip_url <- 'https://www2.census.gov/programs-surveys/decennial/rdo/mapping-files/2013/113-congressional-district-bef/cd113.zip'
-    zip_path <- paste0(zip_dir, '/cd113.zip')
-    download(url = zip_url, path = zip_path)
-    utils::unzip(zip_path, exdir = zip_dir)
-    invisible(zip_dir)
+  zip_dir <- fs::dir_create('data-raw/ZZ_baf_cd113')
+  zip_url <- 'https://www2.census.gov/programs-surveys/decennial/rdo/mapping-files/2013/113-congressional-district-bef/cd113.zip'
+  zip_path <- paste0(zip_dir, '/cd113.zip')
+  download(url = zip_url, path = zip_path)
+  utils::unzip(zip_path, exdir = zip_dir)
+  invisible(zip_dir)
 }
 
 
@@ -116,18 +138,21 @@ download_baf_cd113 <- function() {
 #' @return a tibble with block equivalency for 2010 block GEOIDs to CD113 (2013)
 #' @export
 read_baf_cd113 <- function(state) {
-    if (!fs::dir_exists('data-raw/ZZ_baf_cd113')) {
-        download_baf_cd113()
-    }
+  if (!fs::dir_exists('data-raw/ZZ_baf_cd113')) {
+    download_baf_cd113()
+  }
 
-    path <- stringr::str_glue('data-raw/ZZ_baf_cd113/{censable::match_fips(state)}_{censable::match_abb(state)}_CD113.txt')
+  path <- stringr::str_glue(
+    'data-raw/ZZ_baf_cd113/{censable::match_fips(state)}_{censable::match_abb(state)}_CD113.txt'
+  )
 
-    read_csv(
-        path, col_types = c(BLOCKID = 'c', CD113 = 'i')
-    ) |>
-        rename(
-            cd_2010 = CD113
-        )
+  read_csv(
+    path,
+    col_types = c(BLOCKID = 'c', CD113 = 'i')
+  ) |>
+    rename(
+      cd_2010 = CD113
+    )
 }
 
 
@@ -135,22 +160,34 @@ read_baf_cd113 <- function(state) {
 #'
 #' @param state the state abbreviation
 #' @param to the unit to create the column at. Defaults to `VTD`s. Also supports tract.
+#' @param year_shd the year for the state house districts. Defaults to `2023`.
+#' @param year_ssd the year for the state senate districts. Defaults to `2023`.
 #'
 #' @return a data from of `to` units, with `from` columns added, ready to be joined
 #' @export
 #'
 #' @examples
 #' leg_from_baf('DE')
-leg_from_baf <- function(state, to = "VTD") {
+leg_from_baf <- function(state, to = "VTD", year_shd = 2023, year_ssd = 2023) {
   match.arg(to, c('VTD', 'tract'))
   # only 2020 available for SHD/SSD; use 2023 for shapes adopted in 2022
-  shd_baf <- baf::baf(state = state, year = 2023, geographies = 'shd')$SHD2022 |>
+  shd_baf <- baf::baf(
+    state = state,
+    year = year_shd,
+    geographies = 'shd'
+  )$SHD2022 |>
     rename(BLOCKID = GEOID, shd_20 = SLDLST)
-  ssd_baf <- baf::baf(state = state, year = 2023, geographies = 'ssd')$SSD2022 |>
+  ssd_baf <- baf::baf(
+    state = state,
+    year = year_ssd,
+    geographies = 'ssd'
+  )$SSD2022 |>
     rename(BLOCKID = GEOID, ssd_20 = SLDUST)
 
   if (to == 'VTD') {
-    vtd_baf <- baf::baf(state = state, year = 2023, geographies = 'VTD')[[1]] |>
+    vtd_baf <- baf::baf(state = state, year = 2023, geographies = 'VTD')[[
+      1
+    ]] |>
       mutate(
         BLOCKID = BLOCKID,
         vtd = paste0(stringr::str_sub(BLOCKID, 1, 2), COUNTYFP, DISTRICT),
@@ -171,11 +208,11 @@ leg_from_baf <- function(state, to = "VTD") {
       ssd_2020 = Mode(ssd_20)
     )
   # Why? Some states (nested mainly) use A, B, C... for SSDs
-  if (!any(is.na(suppressWarnings(as.integer(out$shd_2020))))){
+  if (!any(is.na(suppressWarnings(as.integer(out$shd_2020))))) {
     out <- out |>
       dplyr::mutate(shd_2020 = as.integer(shd_2020))
   }
-  if (!any(is.na(suppressWarnings(as.integer(out$ssd_2020))))){
+  if (!any(is.na(suppressWarnings(as.integer(out$ssd_2020))))) {
     out <- out |>
       dplyr::mutate(ssd_2020 = as.integer(ssd_2020))
   }
