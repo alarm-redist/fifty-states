2a8f165c190979c1710d09f15470a3bf60d2c549
Bella Postel <bellapostelteam@gmail.com>
2026-06-23 11:28:24 -0400

    SD files

---
+++ analyses/SD_leg_2020/SD_main_ssd.R
@@ -0,0 +1,5 @@
+setwd("~/fifty-states")
+
+source("analyses/SD_leg_2020/01_prep_SD_leg_2020.R")
+source("analyses/SD_leg_2020/02_setup_SD_leg_2020.R")
+source("analyses/SD_leg_2020/03_sim_SD_ssd_2020.R")
