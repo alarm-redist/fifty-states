6c0cb9afee9cabb593d74eae7b8ae1b3f62a6f40
Bella Postel <bellapostelteam@gmail.com>
2026-06-29 10:41:49 -0400

    Updated WA sim files

---
+++ analyses/WA_leg_2020/WA_main.R
@@ -0,0 +1,5 @@
+setwd("~/fifty-states")
+
+source("analyses/WA_leg_2020/01_prep_WA_leg_2020.R")
+source("analyses/WA_leg_2020/02_setup_WA_leg_2020.R")
+source("analyses/WA_leg_2020/03_sim_WA_shd_2020.R")
