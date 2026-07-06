a3923f19889ebca02d579a5f222b56b57a4791a8
ethanjasny <108710579+ethanjasny@users.noreply.github.com>
2026-07-06 14:08:51 -0700

    ssd convergence/splits

--- analyses/CO_leg_2020/03_sim_CO_ssd_2020.R
+++ analyses/CO_leg_2020/03_sim_CO_ssd_2020.R
@@ -8,16 +8,22 @@ cli_process_start("Running simulations for {.pkg CO_ssd_2020}")
 
 set.seed(2020)
 
-mh_accept_per_smc <- ceiling(n_distinct(map_ssd$ssd_2020)/3) + 15
+constr <- redist_constr(map_ssd) %>%
+    add_constr_total_plan_splits(strength = 2, admin = map_ssd$county) %>%
+    add_constr_total_plan_splits(strength = 1, admin = map_ssd$county_muni)
+
+mh_accept_per_smc <- ceiling(n_distinct(map_ssd$ssd_2020)/3) + 50
 
 plans <- redist_smc(
     map_ssd,
     nsims = 2e3, runs = 5,
     counties = pseudo_county,
+    constraints = constr,
     sampling_space = "linking_edge",
     ms_params = list(frequency = 1L, mh_accept_per_smc = mh_accept_per_smc),
     split_params = list(splitting_schedule = "any_valid_sizes"),
-    verbose = TRUE
+    verbose = TRUE,
+    pop_temper = 0.03
 )
 
 plans <- plans |>
