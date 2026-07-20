139538974caed941dba97bfea58a3cfdee9dcaf9
Bella Postel <bellapostelteam@gmail.com>
2026-07-20 18:06:03 -0400

    Update AL sim files

---
+++ analyses/AL_leg_2020/AL_main_shd.R
@@ -0,0 +1,5 @@
+setwd("~/fifty-states")
+
+source("analyses/AL_leg_2020/01_prep_AL_leg_2020.R")
+source("analyses/AL_leg_2020/02_setup_AL_leg_2020.R")
+source("analyses/AL_leg_2020/03_sim_AL_shd_2020.R")
