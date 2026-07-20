ff2857bdc5f94debd171d27096c455924f3da7c3
Bella Postel <bpa.swim@gmail.com>
2026-07-20 16:36:34 -0400

    CA analysis file changes (#364)
    Co-authored-by: Bella Postel <bellapostelteam@gmail.com>

---
+++ analyses/CA_leg_2020/02_setup_CA_leg_2020.R
@@ -0,0 +1,36 @@
+###############################################################################
+# Set up redistricting simulation for `CA_leg_2020`
+# © ALARM Project, June 2026
+###############################################################################
+cli_process_start("Creating {.cls redist_map} object for {.pkg CA_leg_2020}")
+
+map_ssd <- redist_map(ca_shp, pop_tol = 0.05,
+    existing_plan = ssd_2020, adj = ca_shp$adj)
+
+map_shd <- redist_map(ca_shp, pop_tol = 0.05,
+    existing_plan = shd_2020, adj = ca_shp$adj)
+
+# custom ssd constraints
+constr_ssd <- redist_constr(map_ssd)
+constr_ssd <- add_constr_total_splits(constr_ssd, strength = 1, admin = map_ssd$county)
+
+# custom shd constraints
+constr_shd <- redist_constr(map_shd)
+constr_shd <- add_constr_total_splits(constr_shd, strength = 1.5, admin = map_shd$county)
+
+# make pseudo counties with default settings
+map_ssd <- map_ssd |>
+    mutate(pseudo_county = pick_county_muni(map_ssd, counties = county, munis = muni,
+        pop_muni = get_target(map_ssd)))
+map_shd <- map_shd |>
+    mutate(pseudo_county = pick_county_muni(map_shd, counties = county, munis = muni,
+        pop_muni = get_target(map_shd)))
+
+# Add an analysis name attribute
+attr(map_ssd, "analysis_name") <- "CA_SSD_2020"
+attr(map_shd, "analysis_name") <- "CA_SHD_2020"
+
+# Output the redist_map object. Do not edit this path.
+write_rds(map_ssd, "data-out/CA_2020/CA_leg_2020_map_ssd.rds", compress = "xz")
+write_rds(map_shd, "data-out/CA_2020/CA_leg_2020_map_shd.rds", compress = "xz")
+cli_process_done()
---
+++ analyses/CA_leg_2020/03_sim_CA_shd_2020.R
@@ -0,0 +1,60 @@
+###############################################################################
+# Simulate plans for `CA_shd_2020` SHD
+# © ALARM Project, June 2026
+###############################################################################
+
+# Run the simulation -----
+cli_process_start("Running simulations for {.pkg CA_shd_2020}")
+
+set.seed(2020)
+
+mh_accept_per_smc <- ceiling(n_distinct(map_shd$shd_2020)/3) + 150
+
+plans <- redist_smc(
+    map_shd,
+    nsims = 3000, runs = 5,
+    constraints = constr_shd,
+    ncores = 0,
+    counties = pseudo_county,
+    sampling_space = "linking_edge",
+    ms_params = list(frequency = 1L, mh_accept_per_smc = mh_accept_per_smc),
+    split_params = list(splitting_schedule = "any_valid_sizes"),
+    verbose = TRUE
+)
+
+# IF CORES OR OTHER UNITS HAVE BEEN MERGED:
+# make sure to call `pullback()` on this plans object!
+
+plans <- plans |>
+    group_by(chain) |>
+    filter(as.integer(draw) < min(as.integer(draw)) + 2000) |> # thin samples
+    ungroup()
+plans <- match_numbers(plans, "shd_2020")
+
+cli_process_done()
+cli_process_start("Saving {.cls redist_plans} object")
+
+# Output the redist_map object. Do not edit this path.
+write_rds(plans, here("data-out/CA_2020/CA_shd_2020_plans.rds"), compress = "xz")
+cli_process_done()
+
+# The following line is uncommented when viewing validation plots
+# plans <- readRDS("data-out/CA_2020/CA_shd_2020_plans.rds")
+
+# Compute summary statistics -----
+cli_process_start("Computing summary statistics for {.pkg CA_shd_2020}")
+
+plans <- add_summary_stats(plans, map_shd)
+
+# Output the summary statistics. Do not edit this path.
+save_summary_stats(plans, "data-out/CA_2020/CA_shd_2020_stats.csv")
+
+cli_process_done()
+
+if (interactive()) {
+    library(ggplot2)
+    library(patchwork)
+
+    validate_analysis(plans, map_shd)
+    summary(plans)
+}
