884509a76353a9019a2a5bcbe6fb489278e2aac8
Kento Tamaki <kt794@georgetown.edu>
2026-08-03 00:54:47 +0900

    Update 03_sim_AZ_cd_1990.R

--- analyses/AZ_cd_1990/03_sim_AZ_cd_1990.R
+++ analyses/AZ_cd_1990/03_sim_AZ_cd_1990.R
@@ -8,50 +8,36 @@ cli_process_start("Running simulations for {.pkg AZ_cd_1990}")
 
 set.seed(1990)
 
-constr_az <- redist_constr(map) %>%
-  add_constr_splits(
-    strength = 0.25,
-    admin = county_muni
-  ) %>%
-  add_constr_grp_hinge(
-    20,
-    vap_hisp,
-    vap,
-    0.32
-  ) %>%
-  add_constr_grp_hinge(
-    -20,
-    vap_hisp,
-    vap,
-    0.27
-  )
+constr_seed <- redist_constr(map) %>%
+    add_constr_splits(strength = 0.25, admin = county_muni) %>%
+    add_constr_grp_hinge(120, vap_hisp, vap, 0.50, only_districts = TRUE)
 
-plans <- redist_smc(
-  map,
-  nsims = 4000,
-  runs = 5,
-  counties = pseudo_county,
-  constraints = constr_az
-)
+plans_seed <- redist_smc(map, nsims = 30000, counties = pseudo_county,
+    constraints = constr_seed, n_steps = 1)
+
+init_m <- get_plans_matrix(plans_seed)
+init_m <- init_m[, sample(ncol(init_m), 4000), drop = FALSE]
+
+constr_az <- redist_constr(map) %>%
+    add_constr_splits(strength = 0.10, admin = county_muni) %>%
+    add_constr_grp_hinge(30, vap_hisp, vap, 0.45, only_districts = TRUE) %>%
+    add_constr_grp_hinge(-30, vap_hisp, vap, 0.20, only_districts = TRUE)
+
+set.seed(1990)
+plans <- redist_smc(map, nsims = 4000, runs = 5, counties = pseudo_county,
+    constraints = constr_az, init_particles = init_m)
 
 plans <- plans |>
-  group_by(chain) |>
-  filter(as.integer(draw) < min(as.integer(draw)) + 1000) |>
-  ungroup()
-
+    group_by(chain) |>
+    filter(as.integer(draw) < min(as.integer(draw)) + 1000) |> # thin samples
+    ungroup()
 plans <- match_numbers(plans, "cd_1990")
 
 cli_process_done()
-
-# Save plans -----
 cli_process_start("Saving {.cls redist_plans} object")
 
-write_rds(
-  plans,
-  here("data-out/AZ_1990/AZ_cd_1990_plans.rds"),
-  compress = "xz"
-)
-
+# Output the redist_map object. Do not edit this path.
+write_rds(plans, here("data-out/AZ_1990/AZ_cd_1990_plans.rds"), compress = "xz")
 cli_process_done()
 
 # Compute summary statistics -----
@@ -59,10 +45,8 @@ cli_process_start("Computing summary statistics for {.pkg AZ_cd_1990}")
 
 plans <- add_summary_stats(plans, map)
 
-save_summary_stats(
-  plans,
-  "data-out/AZ_1990/AZ_cd_1990_stats.csv"
-)
+# Output the summary statistics. Do not edit this path.
+save_summary_stats(plans, "data-out/AZ_1990/AZ_cd_1990_stats.csv")
 
 cli_process_done()
 
