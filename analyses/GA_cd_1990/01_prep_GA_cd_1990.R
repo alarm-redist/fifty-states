6b652bb3ef3e3c1efce101d3e516c8f076a120fd
Kento Tamaki <kt794@georgetown.edu>
2026-08-25 01:01:34 +0900

    Update 01_prep_GA_cd_1990.R

--- analyses/GA_cd_1990/01_prep_GA_cd_1990.R
+++ analyses/GA_cd_1990/01_prep_GA_cd_1990.R
@@ -74,7 +74,7 @@ if (!file.exists(here(shp_path))) {
 
     names(ga_shp)
 
-    # 3. For each county, logit-shift ndv/nrv to the 2000 target from MEDSL ----
+    # 3. For each county, logit-shift ndv/nrv to the 1992 target from LEIP ----
     ga_shp <- ga_shp |>
       group_by(county_fips) |>
       group_split() |>
