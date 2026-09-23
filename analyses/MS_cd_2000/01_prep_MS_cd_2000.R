7ff301fd129ce6e6cb50b81c0742f2ef4d34bf69
Brian Zhou <87774511+brianlzhou@users.noreply.github.com>
2026-09-23 12:13:33 -0700

    code fixes (#384)
    * code fixes
    
    * drop duplicated block
    
    ---------
    
    Co-authored-by: Christopher T. Kenny <ctk44@cornell.edu>

--- analyses/MS_cd_2000/01_prep_MS_cd_2000.R
+++ analyses/MS_cd_2000/01_prep_MS_cd_2000.R
@@ -56,34 +56,35 @@ if (!file.exists(here(shp_path))) {
     ms_shp <- ms_shp %>%
         fix_geo_assignment(muni)
 
+    # 1. Load the MEDSL county CSV as `medsl_cty` ----
+    medsl_cty <- read_csv(
+        here("data-raw/baseline_voteshare_medsl_00.csv"),
+        show_col_types = FALSE
+    )
+
+    # 2. Add county_fips column based on VTD GEOID ----
+    ms_shp <- ms_shp |>
+        mutate(county_fips = stringr::str_sub(GEOID, 1, 5))
+
+    names(ms_shp)
+
+    # 3. For each county, logit-shift ndv/nrv to the 2000 target from MEDSL ----
+    ms_shp <- ms_shp |>
+        group_by(county_fips) |>
+        group_split() |>
+        lapply(function(x) {
+            meds <- medsl_cty |>
+                filter(county == x$county_fips[1])
+            target <- meds$dshare_00[1]
+
+            x |>
+                logit_shift_baseline(ndv = ndv, nrv = nrv, target = target)
+        }) |>
+        bind_rows()
+
     write_rds(ms_shp, here(shp_path), compress = "gz")
     cli_process_done()
 } else {
     ms_shp <- read_rds(here(shp_path))
     cli_alert_success("Loaded {.strong MS} shapefile")
 }
-
-# 1. Load the MEDSL county CSV as `medsl_cty` ----
-medsl_cty <- read_csv(
-    here("data-raw/baseline_voteshare_medsl_00.csv"),
-    show_col_types = FALSE
-)
-
-# 2. Add county_fips column based on VTD GEOID ----
-ms_shp <- ms_shp |>
-    mutate(county_fips = stringr::str_sub(GEOID, 1, 5))
-
-names(ms_shp)
-
-# 3. For each county, logit-shift ndv/nrv to the 2000 target from MEDSL ----
-ms_shp |>
-    group_by(county_fips) |>
-    group_split() |>
-    lapply(function(x) {
-        meds <- medsl_cty |>
-            filter(county == x$county_fips[1])
-        target <- meds$dshare_00[1]
-
-        x |>
-            logit_shift_baseline(ndv = ndv, nrv = nrv, target = target)
-    })
